24d359f0a2ec05a8c861d28ac4d8c6b2ad32c95a
burnett <>
2000-12-08 21:54:19 +0000

    fix GlastDetSvc access

--- CalRecon/src/CalRecoAlg.cxx
+++ CalRecon/src/CalRecoAlg.cxx
@@ -44,17 +44,21 @@ StatusCode CalRecoAlg::initialize() {
     
     // now try to find the GlastDevSvc service
     IGlastDetSvc* detSvc = 0;
-    
+#if 1    
+    const IID&  IID_IGlastDetSvc  =  401; // wired it for now!
     StatusCode sc = serviceLocator()->getService ("GlastDetSvc",
         IID_IGlastDetSvc, reinterpret_cast<IInterface*&>( detSvc ));
     
+    m_detSvc = detSvc;
+
+#else //this is the new way in v6, 
+    StatusCode sc = service("GlastDetSvc", m_detSvc);
+#endif    
+
     if (!sc.isSuccess ()){
         log << MSG::ERROR << "Couldn't find the GlastDetSvc!" << endreq;
         return StatusCode::FAILURE;
     }
-    m_detSvc = detSvc;
-    
-    
     return StatusCode::SUCCESS;
 }
 
