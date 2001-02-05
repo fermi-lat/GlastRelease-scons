9cf5c97d8e482fbfd9549821c0fedc49ea6aa252
heather <>
2001-02-05 22:24:49 +0000

    minor updates

--- Event/Event/Digi/AcdDigi.h
+++ Event/Event/Digi/AcdDigi.h
@@ -28,7 +28,7 @@
 //------------------------------------------------------------------------------
  */
 
-extern const CLID& CLID_IrfAcdHit;
+extern const CLID& CLID_AcdDigi;
 
 
 class AcdDigi : virtual public ContainedObject  { 
@@ -50,27 +50,27 @@ public:
 
     //! Retrieve reference to class definition structure
     virtual const CLID& clID() const   { return AcdDigi::classID(); }
-    static const CLID& classID()       { return CLID_IrfAcdHit; }
+    static const CLID& classID()       { return CLID_AcdDigi; }
 
     /// Retrieve digi identifier
-    const idents::ScintillatorId getID() const;
+    const idents::ScintillatorId ID() const;
 
     /// Retrieve pulse height
-    const int getPulseHeight() const;
+    const int PulseHeight() const;
 
-    const bool getVeto() const;
+    const bool Veto() const;
 
-    const bool getLowThreshold() const;
+    const bool LowThreshold() const;
 
-    const bool getHighThreshold() const;
+    const bool HighThreshold() const;
 
 
     /// Serialize the object for writing
-    StreamBuffer& serialize( StreamBuffer& s ) const;
+    virtual StreamBuffer& serialize( StreamBuffer& s ) const;
     /// Serialize the object for reading
-    StreamBuffer& serialize( StreamBuffer& s );
+    virtual StreamBuffer& serialize( StreamBuffer& s );
     /// Fill the ASCII output stream
-    std::ostream& fillStream( std::ostream& s ) const;
+    virtual std::ostream& fillStream( std::ostream& s ) const;
 
 
 
@@ -107,9 +107,65 @@ private:
     unsigned short       m_packedPHA;
 };
 
-    //! Definition of all container types of AcdDigi
-    template <class TYPE> class ObjectVector;
-    typedef ObjectVector<AcdDigi>     AcdDigiVector;
+
+//! Definition of all container types of AcdDigi
+template <class TYPE> class ObjectVector;
+typedef ObjectVector<AcdDigi>     AcdDigiVector;
+
+template <class TYPE> class ObjectList;
+typedef ObjectList<AcdDigi> AcdDigiList;
+
+/// Serialize the object for writing
+inline StreamBuffer& AcdDigi::serialize( StreamBuffer& s ) const
+{
+  ContainedObject::serialize(s);
+  return s
+    << m_ID
+    << m_packedPHA;
+}
+
+
+/// Serialize the object for reading
+inline StreamBuffer& AcdDigi::serialize( StreamBuffer& s )
+{
+  ContainedObject::serialize(s);
+ // s >> m_ID  need to add for ScintillatorId
+   s >> m_packedPHA;
+
+  // unpack the bits from m_packedPHA
+  m_veto =  (m_packedPHA >> ADC_V_VETO) & ADC_M_VETO;
+  m_lowThreshold =  (m_packedPHA >> ADC_V_LOWTHRESH) & ADC_M_LOWTHRESH;
+  m_highThreshold =  (m_packedPHA >> ADC_V_HIGHTHRESH) & ADC_M_HIGHTHRESH;
+
+  return s;
+}
+
+
+/// Fill the ASCII output stream
+inline std::ostream& AcdDigi::fillStream( std::ostream& s ) const
+{
+  return s
+    << "    base class AcdDigi :"
+    << "\n        ID = ( "
+    << GlastEventFloatFormat( GlastEvent::width, GlastEvent::precision )
+    << m_ID << ", "
+    << "\n        pulse height      = "
+    << GlastEventFloatFormat( GlastEvent::width, GlastEvent::precision )
+    << m_pulseHeight << ", "
+    << "\n        veto              = "
+    << GlastEventFloatFormat( GlastEvent::width, GlastEvent::precision )
+    << m_veto   << " )"
+    << "\n        Low Threshold     = "
+    << m_lowThreshold << " )"
+    << "\n        High Threshold     = "
+    << GlastEventFloatFormat( GlastEvent::width, GlastEvent::precision )
+    << m_highThreshold << " )"
+    << "\n        packed PHA        = "
+    << GlastEventFloatFormat( GlastEvent::width, GlastEvent::precision )
+    << m_packedPHA << " )";
+}
+
+
 
 
 #endif    // GlastEvent_AcdDigi_H
