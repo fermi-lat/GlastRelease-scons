2911fb93fda1d19feaa8d0a16763da1a60aacbcd
heather <>
2001-02-05 22:27:32 +0000

    add AcdDigi to EventModel

--- Event/src/TopLevel/EventModel.cpp
+++ Event/src/TopLevel/EventModel.cpp
@@ -38,6 +38,7 @@ public:
         EventModel::Irf::IrfTkrHits = EventModel::Irf::Event + "/IrfTkrHits";
 
         EventModel::Raw::Event = EventModel::Event + "/Raw";
+        EventModel::Raw::AcdDigi = EventModel::Raw::Event + "/AcdDigi";
         EventModel::Raw::TdCsIDatas = EventModel::Raw::Event + "/TdCsIDatas";
         EventModel::Raw::TdSiDatas = EventModel::Raw::Event + "/TdSiDatas";
     }
@@ -77,4 +78,7 @@ public:
     const CLID& CLID_McIntegratingHit   = 2101;
     const CLID& CLID_McVertex           = 2102;
     const CLID& CLID_McPositionHit      = 2103;
+
+    // Digitization class IDs
+    const CLID& CLID_AcdDigi            = 2201;
         
