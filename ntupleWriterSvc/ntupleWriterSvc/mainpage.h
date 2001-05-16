a399b15803e0b69d9906c2d5092df67400085c0d
burnett <>
2001-05-16 14:42:41 +0000

    put in a mainpage

---
+++ ntupleWriterSvc/ntupleWriterSvc/mainpage.h
@@ -0,0 +1,15 @@
+// $Header$
+// Mainpage for doxygen
+
+/*! \mainpage package ntupleWriterSvc
+   See the <a href=../ntupleWriterSvc.htm>manual</a>.
+  <hr>
+  \section notes release notes
+  \include release.notes
+  \section requirements requirements
+  \include requirements
+  \section ChangeLog ChangeLog
+  \include ChangeLog
+
+*/
+
