f8d8bd88ebe6d8b32193077dcb6c5febd75ff833
chehtman <>
2001-06-18 23:42:32 +0000

    Light attenuation coefficient is used now to calculate longitudinal position.

--- CalRecon/src/CalIRFAlg.cxx
+++ CalRecon/src/CalIRFAlg.cxx
@@ -114,6 +114,8 @@ StatusCode CalIRFAlg::execute() {
     // see what is there
 //    csi->printOn(std::cout);
 
+    double latt = m_CalGeo->light_att();
+    double slope = (2-latt)/latt;
 	double ene = 0.0;
 	for (int l=0; l < m_CalGeo->numLayers();l++){
 		for (int v=0; v< m_CalGeo->numViews(); v++){
@@ -148,7 +150,7 @@ StatusCode CalIRFAlg::execute() {
 				Vector dirLog(xdir,ydir,0.); 
 				double asym = recLog->asymmetry();
 //				std::cout << " dirLog = " << dirLog << std::endl;
-				Point pLog = pCenter+dirLog*asym*3.0
+				Point pLog = pCenter+dirLog*asym*slope;
 					;
 
 				recLog->setPosition(pLog);
