c4e84436051252ac59e9fa9dd3e5f2cb5d67ab19
chehtman <>
2001-06-18 23:54:41 +0000

    Missing ntuple variables Cal_Xtal_Ratio, Cal_Fit_errNrm and Cal_TransvOffset added.

--- CalRecon/src/CalNtupleAlg.cxx
+++ CalRecon/src/CalNtupleAlg.cxx
@@ -91,94 +91,103 @@ StatusCode CalNtupleAlg::execute() {
     MsgStream   log( msgSvc(), name() );
     
     m_cls  = SmartDataPtr<CsIClusterList>(eventSvc(),"/Event/CalRecon/CsIClusterList");
-    
-    if (m_cls == 0){ sc = StatusCode::FAILURE;
-    
-    log << MSG::ERROR << "CalNtupleAlg failed to access CsIClusterList" << endreq;
-    return sc;
-    }
-    
-    
-    double fit_ener,fitalpha,fitlambda,profchi2,eleak,start;
-    float energy_sum;
-    
-    int nClust = m_cls->num();
-    //cout << nClust << endl;
-    for ( int icl = 0; icl<nClust;icl++){
-        CsICluster* cl = m_cls->Cluster(icl);
-        energy_sum = cl->energySum();
+
+	if (m_cls == 0){ sc = StatusCode::FAILURE;
+	
+	        log << MSG::ERROR << "CalNtupleAlg failed to access CsIClusterList" << endreq;
+			return sc;
+	}
+
+
+	double fit_ener,fitalpha,fitlambda,profchi2,eleak,start;
+	float energy_sum;
+
+	int nClust = m_cls->num();
+	//cout << nClust << endl;
+	for ( int icl = 0; icl<nClust;icl++){
+		CsICluster* cl = m_cls->Cluster(icl);
+		energy_sum = cl->energySum();
+
+		float zpos = (cl->position()).z();
+		float xpos = (cl->position()).x();
+		float ypos = (cl->position()).y();
+
+		const std::vector<double>& eneLayer = cl->getEneLayer();
+		const std::vector<Vector>& posLayer = cl->getPosLayer();
+
+		float trans_rms = cl->getRmsTrans();
+		float long_rms = cl->getRmsLong();
+
+		Vector caldir = cl->direction();
+		float caltheta=acos(caldir.z());
+		float calphi = 1.570796326794897;
+		if(caldir.x()!=0) calphi = atan(caldir.y()/caldir.x());
+		
+		fit_ener = cl->getFitEnergy();
+		profchi2 = cl->getProfChisq();
+		fitalpha = cl->getCsiAlpha();
+		fitlambda = cl->getCsiLambda();
+		start = cl->getCsiStart();
+		eleak = cl->energyLeak();
+        float transvOffset = cl->getTransvOffset();
         
-        float zpos = (cl->position()).z();
-        float xpos = (cl->position()).x();
-        float ypos = (cl->position()).y();
-        
-        const std::vector<double>& eneLayer = cl->getEneLayer();
-        const std::vector<Vector>& posLayer = cl->getPosLayer();
-        
-        float trans_rms = cl->getRmsTrans();
-        float long_rms = cl->getRmsLong();
-        
-        Vector caldir = cl->direction();
-        float caltheta=acos(caldir.z());
-        double calphi = M_PI/2; //1.570796326794897;
-        if(caldir.x()!=0) calphi = atan(caldir.y()/caldir.x());
-        
-        fit_ener = cl->getFitEnergy();
-        profchi2 = cl->getProfChisq();
-        fitalpha = cl->getCsiAlpha();
-        fitlambda = cl->getCsiLambda();
-        start = cl->getCsiStart();
-        eleak = cl->energyLeak();
-        
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Deposit", energy_sum);
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Inc_Prof", fit_ener);
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Inc_LeakCorr", eleak);
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Incident", fit_ener);  // for the moment
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Energy_Incident_CalTkr", energy_sum);  // to be updated
-        
-        const std::string name_eLayer = "Cal_eLayer";
-        const char* digit[8]={"0","1","2","3","4","5","6","7"};
-        for (int layer=0; layer<8; layer++)
-            sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), (name_eLayer+digit[layer]).c_str(), eneLayer[layer]);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Z", zpos);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_X", xpos);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Y", ypos);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_transv_rms", trans_rms);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_long_rms", long_rms);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_theta", caltheta);
-        
-        sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_phi", calphi);
-        
-    }
-    
-    m_crl = SmartDataPtr<CalRecLogs>(eventSvc(),"/Event/CalRecon/CalRecLogs"); 
-    if (m_crl == 0){ sc = StatusCode::FAILURE;
-    
-    log << MSG::ERROR << "CalNtupleAlg failed to access CalRecLogs" << endreq;
-    return sc;
-    }
-    
-    int no_xtals=0;
-    int no_xtals_trunc=0;
-    int nLogs = m_crl->num();
-    for (int jlog = 0; jlog < nLogs ; jlog++) {
-        CalRecLog* recLog = m_crl->Log(jlog);
-        
-        double eneLog = recLog->energy();
-        if(eneLog>0)no_xtals++;
-        if(eneLog>0.01*energy_sum)no_xtals_trunc++;
-    }
-    
+        float eFactor = .26 + .35 / (cl->energyCorrected());
+        float calFitErrNrm = transvOffset/eFactor;
+
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Transv_Offset", transvOffset);
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Fit_errNrm", calFitErrNrm);
+            
+	
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Deposit", energy_sum);
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Inc_Prof", fit_ener);
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Inc_LeakCorr", eleak);
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Energy_Incident", fit_ener);  // for the moment
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Energy_Incident_CalTkr", energy_sum);  // to be updated
+
+		const std::string name_eLayer = "Cal_eLayer";
+		const char* digit[8]={"0","1","2","3","4","5","6","7"};
+		for (int layer=0; layer<8; layer++)
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), (name_eLayer+digit[layer]).c_str(), eneLayer[layer]);
+	
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Z", zpos);
+		
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_X", xpos);
+
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Y", ypos);
+
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_transv_rms", trans_rms);
+		
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_long_rms", long_rms);
+
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_theta", caltheta);
+
+		sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_phi", calphi);
+
+	}
+
+	m_crl = SmartDataPtr<CalRecLogs>(eventSvc(),"/Event/CalRecon/CalRecLogs"); 
+	if (m_crl == 0){ sc = StatusCode::FAILURE;
+	
+	        log << MSG::ERROR << "CalNtupleAlg failed to access CalRecLogs" << endreq;
+			return sc;
+	}
+	
+	int no_xtals=0;
+	int no_xtals_trunc=0;
+	int nLogs = m_crl->num();
+	for (int jlog = 0; jlog < nLogs ; jlog++) {
+		CalRecLog* recLog = m_crl->Log(jlog);
+
+		double eneLog = recLog->energy();
+		if(eneLog>0)no_xtals++;
+		if(eneLog>0.01*energy_sum)no_xtals_trunc++;
+	}
+    float cal_xtal_ratio= (no_xtals>0) ? float(no_xtals_trunc)/no_xtals : 0;
+
     sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_No_Xtals", no_xtals);
     sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_No_Xtals_Trunc", no_xtals_trunc);
-    
+	sc = m_ntupleWriteSvc->addItem(m_tupleName.c_str(), "Cal_Xtal_Ratio", cal_xtal_ratio);
+ 
     return sc;
 }
 
