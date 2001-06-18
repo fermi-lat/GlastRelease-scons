b1e262a252342d04b33a27833fb17e04e0e4e1f2
chehtman <>
2001-06-18 23:46:37 +0000

    Display of shower direction in calorimeter added.

--- CalRecon/src/CalDisplay.cxx
+++ CalRecon/src/CalDisplay.cxx
@@ -55,10 +55,14 @@ private:
 	CalRecLogs** m_pp_crl;
 	CsIClusterList** m_pp_cls;
 	float m_logheight;
+    float m_calZtop;
+    float m_calZbottom;
 
 public:
-    CalRep(CalRecLogs** pp_crl, CsIClusterList** pp_cls, float logheight)
-		:m_pp_crl(pp_crl),m_pp_cls(pp_cls),m_logheight(logheight){}
+    CalRep(CalRecLogs** pp_crl, CsIClusterList** pp_cls, float logheight,
+        float calZtop, float calZbottom)
+		:m_pp_crl(pp_crl),m_pp_cls(pp_cls),m_logheight(logheight),
+         m_calZtop(calZtop), m_calZbottom(calZbottom){}
     void update(){
 
 		const Point p0(0.,0.,0.);
@@ -150,7 +154,25 @@ public:
 			lineTo(Point(x, y+s, z));
 			lineTo(Point(x, y, z-s));
 			lineTo(Point(x, y-s, z));		
-		}
+            
+            double dirX = (cl->direction()).x();
+            double dirY = (cl->direction()).y();
+            double dirZ = (cl->direction()).z();
+        
+            if(dirZ != 0.){
+
+                double xTop = x+dirX*(m_calZtop-z)/dirZ;
+                double yTop = y+dirY*(m_calZtop-z)/dirZ;
+                double xBottom = x+dirX*(m_calZbottom-z)/dirZ;
+                double yBottom = y+dirY*(m_calZbottom-z)/dirZ;
+
+                moveTo(Point(xTop,yTop,m_calZtop));
+                lineTo(Point(xBottom,yBottom,m_calZbottom));
+
+
+            }
+
+        }
 	}
 };
 
@@ -166,12 +188,18 @@ StatusCode CalDisplay::initialize()
 	
      sc = service("CalGeometrySvc", m_CalGeo);
 	 float logheight = m_CalGeo->logHeight();
+     float layerheight = m_CalGeo->layerHeight();
+     int nlayers = m_CalGeo->numLayers()*m_CalGeo->numViews();
+     float Z0 = m_CalGeo->Z0();
+     float calZtop = Z0+(nlayers-1)*layerheight;
+     float calZbottom = Z0;
 
     
     //Ok, see if we can set up the display
     if (sc.isSuccess())  {
 	//Set up the display rep for Clusters
-	guiSvc->guiMgr()->display().add(new CalRep(&m_crl,&m_cls,logheight), "Cal recon");
+	guiSvc->guiMgr()->display().add(
+        new CalRep(&m_crl,&m_cls,logheight,calZtop,calZbottom), "Cal recon");
     }
     
     return sc;
