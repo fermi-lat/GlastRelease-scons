aa2c4fe59eae6053adcb958d7fcf04967db61f88
burnett <>
2001-12-16 16:20:54 +0000

    clean up call to create materials, remove string id stuff

--- G4Generator/src/DetectorConstruction.cxx
+++ G4Generator/src/DetectorConstruction.cxx
@@ -17,6 +17,7 @@
 
 #include "G4SectionsVisitor.h"
 #include "G4MaterialsVisitor.h"
+#include "GlastDetectorManager.h"
 
 #include <iomanip>
 
@@ -34,86 +35,56 @@ DetectorConstruction::DetectorConstruction(std::string topvol, std::string visit
   gddManager->setMode(visitorMode);
 
   gddManager->build(detModel::Manager::all);
+  // now create the GlastDetector manager, to pass in the id map
+  m_glastdet = new GlastDetectorManager(this);
   
 }
 
 DetectorConstruction::~DetectorConstruction()
 { 
-  delete sDetector;
+  detModel::Manager* gddManager = detModel::Manager::getPointer();
+  gddManager->cleanGdd(); 
+  delete m_glastdet;
 }
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {  
-  sDetector = 0;
   // This method build the material -- MUST be implemented with a MaterialVisitor
-  DefineMaterial();
+  detModel::Manager::getPointer()->startVisitor(&G4MaterialsVisitor());
+  
   // This method build the geometry with a visitor
   return GeometryConstruct();
 }
 
-void DetectorConstruction::DefineMaterial()
-{
-  //------- MATERIALS --------------------------------------
-  
-  detModel::Manager* gddManager = detModel::Manager::getPointer();
-  
-  G4MaterialsVisitor* visitor = new G4MaterialsVisitor;
-
-  gddManager->startVisitor(visitor);
-
-  delete visitor;
-
-}
 
 G4VPhysicalVolume* DetectorConstruction::GeometryConstruct()
 {
-  unsigned int i;
-  G4VPhysicalVolume* res;
+ 
 
   detModel::Manager* gddManager = detModel::Manager::getPointer();
   detModel::Gdd* gdd = gddManager->getGdd();
   
-  G4SectionsVisitor* visitor = new G4SectionsVisitor(m_topvol);
+  // create the visitor as with the topvolume, with a pointer to the map to fill
+  G4SectionsVisitor visitor (m_topvol, &m_idMap);
   
-  G4SDManager* SDman = G4SDManager::GetSDMpointer();
 
-  if(!sDetector)
-    {
-      sDetector = new SensitiveDetector("SensitiveDetector",this);
-      SDman->AddNewDetector( sDetector );
-    }
-
-  gddManager->startVisitor(visitor);
+  gddManager->startVisitor(&visitor);
   
-  //----------------------------------------
-  // alternate code introduced by THB
-  //----------------------------------------
-  for( std::vector<G4LogicalVolume*>::iterator it = visitor->g4Logicals.begin(); 
-  it != visitor->g4Logicals.end(); ++it){
+  for( std::vector<G4LogicalVolume*>::iterator it = visitor.g4Logicals.begin(); 
+  it != visitor.g4Logicals.end(); ++it){
       G4LogicalVolume * lv = *it;
       detModel::Shape* sh = gdd->getShapeByName(lv->GetName());
       if( sh  && sh->getSensitive() ){
-          //std::cout << "Found sensitive: " << sh->getName() << std::endl;
+          std::cout << "Found sensitive: " << sh->getName() << std::endl;
+          m_glastdet->process(lv);
       }
   }
   //----------------------------------------
 
-  for(i=0;i<visitor->g4Logicals.size();i++)
-    {
-      if(gdd->getShapeByName(visitor->g4Logicals[i]->GetName())!=0)
-	if ((gdd->getShapeByName(visitor->g4Logicals[i]->GetName()))->getSensitive())
-	  {
-	    visitor->g4Logicals[i]->SetSensitiveDetector(sDetector);
-	  }
-    }//end for
-  
-  g4ID = visitor->g4Identifiers;
 
-  res = visitor->worldphys;
-  // visitor->summary(std::cout);
+   G4VPhysicalVolume* res = visitor.worldphys;
+  visitor.summary(std::cout);
   
-  delete visitor;
-
   return res;
 }
 
--- G4Generator/src/DetectorConstruction.h
+++ G4Generator/src/DetectorConstruction.h
@@ -3,29 +3,49 @@
 
 class G4VPhysicalVolume;
 class SensitiveDetector;
-
+#ifdef WIN32
+#include <float.h>
+#endif
 #include <string>
+#include <vector>
 #include <map>
 #include "G4VUserDetectorConstruction.hh"
+#include "idents/VolumeIdentifier.h"
 
+class GlastDetectorManager;
+
+/** callec by the RunManager to construct:
+- full detector geometry
+- define materials
+- identify sensitive detectors, set up callbacks
+- create a map of partical ids and physical volumes
+*/
 class DetectorConstruction : public G4VUserDetectorConstruction
 {
  public:
+     //! @param topvol Name of the topvolume, e.g. "LAT"
+     //! @param mode   Visitor mode
      DetectorConstruction(std::string topvol, std::string mode);
   ~DetectorConstruction();
   
- public:
+  //! actuall call to construct things.
   G4VPhysicalVolume* Construct();
 
-  std::map <G4VPhysicalVolume*, std::string> g4ID;    
+  typedef std::map<const G4VPhysicalVolume*, idents::VolumeIdentifier > IdMap;
+
+  //! access to the map of physical volume /id pairs.
+  IdMap* idMap(){return &m_idMap;}
 
  private:
-  void DefineMaterial();
   G4VPhysicalVolume* GeometryConstruct();
   
-  SensitiveDetector* sDetector;  
-
   std::string m_topvol;
+
+  //! map with integer id's instead of string
+   IdMap m_idMap;
+
+   // delegate creation of detectors.
+   GlastDetectorManager* m_glastdet;
 };
 
 #endif
