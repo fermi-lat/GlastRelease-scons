ed8b13b0c3509353d1dd89b6a4fa2b1ebef97b28
burnett <>
2001-12-17 08:34:56 +0000

    fix unix build; remove setup.bat

--- G4Generator/cmt/setup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if .%1==. (set tag=Win32Debug ) else set tag=%1
-set tempfile=%HOMEDRIVE%%HOMEPATH%tmpsetup.bat
-c:\glast\CMT\v1r10\VisualC\cmt.exe -quiet -bat -pack=G4Generator -version=v0r3 setup -tag=%tag% >%tempfile%
-if exist %tempfile% call %tempfile%
-if exist %tempfile% del %tempfile%
-set PATH=%LD_LIBRARY_PATH%;%PATH%
