f833e01dbd047b122c87f251d334126c8dac9014
heather <>
2002-03-15 20:31:26 +0000

    use standard macros to load shareable libraries - new macros available in Gaudi v9

--- G4Generator/src/Dll/G4Generator_dll.cxx
+++ G4Generator/src/Dll/G4Generator_dll.cxx
@@ -1,27 +1,8 @@
-//====================================================================
-//
-//====================================================================
+/** @file G4Generator_dll.cpp
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
-#include <iostream>
-void GaudiDll::initialize(void*) 
-{
-}
+  $Header$
+*/
 
-void GaudiDll::finalize(void*) 
-{
-}
-extern void G4Generator_load();
-#include "GaudiKernel/FactoryTable.h"
-
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-  static bool first = true;
-  if ( first ) {  // Don't call for UNIX
-    G4Generator_load();
-    first = false;
-  }
-  return FactoryTable::instance()->getEntries();
-} 
-
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(G4Generator)
 
--- G4Generator/src/Dll/G4Generator_load.cxx
+++ G4Generator/src/Dll/G4Generator_load.cxx
@@ -1,21 +1,13 @@
-//====================================================================
-//====================================================================
+/** 
+* @file G4Generator_load.cpp
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header$
+*/
 
-#include "GaudiKernel/ICnvFactory.h"
-#include "GaudiKernel/ISvcFactory.h"
-#include "GaudiKernel/IAlgFactory.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
 
-
-#define DLL_DECL_SERVICE(x)    extern const ISvcFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_CONVERTER(x)  extern const ICnvFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_ALGORITHM(x)  extern const IAlgFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_OBJECT(x)     extern const IFactory& x##Factory; x##Factory.addRef();
-
-//! Load all  services: 
-void G4Generator_load() {
-    DLL_DECL_ALGORITHM( G4Generator );
+DECLARE_FACTORY_ENTRIES(G4Generator) {
+    DECLARE_ALGORITHM( G4Generator);
 } 
-
-extern "C" void G4Generator_loadRef()    {
-  G4Generator_load();
-}
