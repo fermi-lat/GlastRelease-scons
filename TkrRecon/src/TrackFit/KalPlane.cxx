7857337ef0120808cfcb8c4dbbc655448adce3ae
lsrea <>
2002-03-28 01:57:10 +0000

    TkrGeometrySvc: First try at getting constants from GlastDetSvc. Seems to work! KalPlane: one more cm->mm, and slightly adjust another hard-wired constant Tkr_Flight.xml: TEMPORARILY set z0 to zero.  For reasons not yet understood,      this makes the fitter start fitting, but of course the track comes out      in the wrong place. Definitely not a taggable version, but it's the workingest so far!

--- TkrRecon/src/TrackFit/KalPlane.cxx
+++ TkrRecon/src/TrackFit/KalPlane.cxx
@@ -174,7 +174,7 @@ KalHit KalPlane::predicted(KalHit::TYPE typ, int &nlayers, int klayer, double &z
     double down = -1.;
     if (nsteps <0 ) down = +1.; // going up;
 
-    double arc_len = nlayers * 32.4/fabs(dir_ini.z()); //mm
+    double arc_len = nlayers * 32.7/fabs(dir_ini.z()); //mm
     std::auto_ptr<IKalmanParticle> 
             kalPart(TkrReconAlg::m_gismoSvc->kalmanParticle(x_ini, dir_ini, arc_min));
     if(kalPart->trackToNextPlane()) {
@@ -231,7 +231,7 @@ KalHit KalPlane::predicted(KalHit::TYPE typ, int nsteps)
         dir_ini = -dir_ini;
     }
     
-    double deltaZ=down*fabs(nsteps)*3.24; // Bad ... need to re-engineer
+    double deltaZ=down*fabs(nsteps)*32.575; //mm and also Very Bad ... need to re-engineer
     
     double arc_len = fabs(deltaZ/dir_ini.z()); 
     std::auto_ptr<IKalmanParticle> 
