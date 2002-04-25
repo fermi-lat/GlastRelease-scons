2d7aac633bb6d2b69a0f292ac9d66af9f3b595b4
chehtman <>
2002-04-25 23:35:01 +0000

    Commented declaration of old algorithms and services

--- CalRecon/src/Dll/CalRecon_load.cpp
+++ CalRecon/src/Dll/CalRecon_load.cpp
@@ -24,14 +24,15 @@
 //! Load all  services: 
 void CalRecon_load() {
     
-    DLL_DECL_SERVICE( CalGeometrySvc );
+//    DLL_DECL_SERVICE( CalGeometrySvc );
     
-    DLL_DECL_ALGORITHM( CalRecLogsAlg );
-    DLL_DECL_ALGORITHM( CalDigiLogsAlg );
+//    DLL_DECL_ALGORITHM( CalRecLogsAlg );
+//    DLL_DECL_ALGORITHM( CalDigiLogsAlg );
 //     DLL_DECL_ALGORITHM( CalIRFAlg );
+    DLL_DECL_ALGORITHM( CalXtalRecAlg );
     DLL_DECL_ALGORITHM( CalClustersAlg );
-    DLL_DECL_ALGORITHM( CalNtupleAlg );
-    DLL_DECL_ALGORITHM( CalDisplay );
+//    DLL_DECL_ALGORITHM( CalNtupleAlg );
+//    DLL_DECL_ALGORITHM( CalDisplay );
 } 
 
 extern "C" void CalRecon_loadRef()    {
