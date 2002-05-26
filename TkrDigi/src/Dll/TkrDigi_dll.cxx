00018591ab1ac2ce0bf038422bc64a120a0e7189
burnett <>
2002-05-26 17:17:20 +0000

    Home for TkrSimpleDigiAlg, moved from GlastDigi, to be independent of Bari for now.

---
+++ TkrDigi/src/Dll/TkrDigi_dll.cxx
@@ -0,0 +1,7 @@
+/** @file TkrDigi_dll.cpp
+
+  $Header$
+*/
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(TkrDigi)
---
+++ TkrDigi/src/Dll/TkrDigi_load.cxx
@@ -0,0 +1,15 @@
+/** 
+* @file TkrDigi_load.cpp
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header$
+*/
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_FACTORY_ENTRIES(TkrDigi) {
+    DECLARE_ALGORITHM( TkrSimpleDigiAlg );
+
+} 
+
