18e1dededdfaa89119ae1c21ca8cfb726587137c
chehtman <>
2002-06-03 21:30:15 +0000

    Comments modified

--- CalRecon/src/CalXtalRecAlg.cxx
+++ CalRecon/src/CalXtalRecAlg.cxx
@@ -1,3 +1,14 @@
+// File and version Information:
+//   $Header$
+//
+// Description:
+//    CalXtalRecAlg is an algorithm to reconstruct calorimeter
+//    information in each individual crystal
+//
+// Author: A.Chekhtman
+
+
+
 #include "CalRecon/CalXtalRecAlg.h"
 #include "Event/Recon/CalRecon/CalXtalRecData.h"
 #include "GaudiKernel/MsgStream.h"
@@ -24,18 +35,36 @@ Algorithm(name, pSvcLocator) {
 }
 
 
-//################################################
 StatusCode CalXtalRecAlg::initialize()
-//################################################
+
+// Purpose and method:
+//           This function sets values to private data members,
+//           representing the calorimeter geometry and digitization
+//           constants. Information  from xml files is obtained using 
+//           GlastdetSvc::getNumericConstByName() function.
+//           To make this constant extraction in a loop, the pairs
+//           'constant pointer, constant name' are stored in
+//           std::map container. 
+//           Double and integer constants are extracted separatly,
+//           because constants of both types are returned
+//           by getNumericConstByName() as double.
+//      
+        
+        
 {
     MsgStream log(msgSvc(), name());
     StatusCode sc = StatusCode::SUCCESS;
 
         
     // extracting int constants
-    double value;
+    double value;  // intermediate variable for reading constants from
+                   // xml file as doubles and converting them to interger 
     typedef std::map<int*,std::string> PARAMAP;
-    PARAMAP param;
+    PARAMAP param; // map containing pointers to integer constants to be read
+                   // with their symbolic names from xml file used as a key 
+
+//     filling the map with information on constants to be read 
+    
     param[&m_xNum]=        std::string("xNum");
     param[&m_yNum]=        std::string("yNum");
     param[&m_eTowerCAL]=   std::string("eTowerCAL");
@@ -63,20 +92,27 @@ StatusCode CalXtalRecAlg::initialize()
 //    IGlastDetSvc* detSvc;
     sc = service("GlastDetSvc", detSvc);
     
-    
-    for(PARAMAP::iterator it=param.begin(); it!=param.end();it++){
+        // loop over all constants information contained in the map
+        for(PARAMAP::iterator it=param.begin(); it!=param.end();it++){
+
+        //  attempt to get teh constant value via the method of GlastDetSvc
         if(!detSvc->getNumericConstByName((*it).second, &value)) {
-            log << MSG::ERROR << " constant " <<(*it).second <<" not defined" << endreq;
+
+            // if not successful - give the error message and return
+            log << MSG::ERROR << " constant " <<(*it).second
+                <<" not defined" << endreq;
             return StatusCode::FAILURE;
+
+        //  if successful - fill the constant using the pointer from the map
         } else *((*it).first)=value;
     }
-    
-    int nTowers = m_xNum * m_yNum;
-    
+        
     // extracting double constants
     
     typedef std::map<double*,std::string> DPARAMAP;
-    DPARAMAP dparam;
+    DPARAMAP dparam; // map containing pointers to double constants to be read
+                     // with their symbolic names from xml file used as a key 
+
     dparam[m_maxEnergy]=std::string("cal.maxResponse0");
     dparam[m_maxEnergy+1]=std::string("cal.maxResponse1");
     dparam[m_maxEnergy+2]=std::string("cal.maxResponse2");
@@ -90,165 +126,265 @@ StatusCode CalXtalRecAlg::initialize()
             return StatusCode::FAILURE;
         } 
     }
-    
-    for (int r=0; r<4;r++) m_maxEnergy[r] *= 1000.; // from GeV to MeV
 
+    // conversion from GeV to MeV
+    for (int r=0; r<4;r++) m_maxEnergy[r] *= 1000.; 
 	
 	
 	return sc;
 }
-//################################################
+
+
 StatusCode CalXtalRecAlg::execute()
-//################################################
+
+//  Purpose and method:
+//     This function is called to do reconstruction
+//     for all hitted crystals in one event.
+//     It calls retrive() method to get access to input and output TDS data.
+//     It iterates over all elements in CalDigiCol and calls
+//     computeEnergy() and computePosition() methods doing real reconstruction.
+//
+//   TDS input: 
+//            CalDigiCol* m_CalDigiCol - private class member containing
+//            a pointer to the calorimeter digi callection 
+//
+//   TDS output: 
+//            CalXtalRecCol* m_CalXtalRecCol - private class member containing
+//            a pointer to the calorimeter crystal reconstructed data collection 
+//
+//     
+
 {
 	StatusCode sc = StatusCode::SUCCESS;
-    MsgStream log(msgSvc(), name());
-	sc = retrieve();
+        MsgStream log(msgSvc(), name());
 
+        sc = retrieve(); //get access to TDS data collections
 
+        // loop over all calorimeter digis in CalDigiCol
 	for (Event::CalDigiCol::const_iterator it = m_CalDigiCol->begin(); 
            it != m_CalDigiCol->end(); it++) {
-               idents::CalXtalId xtalId = (*it)->getPackedId();
-	   int lyr = xtalId.getLayer();
-	   int towid = xtalId.getTower();
-	   int icol  = xtalId.getColumn();
+           idents::CalXtalId xtalId = (*it)->getPackedId();
 		
-	   Event::CalXtalRecData* recData = new Event::CalXtalRecData((*it)->getMode(),xtalId);
+           // create new object to store crystal reconstructed data     
+           Event::CalXtalRecData* recData = 
+                   new Event::CalXtalRecData((*it)->getMode(),xtalId);
 	   
-	   computeEnergy(recData, *it);
-	   computePosition(recData);
-	   m_CalXtalRecCol->push_back(recData);
+           
+           // calculate energy in the crystal
+	   computeEnergy(recData, *it);      
+
+           // calculate position in the crystal
+	   computePosition(recData);   
+
+           // add new reconstructed data to the collection
+           m_CalXtalRecCol->push_back(recData); 
 	}
 
 	return sc;
 }
-//################################################
+
+
 StatusCode CalXtalRecAlg::finalize()
-//################################################
+// empty function: required by base class (Algorithm)
 {
 	StatusCode sc = StatusCode::SUCCESS;
-
-//	m_CalRecLogs->writeOut();
-
 	return sc;
 }
 
-//################################################
+
+
 StatusCode CalXtalRecAlg::retrieve()
-//################################################
+
+// Purpose and method: 
+//            This function provides access to the TDS input and output data
+//            by setting the private data members m_CalDigiCol            
+//            and m_CalXtalRecCol
+//
+//   TDS input: CalDigiCol
+//   TDS output: CalXtalrecCol
+//        
+//        
+        
 {
 	
     MsgStream log(msgSvc(), name());
     StatusCode sc = StatusCode::SUCCESS;
 
-	m_CalXtalRecCol = 0;
+    m_CalXtalRecCol = 0;
 
- 
-	m_CalXtalRecCol = new CalXtalRecCol;
+    //  create output data collection
+    m_CalXtalRecCol = new CalXtalRecCol;
 
-DataObject* pnode=0;
+    DataObject* pnode=0;
 
-    sc = eventSvc()->retrieveObject( EventModel::CalRecon::Event /*"/Event/CalRecon"*/, pnode );
+    // search for CalRecon section of Event directory in TDS
+    sc = eventSvc()->retrieveObject( EventModel::CalRecon::Event, pnode );
     
+    // if the required directory doesn't exist - create it
     if( sc.isFailure() ) {
-        sc = eventSvc()->registerObject( EventModel::CalRecon::Event /*"/Event/CalRecon"*/,new DataObject);
+        sc = eventSvc()->registerObject( EventModel::CalRecon::Event,
+                                         new DataObject);
         if( sc.isFailure() ) {
-            
-            log << MSG::ERROR << "Could not create CalRecon directory" << endreq;
+       
+             // if cannot create the directory - write an error message
+            log << MSG::ERROR << "Could not create CalRecon directory"
+                << endreq;
             return sc;
         }
     }
 
     
+    // get a pointer to the input TDS data collection
+    m_CalDigiCol = SmartDataPtr<Event::CalDigiCol>(eventSvc(),
+                                EventModel::Digi::CalDigiCol); 
 
-	m_CalDigiCol = SmartDataPtr<Event::CalDigiCol>(eventSvc(),EventModel::Digi::CalDigiCol /*"/Event/Digi/CalDigis"*/); 
-
-
-	 sc = eventSvc()->registerObject(EventModel::CalRecon::CalXtalRecCol /*"/Event/CalRecon/CalXtalRecCol"*/,m_CalXtalRecCol);
-	return sc;
+    //register output data collection as a TDS object
+    sc = eventSvc()->registerObject(EventModel::CalRecon::CalXtalRecCol,
+                                                       m_CalXtalRecCol);
+    return sc;
 }
 
-//----------------- private ----------------------
-//################################################
+
+
+
 void CalXtalRecAlg::computeEnergy(CalXtalRecData* recData, const Event::CalDigi* digi)
-//################################################
+
+//   Purpose and method:
+//                 This function calculates the energy for one crystal.
+//                 It makes a loop over all readout ranges (1 or 4, depending
+//                 on readout mode), and converts adc values for both crystal
+//                 faces into energy, using constants contained in private data
+//                 members m_maxEnergy, m_pedestal, m_maxAdc.
+//
+//  
+//  Input: CalDigi* digi - pointer to digitized calorimeter data for
+//                         one crystal
+//                            
+//  Output: CalXtalRecData* recData - pointer to reconstructed data for
+//                                    this crystal
+                                      
+
+
+
 {
-	MsgStream log(msgSvc(), name());
+        MsgStream log(msgSvc(), name());
 
 
-	const Event::CalDigi::CalXtalReadoutCol& readoutCol = digi->getReadoutCol();
+        const Event::CalDigi::CalXtalReadoutCol& readoutCol = digi->getReadoutCol();
 		
-	for ( Event::CalDigi::CalXtalReadoutCol::const_iterator it = readoutCol.begin();
+        // loop over readout ranges
+        for ( Event::CalDigi::CalXtalReadoutCol::const_iterator it = readoutCol.begin();
 		      it !=readoutCol.end(); it++){
-				int rangeP = it->getRange(idents::CalXtalId::POS); 
-				int rangeM = it->getRange(idents::CalXtalId::NEG); 
 
-				double adcP = it->getAdc(idents::CalXtalId::POS);	
-				double adcM = it->getAdc(idents::CalXtalId::NEG);	
+             // get readout range number for both crystal faces
+            int rangeP = it->getRange(idents::CalXtalId::POS); 
+            int rangeM = it->getRange(idents::CalXtalId::NEG); 
 
-				double eneP = m_maxEnergy[rangeP]*(adcP-m_pedestal)/(m_maxAdc-m_pedestal);
-				double eneM = m_maxEnergy[rangeM]*(adcM-m_pedestal)/(m_maxAdc-m_pedestal);
+            // get adc values 
+            double adcP = it->getAdc(idents::CalXtalId::POS);	
+            double adcM = it->getAdc(idents::CalXtalId::NEG);	
+
+            // convert adc values into energy
+            double eneP = m_maxEnergy[rangeP]*(adcP-m_pedestal)/(m_maxAdc-m_pedestal);
+            double eneM = m_maxEnergy[rangeM]*(adcM-m_pedestal)/(m_maxAdc-m_pedestal);
 				
-				CalXtalRecData::CalRangeRecData* rangeRec = new CalXtalRecData::CalRangeRecData(rangeP,eneP,rangeM,eneM);
-				recData->addRangeRecData(*rangeRec);
-		}		
+
+            // create output object
+            CalXtalRecData::CalRangeRecData* rangeRec =
+                new CalXtalRecData::CalRangeRecData(rangeP,eneP,rangeM,eneM);
+
+            // add output object to output collection
+            recData->addRangeRecData(*rangeRec);
+        }		
 	
 }
 
 
-//################################################
 void CalXtalRecAlg::computePosition(CalXtalRecData* recData)
-//################################################
+
+// Purpose and method:
+//              This function calculates the longitudinal position
+//              for each crystal from light asymmetry.
+//              
+//  Input: CalXtalRecData* recData - pointer to the reconstructed crystal data
+//  Output: the same object, the calculated position is stored using
+//             public function SetPosition()  
+
+
+
 {
 	MsgStream log(msgSvc(), name());
 
-	idents::CalXtalId xtalId = recData->getPackedId();	
+        // get crystal identification 
+        idents::CalXtalId xtalId = recData->getPackedId();	
 	
-	   int layer = xtalId.getLayer();
-	   int tower = xtalId.getTower();
-	   int col  = xtalId.getColumn();
+        // unpack crystal identification into tower, layer and column number
+	int layer = xtalId.getLayer();
+	int tower = xtalId.getTower();
+	int col   = xtalId.getColumn();
 
-			idents::VolumeIdentifier segm0Id;
-			segm0Id.append(m_eLATTowers);
-			segm0Id.append(tower/m_xNum);
-			segm0Id.append(tower%m_xNum);
-	  		segm0Id.append(m_eTowerCAL);
-	  		segm0Id.append(layer);
-	  		segm0Id.append(layer%2);
-			segm0Id.append(col);
-			segm0Id.append(m_eXtal);
-			segm0Id.append(0);
+        // create Volume Identifier for segment 0 of this crystal
+	idents::VolumeIdentifier segm0Id;
+	segm0Id.append(m_eLATTowers);
+	segm0Id.append(tower/m_xNum);
+	segm0Id.append(tower%m_xNum);
+	segm0Id.append(m_eTowerCAL);
+	segm0Id.append(layer);
+	segm0Id.append(layer%2); 
+        segm0Id.append(col);
+	segm0Id.append(m_eXtal);
+	segm0Id.append(0);
 
-            HepTransform3D transf;
-			detSvc->getTransform3DByID(segm0Id,&transf);
-			Vector vect0 = transf.getTranslation();
+        HepTransform3D transf;
+
+        //get 3D transformation for segment 0 of this crystal
+	detSvc->getTransform3DByID(segm0Id,&transf);
+
+        //get position of the center of the segment 0
+        Vector vect0 = transf.getTranslation();
 
 			
-			idents::VolumeIdentifier segm11Id;
-			for(int ifield = 0; ifield<fSegment; ifield++)segm11Id.append(segm0Id[ifield]);
-			segm11Id.append(m_nCsISeg-1);
+        // create Volume Identifier for the last segment of this crystal
+        idents::VolumeIdentifier segm11Id;
 
-			detSvc->getTransform3DByID(segm11Id,&transf);
-			Vector vect11 = transf.getTranslation();
+        // copy all fields from segm0Id, except segment number
+	for(int ifield = 0; ifield<fSegment; ifield++)segm11Id.append(segm0Id[ifield]);
+	segm11Id.append(m_nCsISeg-1); // set segment number for the last segment
 
+	//get 3D transformation for the last segment of this crystal
+        detSvc->getTransform3DByID(segm11Id,&transf);
 
-
+        //get position of the center of the last segment
+	Vector vect11 = transf.getTranslation();
 
 	Point p0(0.,0.,0.);		
-	Point pCenter = p0+(vect0+vect11)*0.5;
-	Vector dirLog = 0.5*(vect11-vect0)*m_nCsISeg/(m_nCsISeg-1);	
+
+        // position of the crystal center
+	Point pCenter = p0+(vect0+vect11)*0.5; 
+
+        //normalized vector of the crystal direction 
+        Vector dirXtal = 0.5*(vect11-vect0)*m_nCsISeg/(m_nCsISeg-1);	
 
 	
-	
+        //extract energy for the best range for both crystal faces 	
 	double eneNeg = recData->getEnergy(0,idents::CalXtalId::NEG);
 	double enePos = recData->getEnergy(0,idents::CalXtalId::POS);
 	double asym=0;
-	if(enePos>0 && eneNeg>0)asym = (enePos-eneNeg)/(enePos+eneNeg);
+
+        //calculate light asymmetry
+        if(enePos>0 && eneNeg>0)asym = (enePos-eneNeg)/(enePos+eneNeg);
+
+        // calculate slope - coefficient to convert light asymmetry into the 
+        // deviation from crystal center
 	double slope = (1+m_lightAtt)/(1-m_lightAtt);
 
-	Point pLog = pCenter+dirLog*asym*slope;
+        // calculate average position of energy deposit in this crystal
+	Point pXtal = pCenter+dirXtal*asym*slope;
 	
-	(recData->getRangeRecData(0))->setPosition(pLog);
+
+        // store calculated position in the reconstructed data
+        // for the best readout range
+        (recData->getRangeRecData(0))->setPosition(pXtal);
 
   
 }
