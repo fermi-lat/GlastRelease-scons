cd9366a627da58b919373f0fdae572844fc58678
chehtman <>
2002-06-12 00:16:44 +0000

    Comments added

--- Event/Event/Recon/CalRecon/CalCluster.h
+++ Event/Event/Recon/CalRecon/CalCluster.h
@@ -7,186 +7,274 @@
 #include "GaudiKernel/DataObject.h"
 #include "GaudiKernel/MsgStream.h"
 
-// #include "CalRecon/CalDisplay.h"
-
 extern const CLID& CLID_CalClusterCol;
 
 
-//----------------------------------------------
-//
-//   CalCluster
-//
-//   Transient Storage Data
-//----------------------------------------------
-//   It contains the high level data for the Calorimeter
-//----------------------------------------------
-//             J.A Hernando, Santa Cruz 02/29/00
-//----------------------------------------------
 
-//! High level data for the calorimeter
-/*! Transient storage of the results of the reconstruction performed
- *  in CalClustersAlg. It contains the data from one cluster in the
- *  calorimeter. 
- *  
- *  \author Alexandre Chehtman
- *  \author Regis Terrier
- *  \author Jose Angel Hernando
- *
- * \b Revisions:
- *  - 10/17/00    RT   Comments added
- *  - 08/20/00    RT   m_leakEnergy added
- *  - 02/29/00    JAH  first implementation
- */
 
 namespace Event 
-
 {
 
-//#####################################
-class CalCluster 
-//#####################################
-{
-public:
+/**
+*  @class CalCluster
+*
+*
+*  This class stores the results of the reconstruction performed
+*  in CalClustersAlg. It contains the reconstructed data for one
+*  cluster in the calorimeter. 
+*  
+*  \author Alexandre Chekhtman
+*  \author Regis Terrier
+*  \author Jose Angel Hernando
+*
+*/
+    
+    
+    class CalCluster 
+    {
+        
+    public:
+        
+        /// constructor
+        /// @param e total energy sum in the cluster
+        /// @param p reconstructed cluster position
+        CalCluster(double e, Point p);
+        
+        ~CalCluster() {}
+        
+        /**
+        *   initializing a subset 
+        *   of CalCluster data members
+        *  
+        *   @param eleak energy corrected by last layer correlation method
+        *   @param eneLayer vector of energy depositions for each layer
+        *   @param pLayer   average position for each layer
+        *   @param rmsLayer quadratic position spread for each layer
+        *   @param rms_long RMS of longitudinal position measurements 
+        *   @param rms_long RMS of transversal position measurements 
+        *   @param caldir particle direction reconstructed from calorimeter
+        *                 data
+        *   @param calTransvOffset transverse offset of position measured
+        *                          by calorimeter with respect 
+        *                          to prediction from tracker data
+        */
+        void initialize(double eleak,
+            std::vector<double> eneLayer,
+            std::vector<Vector> pLayer,
+            std::vector<Vector> rmsLayer,
+            double rms_long,
+            double rms_trans,
+            Vector caldir,
+            double calTransvOffset)
+        {
+            m_eneLayer = eneLayer;
+            m_leakEnergy = eleak;
+            m_pLayer = pLayer;
+            m_rmsLayer = rmsLayer;	
+            m_rmslong = rms_long;
+            m_rmstrans= rms_trans;
+            m_direction = caldir;
+            m_transvOffset = calTransvOffset;
+        }
+        
+        /**
+        *   storing the profile fitting results
+        *
+        *  @param fit_energy energy obtained by profile fitting
+        *  @param ki2        chi squared of the fit
+        *  @param fit_start  position of the shower start
+        *  @param fit_alpha  alpha parameter - position of the shower maximum
+        *  @param fit_lambda lambda parameter, describing the shower back tail
+        *  
+        */
+        
+        void initProfile(double fit_energy, double ki2, double fit_start,
+            double fit_alpha, double fit_lambda)
+        {
+            m_fitEnergy = fit_energy;
+            m_ProfChisq = ki2;
+            m_CsiAlpha = fit_alpha;
+            m_CsiLambda = fit_lambda;
+            m_start  = fit_start;
+        }
+        
+            
+        
+        /// get energy sum
+        double getEnergySum()        const {return m_energySum;}
+        
+        /// get energy corrected for leakage with last layer correlation
+        double getEnergyLeak()       const {return m_leakEnergy;}
+        
+        /// get energy corrrected by the best chosen method
+        double getEnergyCorrected()  const {return m_energyCorrected;}
+        
+        /// get energy for the layer with given number
+        double getEneLayer(int i) const {return m_eneLayer[i];}
+        
+        /// get position for the layer with given number
+        const Vector& getPosLayer(int i) const {return m_pLayer[i];}
+        
+        /// get vector of energies for all layers
+        const std::vector<double>& getEneLayer() const {return m_eneLayer;}
+        
+        /// get vector of positions for all layers
+        const std::vector<Vector>& getPosLayer() const {return m_pLayer;}
+        
+        /// get vector of quadratic spreads for all layers
+        const std::vector<Vector>& getRmsLayer() const {return m_rmsLayer;}
+        
+        /// get RMS of longitudinal position measurements
+        double getRmsLong()		  const {return m_rmslong;} 
+        
+        /// get RMS of transverse position measurements
+        double getRmsTrans()	  const {return m_rmstrans;}
+        
+        /// get transversal offset of cluster position with respect to 
+        /// prediction from tracker data
+        double getTransvOffset()  const {return m_transvOffset;}
+        
+        /// get reconstructed position
+        Point getPosition()          const {return m_position;}
+        
+        /// get reconstructed direction
+        Vector getDirection()        const {return m_direction;}
+        
+        /// get energy obtained by profile fitting
+        double getFitEnergy()	  const {return m_fitEnergy;}
+        
+        /// get chi square of profile fitting
+        double getProfChisq()	  const {return m_ProfChisq;}
+        
+        /// get alpha parameter of profile fit
+        double getCsiAlpha()	  const {return m_CsiAlpha;}
+        
+        /// get lambda parameter of profile fit
+        double getCsiLambda()	  const {return m_CsiLambda;}
+        
+        /// get position of shower start
+        double getCsiStart()	  const {return m_start;}
+        
+        /// write some of CalCluster data to the ASCII output file
+        /// for debugging purposes
+        void writeOut(MsgStream& stream) const;
+        
+    protected:
+        
+        ///reset CalCluster data
+        virtual void ini();
+        
+    private:
+        
+        //! Total measured energy in the calorimeter
+        double m_energySum;
+        
+        /// Energy corrected for leakage using correlation method
+        /// ( for E> several GeV)
+        double m_leakEnergy;
+        
+        //! corrected energy not used ( yet )
+        double m_energyCorrected;
+        
+        //! Energy per layer in MeV
+        std::vector<double> m_eneLayer;
+        
+        //! Barycenter position in each layer
+        std::vector<Vector> m_pLayer;
+        
+        //! RMS of energy deposition in each layer
+        std::vector<Vector> m_rmsLayer;
+        
+        //! RMS of longitudinal position measurement
+        double m_rmslong;
+        
+        //! RMS of transverse position measurement
+        double m_rmstrans;
+        
+        //! Transvers offset of calorimeter position measurement
+        double m_transvOffset;
+        
+        //! fitted energy ( for E>10 GeV)
+        double m_fitEnergy;
+        
+        //! Chisquare of the fit ( not a real Chisquare)
+        double m_ProfChisq;
+        
+        //! Alpha parameter used in the fit
+        double m_CsiAlpha;
+        
+        //! Lambda parameter used in the fit 
+        double m_CsiLambda;
+        
+        //! Fitted starting point of the shower (physical meaning is not clear)
+        double m_start;
+        
+        /// reconstructed position 
+        Point m_position;
+        
+        /// reconstructed direction
+        Vector m_direction;
+    };
 
-	//! constructor
-	CalCluster(double e, Point p);
-
-	//! Destructor
-	~CalCluster() {}
-	void initialize(double eleak, std::vector<double> eneLayer, std::vector<Vector> pLayer,
-					std::vector<Vector> rmsLayer, double rms_long, double rms_trans,
-		            Vector caldir, double calTransvOffset)
-	{
-	m_eneLayer = eneLayer;
-	m_leakEnergy = eleak;
-	m_pLayer = pLayer;
-	m_rmsLayer = rmsLayer;	
-	m_rmslong = rms_long;
-	m_rmstrans= rms_trans;
-	m_direction = caldir;
-	m_transvOffset = calTransvOffset;
-	}
-
-    void initProfile(double fit_energy, double ki2, double fit_start,
-		double fit_alpha, double fit_lambda)
-	{
-	m_fitEnergy = fit_energy;
-	m_ProfChisq = ki2;
-	m_CsiAlpha = fit_alpha;
-	m_CsiLambda = fit_lambda;
-	m_start  = fit_start;
-	}
-
-
-	// access
-	double getEnergySum()        const {return m_energySum;}
-	double getEnergyLeak()       const {return m_leakEnergy;}
-	double getEnergyCorrected()  const {return m_energyCorrected;}
-	double getEneLayer(int i) const {return m_eneLayer[i];}
-	const Vector& getPosLayer(int i) const {return m_pLayer[i];}
-	const std::vector<double>& getEneLayer() const {return m_eneLayer;}
-	const std::vector<Vector>& getPosLayer() const {return m_pLayer;}
-	const std::vector<Vector>& getRmsLayer() const {return m_rmsLayer;}
-	double getRmsLong()		  const {return m_rmslong;}
-	double getRmsTrans()	  const {return m_rmstrans;}
-    double getTransvOffset()  const {return m_transvOffset;}
-
-	Point getPosition()          const {return m_position;}
-	Vector getDirection()        const {return m_direction;}
-	double getFitEnergy()	  const {return m_fitEnergy;}
-	double getProfChisq()	  const {return m_ProfChisq;}
-	double getCsiAlpha()	  const {return m_CsiAlpha;}
-	double getCsiLambda()	  const {return m_CsiLambda;}
-	double getCsiStart()	  const {return m_start;}
-	// operations
-	void writeOut(MsgStream& stream) const;
-
-protected:
-
-	virtual void ini();
-
-private:
-
-	//! Total measured energy in the calorimeter
-	double m_energySum;
-	//! Leakage corrected energy using correlation method ( for E> several GeV)
-	double m_leakEnergy;
-	//! corrected energy not used ( yet )
-	double m_energyCorrected;
-	//! Energy per layer in MeV
-	std::vector<double> m_eneLayer;
-	//! Barycenter position in each layer
-	std::vector<Vector> m_pLayer;
-	//! RMS of energy deposition in each layer
-	std::vector<Vector> m_rmsLayer;
-	//! RMS of longitudinal position measurement
-	double m_rmslong;
-	//! RMS of transverse position measurement
-	double m_rmstrans;
-    //! Transvers offset of calorimeter position measurement
-    double m_transvOffset;
-	
-	//! fitted energy ( for E>10 GeV)
-	double m_fitEnergy;
-	//! Chisquare of the fit ( not a real Chisquare)
-	double m_ProfChisq;
-	//! Alpha parameter used in the fit
-	double m_CsiAlpha;
-	//! Lambda parameter used in the fit 
-	double m_CsiLambda;
-	//! Fitted starting point of the shower (physical meaning is not clear)
-	double m_start;
-
-	Point m_position;
-	Vector m_direction;
-};
-
-
-//! High level data for the calorimeter
-/*! Transient storage of the results of the reconstruction performed
- *  in CalClustersAlg. It contains the the list ofall the clusters in the
- *  calorimeter. 
- *  
- * \warning there is no clustering in the calorimeter up to now. There is
- *  just one Cluster stored containing the information for the whole event
- *
- *  \author Jose Angel Hernando
- *
- * \b Revisions:
- * - 02/29/00     JAH    first implementation
- */
-
-//#####################################
-class CalClusterCol : public DataObject, public std::vector<CalCluster*>
-//#####################################
-{
-public:
-
-	CalClusterCol() { clear();}
-	~CalClusterCol() { delClusters();}
-
-
-	// GAUDI members to be use by the converters
-	static const CLID& classID() {return CLID_CalClusterCol;}
-	virtual const CLID& clID() const {return classID();}
-	
-	
-	void add(CalCluster* cl) {push_back(cl);}
-
-	// access
-	int num()                  const {return size();}
-	CalCluster* getCluster(int i) const {return operator[](i);}
-
-	//operations
-	void delClusters();
-
-	virtual void writeOut(MsgStream& stream) const;
-
-protected:
-
-	virtual void ini();
-
-};
+    
+    /*! 
+    * @class CalClusterCol
+    *
+    *  TDS class  to store the results of the reconstruction performed
+    *  in CalClustersAlg. It inherits from DataObject
+    * (to be a part of Gaudi TDS) and from std::vector of pointers
+    * to CalCluster objects. Some methods just rename the std::vector
+    * methods with the same functionality for backward compartibility.
+    *
+    *
+    * @warning there is no clustering in the calorimeter up to now. There is
+    *  just one CalCluster stored, containing the information for the whole
+    *  event
+    *
+    * @author Jose Angel Hernando
+    * @author Alexandre Chekhtman
+    *
+    * @todo replace this class by typedef to ObjectVector, make corresponding
+    *       changes in CalClustersAlg
+    */
+    
+    class CalClusterCol : public DataObject, public std::vector<CalCluster*>
+    {
+    public:
+        
+        CalClusterCol() { clear();}
+        
+        /// destructor - deleting the clusters pointed
+        /// by the vector elements
+        ~CalClusterCol() { delClusters();}
+        
+        
+        // GAUDI members to be use by the converters
+        static const CLID& classID() {return CLID_CalClusterCol;}
+        virtual const CLID& clID() const {return classID();}
+        
+        /// add new cluster
+        void add(CalCluster* cl) {push_back(cl);}
+        
+        /// get the number of clusters in collection
+        int num()                  const {return size();}
+        
+        /// get pointer to the cluster with given number 
+        CalCluster* getCluster(int i) const {return operator[](i);}
+        
+        /// delete all clusters pointed by the vector elements
+        void delClusters();
+        
+        /// write information for all clusters to the ascii file 
+        /// for debugging purposes
+        virtual void writeOut(MsgStream& stream) const;
+        
+    protected:
+        
+        /// does the same function as clear() 
+        virtual void ini();
+        
+    };
 
 }
 
--- Event/src/Recon/CalRecon/CalCluster.cxx
+++ Event/src/Recon/CalRecon/CalCluster.cxx
@@ -1,79 +1,128 @@
+// File and Version information:
+// $Header$
+//
+//  Implementation file of CalCluster and CalClusterCol classes
+//  
+// Authors:
+//
+//    Alexandre Chekhtman
+//    Regis Terrier
+//    Jose Angel Hernando
+//
+//
 
 #include "Event/Recon/CalRecon/CalCluster.h"
 
 using namespace Event;
 
-//----------------- CalCluster ------------------
-
-//################################################
 CalCluster::CalCluster(double e,Point p)
-//################################################
+
+// Purpose: constructor with parameters
+// 
+// Inputs:
+//   e - energy sum
+//   p - cluster position
+//
 { 
-	int nl = 8;
-	m_eneLayer.resize(nl);	
-	m_pLayer.resize(nl);	
-	ini();
-	m_energySum = e;
-	m_energyCorrected = m_energySum;
-	m_position = p;
+    int nl = 8;
+
+    // set vectors length to the number of layers
+    m_eneLayer.resize(nl);	
+    m_pLayer.resize(nl);	
+
+    // reset all data members to 0
+    ini();
+
+    //set energy sum
+    m_energySum = e;
+
+    // temporary this data member is set to raw energy sum
+    m_energyCorrected = m_energySum;
+
+    //set position
+    m_position = p;
 }
-//################################################
+
+
+
 void CalCluster::writeOut(MsgStream& stream) const
-//################################################
+
+// Purpose: provide ascii output of some data members for
+//          debugging purposes
+// Input:
+//        stream - Gaudi message stream
 {
-#if 1 // THB: enable this after it is modified to write to the log object, and the values are all defined
-	stream << "Energy " << m_energySum << " Corrected " << m_energyCorrected;
-	stream << " " << getPosition().x() << " " << getPosition().y() << " " << getPosition().z();
-	stream << " " << getDirection().x() << " " << getDirection().y() << " " << getDirection().z();
-	stream << endreq;
-#endif
+    stream << "Energy " << m_energySum 
+        << " Corrected " << m_energyCorrected;
+    stream << " " << getPosition().x() 
+        << " " << getPosition().y() 
+        << " " << getPosition().z();
+    stream << " " << getDirection().x() 
+        << " " << getDirection().y() 
+        << " " << getDirection().z();
+    stream << endreq;
 }
-//------------- private --------------------------
-//################################################
+
+
+
 void CalCluster::ini()
-//################################################
+// Purpose: reset all data members to 0
+//
+
 {
-	m_energySum       = 0.;
-	m_energyCorrected = 0.;
-
-	m_position = Point(0.,0.,0.);
-	m_direction = Vector(0.,0.,0);
-	int nLayers = m_eneLayer.size();
-	for(int i = 0; i<nLayers; i++){
-		m_eneLayer[i]=0.;
-		m_pLayer[i]=Vector(0.,0.,0.);
-	}
+    m_energySum       = 0.;
+    m_energyCorrected = 0.;
+    
+    m_position = Point(0.,0.,0.);
+    m_direction = Vector(0.,0.,0);
+    int nLayers = m_eneLayer.size();
+    for(int i = 0; i<nLayers; i++){
+        m_eneLayer[i]=0.;
+        m_pLayer[i]=Vector(0.,0.,0.);
+    }
 }
-//----------------- CsIClusterList -----------------
 
-//################################################
 void CalClusterCol::delClusters()
-//################################################
+
+//Purpose: delete all CalCluster object from memory
+
 {
-	int nClusters = num();
-	for (int icl = 0; icl < nClusters; icl++) {
-		delete operator[](icl);
-	}
-        clear();
+    int nClusters = num();
+    for (int icl = 0; icl < nClusters; icl++) {
+        delete operator[](icl);
+    }
+    clear();
 }
 
-//------------ private ---------------------------
-//################################################
 void CalClusterCol::ini()
-//################################################
+
+//Purpose:  delete all pointers to clusters
+// from collection 
+
 {
-clear();
+    clear();
 }
-//################################################
+
+
 void CalClusterCol::writeOut(MsgStream& stream) const
-//################################################
+
+// Purpose: provide symbolic output of some data members
+//          of all clusters in collection for debugging purposes
+//
+// Input:
+//        stream - Gaudi message stream
 {
-#if 1 // fix this to write to the log file for debug purposes
+    
+    // if there is no clusters - return
     if (size()<=0) return;
-	
-	stream << " --- CalClusterCol  --- " << size() << endreq;
-	for (int i = 0; i < size();i++) {
-		(operator[](i))->writeOut(stream);
-	}
-#endif
+    
+    stream << " --- CalClusterCol  --- " << size() << endreq;
+
+    // loop over all clusters
+    for (int i = 0; i < size();i++) {
+        
+        // call the writeOut() method for each cluster
+        (operator[](i))->writeOut(stream);
+    }
+    
 }
