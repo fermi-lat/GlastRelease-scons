76b2bbe0e16b4d1ebfd4508603f6a5cbd333d45e
chehtman <>
2002-06-13 21:47:14 +0000

    New load and dll files, taking G4Generator as an example

--- CalRecon/src/Dll/CalRecon_dll.cpp
+++ CalRecon/src/Dll/CalRecon_dll.cpp
@@ -1,43 +1,9 @@
-//====================================================================
-//  GlastSvc_dll.cpp
-//--------------------------------------------------------------------
-//
-//  Package    : GlastSvc
-//
-//  Description: Implementation of DllMain routine.
-//               The DLL initialisation must be done seperately for 
-//               each DLL. 
-//
-//  Author     : H. Gillespie
-//  Created    : 1 Aug 00
-//  Changes    : 
-//               Version copied from Glast, and addopted for GlastSvc
-//
-//====================================================================
+/** @file CalRecon_dll.cpp
+*
+* @brief Declaration of CalRecon DLL 
+*
+* $Header$
+*/
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
-#include <iostream>
-void GaudiDll::initialize(void*) 
-{
-}
-
-void GaudiDll::finalize(void*) 
-{
-}
-extern void CalRecon_load();
-#include "GaudiKernel/FactoryTable.h"
-
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-  static bool first = true;
-  if ( first ) {  // Don't call for UNIX
-    CalRecon_load();
-    first = false;
-  }
-  return FactoryTable::instance()->getEntries();
-} 
-
-void FATAL(const char * msg) {
-    std::cerr << "Stupid error from Calrecon DLL: " << msg << std::endl;
-}
-
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(CalRecon)
--- CalRecon/src/Dll/CalRecon_load.cpp
+++ CalRecon/src/Dll/CalRecon_load.cpp
@@ -1,41 +1,19 @@
-// $Header$
-//====================================================================
-//  GlastSvc_load.cpp
-//--------------------------------------------------------------------
-//
-//  Package    : Gaudi/System
-//
-//  Description: Implementation of <Package>_load routine.
-//               This routine is needed for forcing the linker
-//               to load all the components of the library. 
-//
-//====================================================================
+/** 
+* @file CalRecon_load.cpp
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+* $Header$
+*/
 
-#include "GaudiKernel/ICnvFactory.h"
-#include "GaudiKernel/ISvcFactory.h"
-#include "GaudiKernel/IAlgFactory.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
 
-
-#define DLL_DECL_SERVICE(x)    extern const ISvcFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_CONVERTER(x)  extern const ICnvFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_ALGORITHM(x)  extern const IAlgFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_OBJECT(x)     extern const IFactory& x##Factory; x##Factory.addRef();
-
-//! Load all  services: 
-void CalRecon_load() {
-    
-//    DLL_DECL_SERVICE( CalGeometrySvc );
-    
-//    DLL_DECL_ALGORITHM( CalRecLogsAlg );
-//    DLL_DECL_ALGORITHM( CalDigiLogsAlg );
-//     DLL_DECL_ALGORITHM( CalIRFAlg );
-    DLL_DECL_ALGORITHM( CalXtalRecAlg );
-    DLL_DECL_ALGORITHM( CalClustersAlg );
-//    DLL_DECL_ALGORITHM( CalNtupleAlg );
-    DLL_DECL_ALGORITHM( CalDisplay );
+DECLARE_FACTORY_ENTRIES(CalRecon) {
+    DECLARE_ALGORITHM( CalXtalRecAlg);
+    DECLARE_ALGORITHM( CalClustersAlg);
+    DECLARE_ALGORITHM( CalDisplay);
 } 
 
-extern "C" void CalRecon_loadRef()    {
-    CalRecon_load();
-}
+
+
 
