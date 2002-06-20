6543c2e07bd6ab1f96c8bc6932cce9df5adc88b2
burnett <>
2002-06-20 14:13:23 +0000

    remove arbitrary and temporary cut on the number of McParticles, to fix memory leak. Could not just delete the remainder, due to use of link from McIntegrating hit objects

--- G4Generator/src/McParticleManager.cxx
+++ G4Generator/src/McParticleManager.cxx
@@ -57,9 +57,9 @@ void McParticleManager::save()
   // fill the McParticleCol with McParticles
   std::map <unsigned int, Event::McParticle*>::iterator it;
   
-  // note limit on size, temporarily wired in
-  for(it=m_particles.begin();it != m_particles.end() && pcol->size()<10 ; it++)
+  for(it=m_particles.begin();it != m_particles.end() ; it++){
     pcol->push_back(it->second);
+  }
 }
 
 
