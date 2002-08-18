8a41df89d9e7c07118c102be87de12b514f1dea1
richard <>
2002-08-18 16:09:54 +0000

    updates for taper models as Tools

--- CalDigi/src/Dll/CalDigi_load.cxx
+++ CalDigi/src/Dll/CalDigi_load.cxx
@@ -7,9 +7,15 @@
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IToolFactory.h"
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
+
 
 DECLARE_FACTORY_ENTRIES(CalDigi) {
     DECLARE_ALGORITHM( CalDigiAlg );
+    DLL_DECL_TOOL( LinearTaper   );
+    DLL_DECL_TOOL( OnePlusExpTaper   );
 
 } 
 
