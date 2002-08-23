d457078c1102d3d84f0dbf891acc822a9b8cda06
srobinsn <>
2002-08-23 22:15:36 +0000

    Julian date structure removed from TimeStamp - that functionality will be put into the astro package.

--- Event/src/test/testmain.cxx
+++ Event/src/test/testmain.cxx
@@ -2,7 +2,5 @@
 #include "geometry/Vector.h"
 #include "Event/Utilities/TimeStamp.h"
 int main(){
-    TimeStamp ghi(2009,12,19,11);
-    std::cout << ghi << std::endl;
-    return 0;
+  return 0;
 }
