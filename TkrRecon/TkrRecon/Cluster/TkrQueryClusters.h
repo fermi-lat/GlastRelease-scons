a7acf76dea61c6e2df836ad5e6b2e606b5309d1c
lsrea <>
2002-08-31 17:51:45 +0000

    Documentation and general cleanup Remove TkrAxis: use idents::GlastAxis instead Remove TkrDetGeo: no longer needed TkrGeometrySvc: Remove unnecessary getDoubleStripPosition()                 Add reverseLayerNumber() to replace ilayer()                 TkrAxis->GlastAxis, remove refs to TkrDetGeo TkrBadStripsSvc: TkrAxis->GlastAxis, remove refs to TkrDetGeo test_TkrRecon: add a bit of output

--- TkrRecon/TkrRecon/Cluster/TkrQueryClusters.h
+++ TkrRecon/TkrRecon/Cluster/TkrQueryClusters.h
@@ -14,7 +14,9 @@
 * @brief Contains methods that operate on the clusters and return information.
 *
 * Only one of the methods in this class is currently being used, but I'm keeping the others
-* they would be tedious to re-code.
+* since they would be tedious to re-code.
+*
+* @authors Bill Atwood, Leon Rochester
 *
 * $Header$
 */
@@ -26,24 +28,24 @@ public:
     TkrQueryClusters(Event::TkrClusterCol* pClus):m_pClus(pClus) {};
     ~TkrQueryClusters() {};
 
-	/// returns the mean space point in for a given view and plane
-    Point meanHit(Event::TkrCluster::view v, int iplane);
-	/** returns the mean space point for a given plane, view, within "inDistance" of a point Pini
+	/// returns the mean space point in for a given view and layer
+    Point meanHit(Event::TkrCluster::view v, int layer);
+	/** returns the mean space point for a given layer, view, within "inDistance" of a point Pini
 	*  in the measurement view, and within one tower in the other view.
 	*/
-    Point meanHitInside(Event::TkrCluster::view v, int iplane, double inDistance, Point Pini);
+    Point meanHitInside(Event::TkrCluster::view v, int layer, double inDistance, Point Pini);
 	/** returns the nearest point outside of "inDistance" of a point "Pini" in the measured view, 
 	*  within one tower in the other view, and a ref. to the id
 	*/
-    Point nearestHitOutside(Event::TkrCluster::view v, int iplane, double inDistance, 
+    Point nearestHitOutside(Event::TkrCluster::view v, int layer, double inDistance, 
 		Point Pini, int& id);
 	
 	/// Finds the number of clusters with measured distances inside a square of side 2*inDistance of a point
-    int numberOfHitsNear( int iPlane, double inDistance, Point& x0);
+    int numberOfHitsNear( int layer, double inDistance, Point& x0);
 	/// Finds the number of clusters with measured distances inside a rectangle of side 2*dX by 2*dY of a point
-    int numberOfHitsNear( int iPlane, double dX, double dY, Point& x0);
+    int numberOfHitsNear( int layer, double dX, double dY, Point& x0);
     /// Finds the number of clusters within "inDistance" of a point and within one tower.
-    int numberOfHitsNear( Event::TkrCluster::view v, int iPlane, double inDistance, Point& x0);
+    int numberOfHitsNear( Event::TkrCluster::view v, int layer, double inDistance, Point& x0);
     
 	/// gets filled with towerPitch 	
     static double s_towerPitch;
