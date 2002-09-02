a588e415419bce65f209bb0800ef955383f26ecf
usher <>
2002-09-02 23:08:11 +0000

    fixing documentation slightly

--- TkrRecon/src/PatRec/Combo/ComboFindTrackTool.h
+++ TkrRecon/src/PatRec/Combo/ComboFindTrackTool.h
@@ -2,7 +2,7 @@
  * @class ComboFindTrackTool
  *
  * @brief Implements a Gaudi Tool for find track candidates. This particular tool uses the 
- *        "Combo" method, described in detail in TkrComboPatRec.h, which is very similar to 
+ *        "Combo" method, described in detail in TkrComboPatRec, which is very similar to 
  *        the method employed by Glast up to the time of the PDR.
  *
  * @author The Tracking Software Group
