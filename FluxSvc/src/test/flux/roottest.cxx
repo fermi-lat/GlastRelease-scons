ae48351b9de86d4f4f5b9058cbdc03dcbfc9f680
burnett <>
2002-09-12 18:25:46 +0000

    unix mods for linking executables and shareables against static library.

--- FluxSvc/src/test/flux/roottest.cxx
+++ FluxSvc/src/test/flux/roottest.cxx
@@ -4,12 +4,12 @@
 #include "rootplot.h"
 
 
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
     std::vector<char*> arguments;
     arguments.push_back("chime");
     arguments.push_back("-min");
     arguments.push_back("10");
     rootplot abc(arguments);
-    //return 0;
+    return 0;
 }
