71becc7b2e054a6ddae1678b1077eba2d4037077
richard <>
2002-09-23 16:30:40 +0000

    set log error message to FATAL to hopefully flag test as failed

--- CalDigi/src/test/test_CalDigi.cxx
+++ CalDigi/src/test/test_CalDigi.cxx
@@ -73,7 +73,7 @@ StatusCode test_CalDigi::execute()
   SmartDataPtr<Event::CalDigiCol> digiCol(eventSvc(),EventModel::Digi::CalDigiCol );
 
   if ((digiCol == 0) || (digiCol->size()) == 0) {
-    log << MSG::DEBUG << "no/incorrect number calorimeter digis found" << endreq;
+    log << MSG::FATAL << "no/incorrect number calorimeter digis found" << endreq;
     sc = StatusCode::FAILURE;
     return sc;
   }
