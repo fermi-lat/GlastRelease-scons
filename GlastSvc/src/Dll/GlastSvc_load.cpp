d26a15d61c0b8585c40e0a6c0944d6faf1e1468d
kyoung <>
2002-10-04 18:28:13 +0000

    Add GlastRandSvc

--- GlastSvc/src/Dll/GlastSvc_load.cpp
+++ GlastSvc/src/Dll/GlastSvc_load.cpp
@@ -7,11 +7,17 @@
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
+// #include "GaudiKernel/IToolFactory.h"
+
+// #define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 DECLARE_FACTORY_ENTRIES(GlastSvc) {
     DECLARE_SERVICE( GlastDetSvc);
+    DECLARE_SERVICE( GlastRandomSvc);
     DECLARE_SERVICE( GlastEventSelector );
     DECLARE_SERVICE( EventCnvSvc );
+
+//    DLL_DECL_TOOL( RandomAccess );
     
     DECLARE_CONVERTER( EventCnv );
     DECLARE_CONVERTER( MCEventCnv );
