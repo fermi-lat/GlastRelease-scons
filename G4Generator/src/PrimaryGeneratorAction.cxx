16ed71e72e163b7a46cddd8b35a459bea43e5aee
riccardo <>
2002-11-06 18:37:25 +0000

    Added a conversion from Gev/c^2 to AMU for the mass of ions in the PrimaryGeneratorAction class

--- G4Generator/src/PrimaryGeneratorAction.cxx
+++ G4Generator/src/PrimaryGeneratorAction.cxx
@@ -67,8 +67,9 @@ void PrimaryGeneratorAction::init(Event::McParticle* part, IParticlePropertySvc*
   // the position has to be expressed in mm
   // while the energy in MeV
 
+  // note the conversion from mass in Gev/c^2 to AMU in case of the ion.
   if (isIon(hepid))
-    setIon((int)ppty->charge() , (int)ppty->mass());
+    setIon((int)ppty->charge() , (int)ppty->mass()/0.93149);
   else
     setParticle(ppty->particle());
   
