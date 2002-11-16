5b93b393e59ce48398a8c9e552bd1e18cf614388
richard <>
2002-11-16 22:31:29 +0000

    for converter tool

--- CalUtil/src/Dll/CalUtil_load.cxx
+++ CalUtil/src/Dll/CalUtil_load.cxx
@@ -7,11 +7,16 @@
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IToolFactory.h"
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 #define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 
 DECLARE_FACTORY_ENTRIES(CalUtil) {
     DECLARE_SERVICE( CalFailureModeSvc);
+    DLL_DECL_TOOL( LinearConvertAdc   );
 } 
+
 
