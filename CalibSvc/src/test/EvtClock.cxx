1034b2102514de104da214cd8ab60832e4ded459
jrb <>
2003-01-04 00:23:44 +0000

    Add fake event time generator, after LHCb version in DetCondExample

---
+++ CalibSvc/src/test/EvtClock.cxx
@@ -0,0 +1,117 @@
+//$Header$
+#include <stdio.h>
+
+#include "EvtClock.h"
+#include "CalibData/CalibTime.h"
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/IDetDataSvc.h"
+#include "GaudiKernel/Service.h"
+#include "GaudiKernel/MsgStream.h"
+
+
+//  #include "GaudiKernel/TimePoint.h"
+
+/// Instantiation of a static factory to create instances of this algorithm
+static const AlgFactory<EvtClock> Factory;
+const IAlgFactory& EvtClockFactory = Factory;
+
+
+EvtClock::EvtClock( const std::string&  name, 
+		    ISvcLocator*        pSvcLocator )
+  : Algorithm     ( name, pSvcLocator )
+  , m_eventNumber ( 0 )
+  , m_detDataSvc  ( 0 )
+{
+  declareProperty( "startTime",  
+                   m_startTime = facilities::Timestamp().getClibTime());
+  declareProperty( "delayTime",  m_delayTime = 2 );
+}
+
+
+StatusCode EvtClock::initialize() {
+  StatusCode sc;
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Initialize()" << endreq;
+
+  IDataProviderSvc* calibSvc;
+  sc = service("CalibDataSvc", calibSvc, true);
+
+  // Query the IDetDataSvc interface of the calib data service
+  sc = calibSvc->queryInterface(IID_IDetDataSvc, 
+                                (void**) &m_detDataSvc);
+  if ( !sc.isSuccess() ) {
+    log << MSG::ERROR 
+	<< "Could not query IDetDataSvc interface of CalibDataSvc" 
+	<< endreq;
+    return sc;
+  } else {
+    log << MSG::DEBUG 
+	<< "Retrieved IDetDataSvc interface of CalibDataSvc" 
+	<< endreq;
+  }
+
+  // Get properties from the JobOptionsSvc
+  sc = setProperties();
+  if ( !sc.isSuccess() ) {
+    log << MSG::ERROR << "Could not set jobOptions properties" << endreq;
+    return sc;
+  }
+  log << MSG::DEBUG << "Properties were read from jobOptions" << endreq;
+  log << MSG::INFO << "Time of first event: (seconds since 1970) "
+      << m_startTime 
+      << std::dec << ")" 
+      << endreq; 
+  log << MSG::INFO << "Time between events (seconds): "
+      << m_delayTime 
+      << endreq;
+  return StatusCode::SUCCESS;
+
+}
+
+
+StatusCode EvtClock::execute( ) {
+
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO 
+      << "------------- NEW EVENT! -------------------------------------------"
+      << endreq;
+  log << MSG::INFO << "Execute()" << endreq;
+
+  // Increment the event counter
+  m_eventNumber++;
+  log << MSG::INFO << "EvtClock Event number: " << m_eventNumber << endreq;
+
+  // Set the event time
+  facilities::Timestamp time = i_evtTime();
+  log << MSG::INFO << "Event time: "
+      << time.getString()
+      << " Julian day number "
+      << time.getJulian()
+      << endreq; 
+  m_detDataSvc->setEventTime(CalibData::CalibTime(time));
+
+  return StatusCode::SUCCESS;
+}
+
+
+
+StatusCode EvtClock::finalize( ) {
+
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO 
+      << "------------- FINALIZE!! -------------------------------------------"
+      << endreq;
+  log << MSG::INFO << "Total #events: " << m_eventNumber << endreq;
+  
+  return StatusCode::SUCCESS;
+}
+
+
+//longlong EvtClock::i_evtTime( ) {
+facilities::Timestamp EvtClock::i_evtTime( ) {
+
+  return facilities::Timestamp(m_startTime + 
+                               (m_eventNumber - 1) * m_delayTime);
+
+}
---
+++ CalibSvc/src/test/EvtClock.h
@@ -0,0 +1,53 @@
+//$Header$
+#ifndef EVTCLOCK_H
+#define EVTCLOCK_H 1
+
+#include "facilities/Timestamp.h"
+
+// Base class
+#include "GaudiKernel/Algorithm.h"
+
+// Forward declarations
+class IDetDataSvc;
+
+/** @class EvtClock EvtClock.h
+
+    Simple algorithm to set fake event times on the detector data service.
+
+    @author Joanne Bogart, adapted from similar class of Andrea Valassi 
+*/
+
+class EvtClock : public Algorithm {
+
+ public:
+
+  EvtClock( const std::string& name, ISvcLocator* pSvcLocator ); 
+  
+  // Algorithm standard methods
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();
+  
+ private:
+
+  /// Absolute time of current event
+  //  longlong i_evtTime();
+  facilities::Timestamp i_evtTime();
+
+ private:
+
+  /// Current event number
+  long m_eventNumber;
+
+  /// Absolute time of first event (seconds)
+  long m_startTime;
+
+  /// Absolute time spacing between events
+  long m_delayTime;
+
+  /// Handle to the IDetDataSvc interface of the CalibDataSvc
+  IDetDataSvc* m_detDataSvc;
+  
+};
+
+#endif    // EVTCLOCK_H
