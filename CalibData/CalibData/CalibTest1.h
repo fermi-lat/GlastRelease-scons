3309970cda4fa3e3d10f44d22a751fe5fadcd12b
jrb <>
2003-01-15 23:16:20 +0000

    Move some implementations to .cxx file

--- CalibData/CalibData/CalibTest1.h
+++ CalibData/CalibData/CalibTest1.h
@@ -19,30 +19,26 @@ namespace CalibData {
   class CalibTest1 : public CalibBase {
   public:
     CalibTest1(const std::string& name, int value, 
-               const ITime& since, const ITime& till, int serNo = -1) :
-      CalibBase(since, till, serNo), m_name(name), m_value(value) {}
+               const ITime& since, const ITime& till, int serNo = -1);
 
     CalibTest1(const CalibTest1& other);
 
     virtual ~CalibTest1() {}
 
     // Re-implemented from DataObject
-    /// Class ID of this instance
     inline virtual const CLID& clID() const { return classID(); } 
     
-    /// Class ID of this class
     inline static  const CLID& classID() { return CLID_Calib_CalibTest1; };
 
     // Re-implemented from CalibBase
     virtual void    update(CalibTest1& other);
-    std::string getValueName() const {return m_name;}
+    std::string getValueName() const;
     int         getValue() const {return m_value;}
 
   private:
     std::string m_name;
     int         m_value;
   };
-
 }
 
 
