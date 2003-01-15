1cafc1611b7c7bcf6e770d4c583ff8dd84eb9e26
chehtman <>
2003-01-15 00:39:55 +0000

    Gain implementation added

--- CalUtil/src/CalCalib.cxx
+++ CalUtil/src/CalCalib.cxx
@@ -1,5 +1,8 @@
 #include "CalUtil/CalCalibMap.h"
 #include "CalUtil/CalPedCalib.h"
+#include "CalUtil/CalGainCalib.h"
 
 template<> class CalXtalCalib<CalPedCalib,CalPedElement,2>;
 template<> class CalCalibMap<CalPedCalib,CalPedElement,2>;
+template<> class CalXtalCalib<CalGainCalib,CalGainElement,2>;
+template<> class CalCalibMap<CalGainCalib,CalGainElement,2>;
