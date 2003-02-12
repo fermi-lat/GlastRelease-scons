03c6fc76f97c418d01e50be7fdae747976ae3164
richard <>
2003-02-12 03:29:54 +0000

    intermediate checkin for Cluster tool. Still fiddling with internals and for test on linux

--- CalRecon/src/Dll/CalRecon_load.cpp
+++ CalRecon/src/Dll/CalRecon_load.cpp
@@ -7,11 +7,17 @@
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IToolFactory.h"
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 DECLARE_FACTORY_ENTRIES(CalRecon) {
     DECLARE_ALGORITHM( CalXtalRecAlg);
     DECLARE_ALGORITHM( CalClustersAlg);
     DECLARE_ALGORITHM( CalDisplay);
+    DLL_DECL_TOOL( SingleClusterTool );
 } 
 
 
