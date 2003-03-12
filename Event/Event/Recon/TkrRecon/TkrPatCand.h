e350ff0f4a877ad6da6f94664657a58a87d0b148
usher <>
2003-03-12 23:20:33 +0000

    1) Modified TkrPatCand.h to include estimated error on track energy 2) Added two header files to declare Pat Track/Fit Track and Vertex/Fit Track    relational tables

--- Event/Event/Recon/TkrRecon/TkrPatCand.h
+++ Event/Event/Recon/TkrRecon/TkrPatCand.h
@@ -24,13 +24,16 @@ class TkrPatCand: public TkrRecInfo, virtual public ContainedObject
 {    
 public:
     
-    TkrPatCand(int layer, int tower, double energy, double quality, const Ray& testRay);
+    TkrPatCand(int layer, int tower, double energy, double energyErr, double quality, const Ray& testRay);
+    TkrPatCand(int layer, int tower, double energy, double quality, const Ray& testRay) 
+                    {TkrPatCand(layer,tower,energy,1.,quality,testRay);}
     ~TkrPatCand();
 
     /// Define the TkrRecInfo routines
 
     double        getQuality()                       const {return m_quality;};
     double        getEnergy(TrackEnd end = Start)    const ;
+    double        getEnergyErr(TrackEnd end = Start) const ;
     int           getLayer(TrackEnd end = Start)     const ;
     int           getTower(TrackEnd end = Start)     const ;
     Point         getPosition(TrackEnd end = Start)  const ;
@@ -49,7 +52,8 @@ public:
     void addCandHit(TkrPatCandHit candHit);
 
     //Methods to set member variables if need be
-    void setEnergy(double energy) {m_energy = energy;}
+    void setEnergy(double energy)       {m_energy = energy;}
+    void setEnergyErr(double energyErr) {m_energyErr = energyErr;}
 
     //Access to some information regarding hits (if they exist)
     int              lastLayer();
@@ -72,6 +76,7 @@ private:
     Point         m_position;
     Vector        m_direction;
     double        m_energy;
+    double        m_energyErr;
     double        m_quality;
     int           m_firstLayer;
     int           m_itower;    
---
+++ Event/Event/Recon/TkrRecon/TkrVertexTab.h
@@ -0,0 +1,28 @@
+#ifndef __TKRVERTEXTAB_H
+#define __TKRVERTEXTAB_H 1
+
+#include "GaudiKernel/MsgStream.h"
+#include "Event/RelTable/RelTable.h"
+#include "Event/RelTable/Relation.h"
+#include "Event/Recon/TkrRecon/TkrFitTrackBase.h"
+#include "Event/Recon/TkrRecon/TkrVertex.h"
+
+/** 
+* @class TkrVertexTab
+*
+* @brief TkrRecon header file for relational table typedefs
+*
+*
+* @author(s) The Tracking Software Group
+*
+* $Header$
+*/
+
+namespace Event
+{
+    typedef Event::RelTable<Event::TkrVertex, Event::TkrFitTrackBase>              TkrVertexTab;
+    typedef Event::Relation<Event::TkrVertex, Event::TkrFitTrackBase>              TkrVertexRel;
+    typedef ObjectList< Event::Relation<Event::TkrVertex,Event::TkrFitTrackBase> > TkrVertexTabList;
+};
+
+#endif
