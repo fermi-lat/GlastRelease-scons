71f0cc4874ff100a5ab0198efd7cbceeffed68d8
jrb <>
2003-03-22 01:38:22 +0000

    Modifications to support new sigma field for gain and error for mu slope

--- CalibData/CalibData/Cal/Gain.h
+++ CalibData/CalibData/Cal/Gain.h
@@ -8,15 +8,17 @@ namespace CalibData {
 
   class Gain : public RangeBase {
   public:
-    Gain(float gain = 0.0) : m_gain(gain) {}
+    Gain(float gain = 0.0, float sig = 0.0) : m_gain(gain), m_sig(sig) {}
     ~Gain() {}
 
     float getGain() {return m_gain;}
+    float getSig() {return m_sig;}
 
     virtual void update(RangeBase* other);
 
   private:
     float m_gain;
+    float m_sig;
 
   };
 }
--- CalibData/CalibData/Cal/MuSlope.h
+++ CalibData/CalibData/Cal/MuSlope.h
@@ -8,15 +8,18 @@ namespace CalibData {
 
   class MuSlope : public RangeBase {
   public:
-    MuSlope(float slope = 0.0) : m_muSlope(slope) {}
+    MuSlope(float slope = 0.0, float error = 0.0) : m_muSlope(slope),
+    m_error(error) {}
     ~MuSlope() {}
 
     float getSlope() {return m_muSlope;}
+    float getError() {return m_error;}
 
     virtual void update(RangeBase* other);
 
   private:
     float m_muSlope;
+    float m_error;
 
   };
 }
