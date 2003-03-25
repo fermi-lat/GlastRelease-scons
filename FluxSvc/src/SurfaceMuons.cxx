c0951da676009a03d2d48f3b0f6f854a68f0fa44
xchen <>
2003-03-25 21:58:49 +0000

    modify code so that it can generate muons with an analytic energy spectrum

--- FluxSvc/src/SurfaceMuons.cxx
+++ FluxSvc/src/SurfaceMuons.cxx
@@ -56,7 +56,15 @@ public:
     
 private:
 
-    // local function that approximates the spectrum as a function of E*cos(theta)
+  // option to choose which energy spectrum is implemented.
+  // 0 (default): use the spectrum as a function of E*cos(theta)
+  // 1: use an analytic form, see analyticSpectrum()
+  int m_option;
+
+
+  double analyticSpectrum(double time) const;
+
+  // local function that approximates the spectrum as a function of E*cos(theta)
     double spectrum(double e);
 
     /**
@@ -91,19 +99,24 @@ SurfaceMuons::SurfaceMuons(const std::string& paramstring)
 
     m_cosmin = (params.size()>0)? params[0]: 0.0;
     m_cosmax = (params.size()>1)? params[1]: 1.0;
-    m_flux = 70. * 2*M_PI * fabs(pow(m_cosmax,3) - pow(m_cosmin,3))/3;
 
-    // create integral table of the flux function, as a map of
-    // energy and e*flux(e), with logarithmic energies
-    int n=100;
-    double  emin=1;
+    m_option = (int) (params.size()>2 ? params[2]: 0.);
+
+    if(m_option == 0) {
+      m_flux = 70. * 2*M_PI * fabs(pow(m_cosmax,3) - pow(m_cosmin,3))/3;
+
+      // create integral table of the flux function, as a map of
+      // energy and e*flux(e), with logarithmic energies
+      int n=100;
+      double  emin=1;
         
-    for( int i=0; i< n; ++i){
+      for( int i=0; i< n; ++i){
         double 
-            e = pow(10, 0.025*i),
-            f= e*spectrum(e);
+	  e = pow(10, 0.025*i),
+	  f= e*spectrum(e);
         
         m_ispec[m_total +=f] =e;
+      }
     }
 }
 
@@ -121,13 +134,15 @@ const char* SurfaceMuons::particleName()const
 static inline double cube(double x){return x*x*x;}
 
 /// sample a single particle energy from the spectrum: assume called first
-double SurfaceMuons::energy( double )
+double SurfaceMuons::energy( double time )
 {
     using namespace std;
     // first choose the angle for the dir function, assuming cos**2 distribution
     static double third=1.0/3.0;
     double r = RandFlat::shoot();
     m_costh = pow(r*cube(m_cosmax)+(1-r)*cube(m_cosmin), third);
+
+    if(m_option == 1) return analyticSpectrum(time);
 
     // select an energy*costh from the distribution
 
@@ -168,4 +183,48 @@ double SurfaceMuons::spectrum(double ecth)
 
 
     return pow(ecth, -2.71)*atmos*cutoff;
+}
+
+double SurfaceMuons::analyticSpectrum(double time) const 
+{
+  // analytic form is based on following energy spectrum
+  //  0.003 flat (0.2 GeV < E < 1. GeV)
+  //  0.003 * E^(-1.1) (1 GeV < E < 4 GeV)
+  //  0.006 * E^(-1.6) (4 GeV < E < 10 GeV)
+  //  0.038 * E^(-2.6) (10 GeV < E < 200 GeV)
+  // only generated events with energy from 0.2 GeV to 200 GeV
+
+  // ratio used later in determining in which energy range the event is going
+  // to be generated
+  double norm = 0.0024 + 0.00388 + 0.00184 + 0.000592;
+  double ratio[] = {0.0024/norm, 0.00388/norm, 0.00184/norm, 0.000592/norm};
+  double energy;
+
+  static HepRandom* randomEng = HepRandom::getTheGenerator();
+
+  // determine the energy range of the event to be generated
+  float range = randomEng->flat();
+
+  // use transformation method to generate random numbers
+  float rand = randomEng->flat();
+
+  if(range <= ratio[0]) {  // 0.2 GeV < E < 1 GeV
+    energy = 0.2 + 0.8 * rand;
+  }
+  else if(range <= ratio[0] + ratio[1]) {  // 1 GeV < E < 4 GeV
+    static double normE0 = 1. - pow(4., -0.1);
+    energy = pow(1. - normE0 * rand, -10.);
+  }
+  else if(range <= ratio[0] + ratio[1] + ratio[2]) {  // 4 GeV < E < 10 GeV
+    static double normE1 = pow(4., -0.6) - pow(10., -0.6);
+    static double temp = pow(4., -0.6);
+    energy = pow(temp - normE1 * rand, -5./3.);
+  }
+  else { // 10 GeV < E < 200 GeV
+    static double normE2 = pow(10., -1.6) - pow(200., -1.6);
+    static double temp = pow(10., -1.6);
+    energy = pow(temp - normE2 * rand, -1./1.6);
+  }
+
+  return energy;
 }
