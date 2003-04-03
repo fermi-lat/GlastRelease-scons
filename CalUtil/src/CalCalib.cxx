c98d17fc8da09be8dfbeca5ec272e9d163f44cf5
chehtman <>
2003-04-03 21:18:35 +0000

    CalibSvc is used by CalRecon to get calibration data - old calibration classes aren't needed anymore

--- CalUtil/src/CalCalib.cxx
+++
@@ -1,8 +0,0 @@
-#include "CalUtil/CalCalibMap.h"
-#include "CalUtil/CalPedCalib.h"
-#include "CalUtil/CalGainCalib.h"
-
-template<> class CalXtalCalib<CalPedCalib,CalPedElement,2>;
-template<> class CalCalibMap<CalPedCalib,CalPedElement,2>;
-template<> class CalXtalCalib<CalGainCalib,CalGainElement,2>;
-template<> class CalCalibMap<CalGainCalib,CalGainElement,2>;
