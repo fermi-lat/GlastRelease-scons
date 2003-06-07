ee7f3b0665dd95a09289b12d4f64926094f39d2f
berrie <>
2003-06-07 18:08:24 +0000

    Major changes in LastLayer tool. Coefficients called in xml. Iteration now done in the code. Also a status code as follows:
    eLeak=-1 not enough energy in the CAL available to use this method.
    
    eLeak=-2 there is no tracker information. We will later use the CAL information
             in this case and give it a special flag.
    
    eLeak=-3 the reconstructed angle is not in the valid range.
    
    eLeak=-4 the impact parameter on the CAL is such that there is a need for crack
             correction.

--- CalRecon/src/LastLayerCorrTool.cxx
+++ CalRecon/src/LastLayerCorrTool.cxx
@@ -3,6 +3,13 @@
 #include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/ToolFactory.h"
 #include "GlastSvc/GlastDetSvc/IGlastDetSvc.h"
+#include "Event/Recon/TkrRecon/TkrVertex.h"
+#include "Event/TopLevel/EventModel.h"
+#include "GaudiKernel/ObjectVector.h"
+#include "GaudiKernel/SmartDataPtr.h"
+// to access an XML containing Digi parameters file
+#include "xml/IFile.h"
+
 
 static const ToolFactory<LastLayerCorrTool>  s_factory;
 const IToolFactory& LastLayerCorrToolFactory = s_factory;
@@ -12,7 +19,7 @@ LastLayerCorrTool::LastLayerCorrTool( const std::string& type, const std::string
 
     // declare base interface for all consecutive concrete classes
     declareInterface<IEnergyCorr>(this);
-
+    declareProperty ("xmlFile", m_xmlFile="$(CALRECONROOT)/xml/CalLayer.xml");
 };
 
 
@@ -28,7 +35,7 @@ StatusCode LastLayerCorrTool::initialize()
 
     IGlastDetSvc* detSvc;
 
-        
+    IService*   iService = 0;       
     // get pointer to GlastDetSvc
     sc = service("GlastDetSvc", detSvc);
     
@@ -50,7 +57,28 @@ StatusCode LastLayerCorrTool::initialize()
         return StatusCode::FAILURE;
     } else setNLayers(int(value));
    
-        
+ // Read in the parameters from the XML file
+    xml::IFile m_ifile(m_xmlFile.c_str());
+    if (m_ifile.contains("lastlayer","c0") && m_ifile.contains("lastlayer","c1") && m_ifile.contains("lastlayer","c2") && m_ifile.contains("lastlayer","c3") && m_ifile.contains("lastlayer","b0") && m_ifile.contains("lastlayer","b1") ) {
+        m_c0 = m_ifile.getDouble("lastlayer", "c0");
+	    log << MSG::INFO << " value for c0 " << m_c0 << endreq;
+        m_c1 = m_ifile.getDouble("lastlayer", "c1");
+	    log << MSG::INFO << " value for c1 " << m_c1 << endreq;
+        m_c2 = m_ifile.getDouble("lastlayer", "c2");
+	    log << MSG::INFO << " value for c0 " << m_c2 << endreq;
+        m_c3 = m_ifile.getDouble("lastlayer", "c3");
+	    log << MSG::INFO << " value for c3 " << m_c3 << endreq;
+        m_b0 = m_ifile.getDouble("lastlayer", "b0");
+	    log << MSG::INFO << " value for b0 " << m_b0 << endreq;
+        m_b1 = m_ifile.getDouble("lastlayer", "b1");
+	    log << MSG::INFO << " value for b1 " << m_b1 << endreq;
+    }
+    else return StatusCode::FAILURE;
+
+    sc        = serviceLocator()->getService("EventDataSvc", iService);
+    m_dataSvc = dynamic_cast<DataSvc*>(iService);   
+
+     
     return sc;
 }
 
@@ -67,36 +95,108 @@ StatusCode LastLayerCorrTool::doEnergyCorr(double eTotal, Event::CalCluster* clu
 // TDS input: CalCluster
 // TDS output: CalClusters
 
-
 {
-    MsgStream lm(msgSvc(), name());
-    StatusCode sc = StatusCode::SUCCESS;
-    if(eTotal<200.) {
-        setEnergyCorr(0.);
-        return sc;
-    }
-    else
+
+  MsgStream lm(msgSvc(), name());
+  StatusCode sc = StatusCode::SUCCESS;
+  
+  if(eTotal<500.) {
+    setEnergyCorr(-1.);
+    return sc;
+  }
+  else
     {
+
+      Event::TkrVertexCol *tkrRecData =  SmartDataPtr<Event::TkrVertexCol> (m_dataSvc,
+      EventModel::TkrRecon::TkrVertexCol);    
+           
+      // if reconstructed tracker data doesn't exist - put the debugging message
+      if (tkrRecData == 0) {
+	setEnergyCorr(-2);
+	//log << MSG::DEBUG << "No TKR Reconstruction available " << endreq;
+         return sc;
+      }
+      
+      
+      // if data exist and number of tracks not zero 
+      // - get information of track position and direction 
+      else
+	{
+
+	  int rectkr=0;  
+	  int ntracks=0;    
+	  // First get reconstructed direction from tracker
+
+	  ntracks = tkrRecData->size();
+	  //log << MSG::DEBUG << "number of tracks = " << ntracks << endreq;
+	  //cout<<">> number of tracks "<< ntracks <<endl;	  
+	  Vector trackDirection;
+	  Point trackVertex;
+	double slope;
+	double thetarec;//reconstructed theta
+	double phirec;  //reconstructed phi
+	double px;      //projected position on (0,0,0 plane) 
+	double py;
+        
+        if (ntracks > 0) {
+            rectkr++;
+            trackDirection = tkrRecData->front()->getDirection();
+            trackVertex = tkrRecData->front()->getPosition();
+            slope = fabs(trackDirection.z());
+	    thetarec=trackDirection[2];
+	    phirec=atan2(trackDirection[1],trackDirection[0]);
+	    
+	    //find projected positions on the 0,0,0 plane
+	    px = trackVertex[0]+sqrt((-trackVertex[2]/thetarec)*(-trackVertex[2]/thetarec)-trackVertex[2]*trackVertex[2])*cos(phirec); 
+	    py = trackVertex[1]+sqrt((-trackVertex[2]/thetarec)*(-trackVertex[2]/thetarec)-trackVertex[2]*trackVertex[2])*sin(phirec); 
+	    //cout<<px<<"    "<<py<<endl;
+	    //log << MSG::INFO << "track direction = " << thetarec << endreq;
+            
+        } else {
+	  //log << MSG::DEBUG << "No reconstructed tracks " << endreq;
+        }	
+
+	//for now valid up to 5 degrees.. 
+	if (-thetarec < 0.996 ) {setEnergyCorr(-3.);return sc;}
+
+	//make cuts for the 0-5 degree limit. Will later make the (theta,phi) dependence available
+
+	if (fabs(fabs(fabs(fabs(px)-375)-187.5)-187.5)>55 && fabs(fabs(fabs(fabs(py)-375)-187.5)-187.5)>55) {
+
         // Evaluation of energy using correlation method
         // Coefficients fitted using GlastSim.
+	  /*
         double p0 = -1.49 + 1.72*getTrackSlope();
         double p1 = 0.28 + 0.434 * getTrackSlope();
         double p2 = -15.16 + 11.55 * getTrackSlope();
         double p3 = 13.88 - 10.18 * getTrackSlope();
         double lnE = log(eTotal/1000.);
         double funcoef = (p0 + p1 * lnE )/(1+exp(-p2*(lnE - p3)));
-        
-        setEnergyCorr( funcoef * cluster->getEneLayer()[getNLayers()-1]);
-        
-        // Evaluation of energy using correlation with last layer
-       	// coefficients fitted using tbsim and valid for ~1GeV<E<~50GeV
-       	//double slope = 1.111 + 0.557*log(eTotal/1000.);
-        //double intercept = 210. + 112.* log(eTotal/1000.)*log(eTotal/1000.); 
-       	//double e_leak = slope * elast + intercept;
+	  */
+
+        double lnE = log(eTotal/1000.);
+        double funcoef = (m_c0 + m_c1 * lnE )/(1+exp(-m_c2*(lnE - m_c3)));
+	double E1 = eTotal + funcoef* cluster->getEneLayer()[getNLayers()-1];
+	double biascoef = m_b0 + m_b1*log(E1/1000);
+	double E2 = E1/biascoef;
+	funcoef = (m_c0 + m_c1 * log(E2/1000) )/(1-exp(-m_c2*(log(E2/1000) - m_c3)));
+	///first iteration
+	double E3 = eTotal + funcoef* cluster->getEneLayer()[getNLayers()-1];
+	biascoef= m_b0 + m_b1*log(E3/1000);
+	double E4 = E3/biascoef;
+	funcoef = (m_c0 + m_c1 * log(E4/1000) )/(1-exp(-m_c2*(log(E4/1000) - m_c3)));
+	///second iteration.. probably overkill
+	setEnergyCorr( E4);
+	
+        //setEnergyCorr( funcoef * cluster->getEneLayer()[getNLayers()-1]);
+
+	} else {setEnergyCorr(-4);}
+      }
     }
 
     return sc;
 }
+
 
 StatusCode LastLayerCorrTool::finalize()
 {
