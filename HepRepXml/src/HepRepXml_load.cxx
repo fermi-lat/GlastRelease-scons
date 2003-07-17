f2bcd721f5b2653adf654d0c11c782e70e4f63a2
riccardo <>
2003-07-17 05:02:52 +0000

    Added HepRepXml_dll file.

--- HepRepXml/src/HepRepXml_load.cxx
+++ HepRepXml/src/HepRepXml_load.cxx
@@ -6,13 +6,8 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/IToolFactory.h"
 
-#define DLL_DECL_TOOL(x) extern const IToolFactory& x##Factory; x##Factory.addRef();
-
-
 DECLARE_FACTORY_ENTRIES(HepRepXml) {
-  DLL_DECL_TOOL(RegisterXml);
+  DECLARE_TOOL(RegisterXml);
 }
 
-#include "GaudiKernel/LoadFactoryEntries.h"
 
-LOAD_FACTORY_ENTRIES(HepRepXml)
