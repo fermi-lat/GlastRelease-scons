ecdbf89b9120d3d556663ebd43603e15fd98dec9
riccardo <>
2003-07-17 05:01:51 +0000

    Added HepRepCorba_dll file.

--- HepRepCorba/src/HepRepCorba_load.cxx
+++ HepRepCorba/src/HepRepCorba_load.cxx
@@ -6,13 +6,7 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/IToolFactory.h"
 
-#define DLL_DECL_TOOL(x) extern const IToolFactory& x##Factory; x##Factory.addRef();
-
-
 DECLARE_FACTORY_ENTRIES(HepRepCorba) {
-  DLL_DECL_TOOL(RegisterCorba);
+  DECLARE_TOOL(RegisterCorba);
 }
 
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES(HepRepCorba)
