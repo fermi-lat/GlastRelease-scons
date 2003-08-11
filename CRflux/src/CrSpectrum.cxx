004c976802060ae9c52f0605cddb9260db8a4853
mizuno <>
2003-08-11 21:31:45 +0000

    CrSpectrum.cxx is modified to make solar minimum as a default

--- CRflux/src/CrSpectrum.cxx
+++ CRflux/src/CrSpectrum.cxx
@@ -44,7 +44,10 @@ CrSpectrum::CrSpectrum()
   //
 
   // elapsed seconds of 2001-11-01 (solar maximum) from 2000-01-01
-  m_time = (304+365)*86400; 
+  //  m_time = (304+365)*86400; 
+  // elapsed seconds of 2001-11-01 from 2000-01-01 + 5.5year 
+  m_time = (304+365)*86400+5.5*86400*365; // solar minimum
+
   // altitude of the satellite is assumed to be 500km,
   // typical value for low earth orbit
   m_altitude = 500.0; 
