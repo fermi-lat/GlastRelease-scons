db1a18ff4f364a4bdf21d90e68d0eb6cede84305
burnett <>
2003-09-27 18:53:29 +0000

    Implement exception, use to protect against bad energy, by rejecting the event

---
+++ TkrRecon/src/Utilities/TkrException.h
@@ -0,0 +1,23 @@
+/**
+ * @file TkrException.h
+ *
+
+ * $Header$
+ *
+*/
+
+#ifndef __TkrException_H
+#define __TkrException_H
+
+    /** @class TkrException 
+         @brief hold a string
+         */
+    class TkrException : public std::exception{
+    public: 
+        TkrException(std::string error):m_what(error){}
+	~TkrException() throw() {;}
+        virtual const char *what( ) const  throw() { return m_what.c_str();} 
+        std::string m_what;
+    };
+
+#endif
