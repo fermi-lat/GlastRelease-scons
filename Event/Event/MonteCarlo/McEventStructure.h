941f6101debc852d281946f84e230f4685bf9bca
usher <>
2004-02-18 18:54:28 +0000

    Add the new McRelTableDefs.h file which defines the various Mc relational tables to be used for the extended Monte Carlo information. Remove the now unecessary McSiLayerHit.h and update McEventStructure to reflect the changes

--- Event/Event/MonteCarlo/McEventStructure.h
+++ Event/Event/MonteCarlo/McEventStructure.h
@@ -1,20 +1,31 @@
 /**
  * @class McEventStructure
  *
- * @brief Represents a Monte Carlo hit in a tracker layer
+ * @brief This object attempts to use the Monte Carlo information to organize and
+ *        classify a single event in LAT. To that end, it identifies the primary 
+ *        particle and its interactions within the LAT. After this it identifies any
+ *        direct daughters of the primary particle WHICH RESULT IN MCPOSITIONHITs in
+ *        the tracker, and then identifies any "associated" (daughters of daughters) 
+ *        which also leave McPositionHits in the tracker. 
  *
- * @author The Tracking Software Group
+ *        The type of event, charged or gamma, the type of interaction in the LAT (also
+ *        within the tracker) can be determined from the event classification bit string.
+ *
+ * @author Tracy Usher
  *
  * $Header$
  */
+
+#ifndef McEventStructure_h
+#define McEventStructure_h
+
 #include "GaudiKernel/IDataProviderSvc.h"
 #include "GaudiKernel/IParticlePropertySvc.h"
 #include "GaudiKernel/ContainedObject.h"
 #include "GaudiKernel/SmartRefVector.h"
 #include "Event/MonteCarlo/McParticle.h"
-
-#ifndef McEventStructure_h
-#define McEventStructure_h
+#include "Event/MonteCarlo/McPositionHit.h"
+#include "Event/RelTable/RelTable.h"
 
 namespace Event {
 
@@ -24,7 +35,7 @@ typedef SmartRef<Event::McParticle>       McParticleRef;
 class McEventStructure : public DataObject
 {
 public:
-    //! Define some bits to help classify the event
+    //! Define bits to help classify the event
     enum ClassificationBits{  
         NOPRIMARY  = 1 ,    //! No primary particle found (can't happen?)
         CHARGED    = 1<<1,  //! Primary particle is charged
@@ -35,31 +46,35 @@ public:
         COMPT      = 1<<6,  //! Secondaries from Compton scatter
         PHOT       = 1<<7,  //! Secondaries from Photoelectric effect (?)
         OTHER      = 1<<8,  //! Secondaries from some other process 
-        TRKCONVERT = 1<<12, //! Secondaries from gamma conversion
-        TRKBREM    = 1<<13, //! Secondaries from Bremstrahlung
-        TRKCOMPT   = 1<<14, //! Secondaries from Compton scatter
-        TRKPHOT    = 1<<15, //! Secondaries from Photoelectric effect (?)
-        TRKOTHER   = 1<<16, //! Secondaries from some other process 
+        TRKCONVERT = 1<<12, //! Secondaries from gamma conversion in tracker
+        TRKBREM    = 1<<13, //! Secondaries from Bremstrahlung in tracker
+        TRKCOMPT   = 1<<14, //! Secondaries from Compton scatter in tracker
+        TRKPHOT    = 1<<15, //! Secondaries from Photoelectric effect in tracker
+        TRKOTHER   = 1<<16, //! Secondaries from some other process in tracker
         RUNBIT     = 1<<24  //! Indicates code was called (does nothing)
     };
 
-    /// Standard Gaudi Tool interface constructor
+    //! Dataobject compliant constructor
     McEventStructure(IDataProviderSvc* dataSvc, IParticlePropertySvc* partPropSvc);
    ~McEventStructure();
 
-    /// Retrieve results
+    //! Retrieve classification bits (see above definitions)
     const unsigned long                     getClassificationBits() const {return m_classification;}
     
+    //! Retrieve reference to the primary particle
     const Event::McParticleRef              getPrimaryParticle()    const {return m_primary;}
 
+    //! Retrieve number and iterators to the daughters of the primary with hits in tracker
     const int                               getNumSecondaries()     const {return m_secondaries.size();}
     Event::McParticleRefVec::const_iterator beginSecondaries()      const {return m_secondaries.begin();}
     Event::McParticleRefVec::const_iterator endSecondaries()        const {return m_secondaries.end();}
 
+    //! Retrieve number and iterators to the particles associated with primary 
     const int                               getNumAssociated()      const {return m_associated.size();}
     Event::McParticleRefVec::const_iterator beginAssociated()       const {return m_associated.begin();}
     Event::McParticleRefVec::const_iterator endAssociated()         const {return m_associated.end();}
 
+    //! Return an McParticle reference vector of all McParticles which leave hits in tracker
     Event::McParticleRefVec                 getTrackVector();
 
 private:
@@ -74,6 +89,13 @@ private:
     Event::McParticleRefVec   m_associated;
 };
 
+/*
+// typedefs for relating McParticles (above) to associated McPositionHits
+typedef Event::RelTable<Event::McParticle, Event::McPositionHit>   McPartToPosHitTab;
+typedef Event::Relation<Event::McParticle, Event::McPositionHit>   McPartToPosHitRel;
+typedef ObjectList<McPartToPosHitRel>                              McPartToPosHitTabList;
+typedef std::vector<Event::McPartToPosHitRel*>                     McPartToPosHitVec;
+*/
 };
 
 #endif
---
+++ Event/Event/MonteCarlo/McRelTableDefs.h
@@ -0,0 +1,70 @@
+/**
+ * @class McRelTableDefs
+ *
+ * @brief This header file serves to define the various relational tables used with 
+ *        the Monte Carlo information
+ *
+ * @author Tracy Usher
+ *
+ * $Header$
+ */
+
+#ifndef McRelTableDefs_h
+#define McRelTableDefs_h
+
+// Necessary Gaudi stuff
+#include "GaudiKernel/SmartRefVector.h"
+#include "Event/RelTable/RelTable.h"
+
+// Start the defintions
+namespace Event {
+
+// Monte Carlo objects with the "truth"
+class McParticle;
+class McPositionHit;
+class McSiLayerHit;
+class McCandTrack;
+
+// Forward declarations for recon information
+class TkrCluster;
+class TkrPatCandHit;
+class TkrPatCand;
+
+// typedefs for relating McParticles to associated McPositionHits
+typedef Event::RelTable<Event::McParticle, Event::McPositionHit>   McPartToPosHitTab;
+typedef Event::Relation<Event::McParticle, Event::McPositionHit>   McPartToPosHitRel;
+typedef ObjectList<McPartToPosHitRel>                              McPartToPosHitTabList;
+typedef std::vector<Event::McPartToPosHitRel*>                     McPartToPosHitVec;
+
+// typedefs for relating TkrClusters to McPositionHits 
+typedef Event::RelTable<Event::TkrCluster, Event::McPositionHit>   ClusMcPosHitTab;
+typedef Event::Relation<Event::TkrCluster, Event::McPositionHit>   ClusMcPosHitRel;
+typedef ObjectList<ClusMcPosHitRel>                                ClusMcPosHitTabList;
+typedef std::vector<Event::ClusMcPosHitRel*>                       ClusMcPosHitVec;
+
+// typedefs for relating McParticles to TkrClusters
+typedef Event::RelTable<Event::McParticle, Event::TkrCluster>      McPartToClusTab;
+typedef Event::Relation<Event::McParticle, Event::TkrCluster>      McPartToClusRel;
+typedef ObjectList<McPartToClusRel>                                McPartToClusTabList;
+typedef std::vector<Event::McPartToClusRel*>                       McPartToClusVec;
+
+// typedefs for relating McParticles to ClusMcPosHitRels
+typedef Event::RelTable<Event::McParticle, ClusMcPosHitRel>        McPartToClusPosHitTab;
+typedef Event::Relation<Event::McParticle, ClusMcPosHitRel>        McPartToClusPosHitRel;
+typedef ObjectList<McPartToClusPosHitRel>                          McPartToClusPosHitTabList;
+typedef std::vector<Event::McPartToClusPosHitRel*>                 McPartToClusPosHitVec;
+
+// typedefs for relating TkrPatCandHits to McParticles
+typedef Event::RelTable<Event::McParticle, Event::TkrPatCandHit>   McPartToTkrCandHitTab;
+typedef Event::Relation<Event::McParticle, Event::TkrPatCandHit>   McPartToTkrCandHitRel;
+typedef ObjectList<McPartToTkrCandHitRel>                          McPartToTkrCandHitTabList;
+typedef std::vector<McPartToTkrCandHitRel*>                        McPartToTkrCandHitVec;
+
+// typedefs for relating TkrPatCands to McParticles
+typedef Event::RelTable<Event::McParticle, Event::TkrPatCand>      McPartToTkrPatCandTab;
+typedef Event::Relation<Event::McParticle, Event::TkrPatCand>      McPartToTkrPatCandRel;
+typedef ObjectList<Event::McPartToTkrPatCandRel>                   McPartToTkrPatCandTabList;
+typedef std::vector<Event::McPartToTkrPatCandRel*>                 McPartToTkrPatCandVec;
+};
+
+#endif
