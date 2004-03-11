c4ba11c3506f3b4c3314044deb01a6f681a32cbe
heather <>
2004-03-11 05:20:29 +0000

    fix up CLIDs for conversion services

--- Event/src/TopLevel/EventModel.cpp
+++ Event/src/TopLevel/EventModel.cpp
@@ -173,18 +173,26 @@ public:
     //! Reconstruction: Tkr class IDs
     const CLID& CLID_TkrRecon           = 3100;
     const CLID& CLID_SiLayers           = 3101;  // Use Tkr instead of Si ?
-    const CLID& CLID_TkrClusterCol      = 3102;  // Is this really Layers, Clusters, RecObjs
-    const CLID& CLID_TkrPatCandCol      = 3103;  // Is this really Layers, Clusters, RecObjs
+    const CLID& CLID_TkrCluster         = 3102;  // Is this really Layers, Clusters, RecObjs
+    const CLID& CLID_TkrClusterCol      = CLID_TkrCluster + CLID_ObjectVector;
+    const CLID& CLID_TkrPatCand         = 3103;
+    const CLID& CLID_TkrPatCandCol      = CLID_TkrPatCand + CLID_ObjectVector;  // Is this really Layers, Clusters, RecObjs
     const CLID& CLID_SiRecObjs          = 3104;  // or rather Layer, Cluster, RecObj objects?
-    const CLID& CLID_TkrFitTrackCol     = 3105;  // or rather Layer, Cluster, RecObj objects?
+    const CLID& CLID_TkrFitTrack        = 3105;
+    const CLID& CLID_TkrFitTrackCol     = CLID_TkrFitTrack + CLID_ObjectVector;  // or rather Layer, Cluster, RecObj objects?
     const CLID& CLID_TkrVertex          = 3106;  
     const CLID& CLID_TkrVertexCol       = CLID_TkrVertex+CLID_ObjectVector;// CLID_TkrVertexCol is ObjectVector of TkrVertices
+    const CLID& CLID_TkrFitTrackBase    = 3107;
+    const CLID& CLID_TkrFitTrackBaseCol = CLID_TkrFitTrackBase + CLID_ObjectVector;
+    const CLID& CLID_TkrPatCandHit      = 3108;
+    const CLID& CLID_TkrPatCandHitCol   = CLID_TkrPatCandHit + CLID_ObjectVector;
 
     //! Reconstruction: Cal class IDs
     const CLID& CLID_CalRecon           = 3200;
     const CLID& CLID_CalXtalRecData     = 3201;
     const CLID& CLID_CalXtalRecCol      = CLID_CalXtalRecData+CLID_ObjectVector;
-    const CLID& CLID_CalClusterCol      = 3202;
+    const CLID& CLID_CalCluster         = 3202;
+    const CLID& CLID_CalClusterCol      = CLID_CalCluster + CLID_ObjectVector;
 
     //! Reconstruction: Acd class IDs
     const CLID& CLID_AcdRecon           = 3300;
