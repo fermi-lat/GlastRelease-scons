e4c194825aa537ce2c3ceba22bd439d1a458ce51
jrb <>
2004-03-24 02:01:32 +0000

    Pure virtual classes (Connection, ResultHandle) for abstract database interaction and derived classes for MySQL database

---
+++ rdbModel/rdbModel/Db/MysqlResults.h
@@ -0,0 +1,35 @@
+// $Header$
+#ifndef RDBMODEL_MYSQLRESULTS_H
+#define RDBMODEL_MYSQLRESULTS_H
+
+#include "rdbModel/Db/ResultHandle.h"
+
+namespace rdbModel{
+
+  /** 
+      Concrete implementation of ResultHandle, to accompany MysqlConnection.
+  */
+  class MysqlResults : virtual public ResultHandle {
+
+    MysqlResults() {};
+    virtual ~MysqlResults() {};
+
+    /// Return number of rows in results
+    int getNRows() const;
+
+    /// Return specified row in results as a string
+    bool getRowString(std::string& row, unsigned int iRow=0);
+
+    /**  Return vector of rows
+         @param rows     to hold returned data
+         @param iRow     starting row
+         @param maxRow   maximum number of rows requested.  0 means "all"
+         @param clear    if true, clear @a rows before storing new data
+         @return         status
+    */
+    bool getRowStrings(std::vector<std::string>& rows, unsigned int iRow=0,
+                       unsigned int maxRow=0, bool clear=true) const;
+
+  };
+}
+#endif
