595d40612ae387e06fed3b21635c0e68db093c4f
frailis <>
2004-04-08 16:09:39 +0000

    First import

---
+++ rdbGui/src/ResultTable.h
@@ -0,0 +1,23 @@
+
+#ifndef RESULTTABLE_H
+#define RESULTTABLE_H
+
+#include "fx.h"
+
+class ResultTable: public FXTable
+{
+  public:
+  
+    ResultTable(FXComposite *p, FXObject* tgt=NULL,FXSelector sel=0,
+                FXuint opts=0,FXint x=0,FXint y=0,FXint w=0,FXint h=0,
+                FXint pl=DEFAULT_MARGIN,FXint pr=DEFAULT_MARGIN,
+                FXint pt=DEFAULT_MARGIN,FXint pb=DEFAULT_MARGIN);
+                
+    void format();  
+    
+  protected:
+    ResultTable();
+};
+
+
+#endif // end of RESULTTABLE_H
