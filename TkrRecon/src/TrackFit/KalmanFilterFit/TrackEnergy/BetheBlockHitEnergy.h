f2ac31ac1a903c8cad91d61e6ea0215d096a2c08
usher <>
2004-04-19 22:51:00 +0000

    These objects implement various schemes for assigning the energy at a given hit

---
+++ TkrRecon/src/TrackFit/KalmanFilterFit/TrackEnergy/BetheBlockHitEnergy.h
@@ -0,0 +1,35 @@
+/**
+ * @class BetheBlockHitEnergy
+ *
+ * @brief Implementation of a class used for assigning energy to a hit during the fit process
+ *        This version implements "standard" Bethe-Block energy loss
+ *
+ * @author Tracy Usher
+ *
+ * $Header$
+ */
+
+#ifndef BetheBlockHitEnergy_h
+#define BetheBlockHitEnergy_h
+
+#include "src/TrackFit/KalmanFilterFit/TrackEnergy/IFitHitEnergy.h"
+#include "src/Track/TkrControl.h"
+
+class BetheBlockHitEnergy : public IFitHitEnergy
+{
+public:
+
+    // Constructor needs the matrices that transform state vector, covariance matrix
+    BetheBlockHitEnergy();
+    ~BetheBlockHitEnergy() {};
+
+    double initialHitEnergy(const Event::TkrPatCandHit& candHit, const double trkEnergy);
+    double updateHitEnergy(const double curEnergy, const double radLen);
+    double getHitEnergy(const double energy) {return energy;}
+
+private:
+    TkrControl* m_control;
+};
+
+
+#endif
