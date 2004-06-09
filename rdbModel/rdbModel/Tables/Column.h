9053e5ff42b5bc33b349e0af01fdaa04d6dce37a
jrb <>
2004-06-09 19:09:04 +0000

    Fix compile bug (why did it ever work?)

--- rdbModel/rdbModel/Tables/Column.h
+++ rdbModel/rdbModel/Tables/Column.h
@@ -21,11 +21,6 @@ namespace rdbModel {
     //    class ColumnSource;      // embedded class, described below
 
   public:
-    Column(Table* myTable=0) : m_myTable(myTable), m_type(0), m_contents(0) {
-      m_default = std::string("");};
-    // Column(Table* myTable=0) : m_myTable(myTable), m_type(0), m_source(0) {};
-    ~Column();
-
     /// Source of value.
     enum FROM {
       FROMdefault = 1,          // enduser can override default, however
@@ -41,6 +36,13 @@ namespace rdbModel {
       CONTENTSserviceName = 1,
       CONTENTSusername 
     };
+
+    Column(Table* myTable=0) : m_myTable(myTable), m_type(0) {
+      m_contents = CONTENTSunspecified;
+      m_default = std::string("");};
+    // Column(Table* myTable=0) : m_myTable(myTable), m_type(0), m_source(0) {};
+    ~Column();
+
 
     const std::string& getName() const {return m_name; };
     const std::string& getComment() const {return m_comment;};
