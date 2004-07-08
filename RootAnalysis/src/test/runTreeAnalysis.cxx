f8a3c96edb02817d9519a04eafe6a3c6c1cd9c4a
kuss <>
2004-07-08 14:26:45 +0000

    more positional parameters added, i.e. 4th is histFileName, 5th is event number

--- RootAnalysis/src/test/runTreeAnalysis.cxx
+++ RootAnalysis/src/test/runTreeAnalysis.cxx
@@ -1,18 +1,28 @@
+/**
+ *
+ * @file runTreeAnalysis
+ *
+ * @brief Simple test program to exercise RootTreeAnalysis
+ *
+ * @author no clue
+ *
+ * $Header$
+ *
+ */
+
 #include "RootTreeAnalysis.h"
 #include "TSystem.h"
 #include <iostream>
 #include <string>
 
-/** @file runTreeAnalysis
-@brief Simple test program to exercise RootTreeAnalysis
-*/
 int main(int argn, char** argc) {
-    
+
 #ifdef WIN32
     gSystem->Load("libTree.dll");
     gSystem->Load("reconRootData.dll");
 #endif
     unsigned int numEvents = 100;
+    if ( argn > 5) numEvents = atoi(argc[5]);
     const char* path = ::getenv("ROOTANALYSISROOT");
     std::string digiFileName(path);
     digiFileName += "/src/test/digi.root";
@@ -23,6 +33,8 @@ int main(int argn, char** argc) {
     if (argn > 1) mcFileName = argc[1];
     if (argn > 2) digiFileName = argc[2];
     if (argn > 3) reconFileName = argc[3];
+    if (argn > 4 && strlen(argc[4]) > 0)
+        r->SetHistFileName(argc[4]);
     std::cout << "Setup for Processing" << std::endl;
     RootTreeAnalysis r(digiFileName.c_str(), reconFileName.c_str(), mcFileName.c_str());
     r.Go(numEvents);
@@ -31,10 +43,3 @@ int main(int argn, char** argc) {
 
     return 0;
 }
-
-
-
-
-
-
-
