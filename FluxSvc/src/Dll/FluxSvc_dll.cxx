bd4210844df312b0e85b8a9cc01f650d9624b854
burnett <>
2004-07-14 21:14:10 +0000

    convert to use proper macro instead of explicit code for getFactoryEntries.

--- FluxSvc/src/Dll/FluxSvc_dll.cxx
+++ FluxSvc/src/Dll/FluxSvc_dll.cxx
@@ -1,37 +1,10 @@
-//====================================================================
-//  FluxSvc_dll.cpp
-//--------------------------------------------------------------------
-//
-//  Package    : Gaudi/System
-//
-//  Description: Implementation of DllMain routine.
-//               The DLL initialisation must be done seperately for 
-//               each DLL. 
-//
-//  Author     : Toby Burnett
-//
-//====================================================================
+/** @file FluxSvc_dll.cxx
+    @brief declare DLL entry point
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
+    $Header$
 
-void GaudiDll::initialize(void* /* hinstDLL */ )    {
-}
+*/
 
-void GaudiDll::finalize(void* /* hinstDLL */ )      {
-}
-
-extern void FluxSvc_load();
-
-#include "GaudiKernel/FactoryTable.h"
-
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-    static bool first = true;
-    if ( first ) {
-        FluxSvc_load();
-        first = false;
-    }
-    return FactoryTable::instance()->getEntries();
-} 
-
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(CalDigi)
 
