879d2f1f45744e02e5dbe3a3f254b01e7b983251
burnett <>
2004-08-13 18:14:32 +0000

    remove (for now) dependence on GRB, as it is moved. tagging v6r15

--- FluxSvc/src/FluxSvc.cxx
+++ FluxSvc/src/FluxSvc.cxx
@@ -55,8 +55,10 @@ class FluxMgr;  // actual manager
 class IParticlePropertySvc; 
 class IAppMgrUI;
 
+#if 0 //THB: these are moved now
 ISpectrumFactory & GRBmanagerFactory();
 ISpectrumFactory & GRBobsFactory();
+#endif
 
 class FluxSvc : 
     virtual public Service, 
@@ -311,12 +313,14 @@ StatusCode FluxSvc::initialize ()
     IToolFactory* toolfactory = 0;
     
     // search throught all objects (factories?)
+#if 0 // THB: GRB moved
     //////////////////////////////////////////////////
     // Adding GRB Factory!
     log << MSG::INFO << "Register GRB models!" << endreq;
     GRBmanagerFactory();
     GRBobsFactory();
     //////////////////////////////////////////////////
+#endif
     for(IObjManager::ObjIterator it = objManager->objBegin(); it !=objManager->objEnd(); ++ it){
 
         std::string tooltype= (*it)->ident();
