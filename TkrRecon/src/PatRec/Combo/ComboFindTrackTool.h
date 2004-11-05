62800b448576e9556b3ded8103c7ede1d8ef97c3
atwood <>
2004-11-05 18:28:56 +0000

    second  pass at adding control parameters via tool properties

--- TkrRecon/src/PatRec/Combo/ComboFindTrackTool.h
+++ TkrRecon/src/PatRec/Combo/ComboFindTrackTool.h
@@ -2,8 +2,8 @@
  * @class ComboFindTrackTool
  *
  * @brief Implements a Gaudi Tool for find track candidates. This particular tool uses the 
- *        "Combo" method, described in detail in TkrComboPatRec, which is very similar to 
- *        the method employed by Glast up to the time of the PDR.
+ *        "Combo" method, described in detail in ComboFindTrackTool.cxx, which is very similar to 
+ *        the method employed by Glast up to the time of the PDR.  
  *
  * @author The Tracking Software Group
  *
