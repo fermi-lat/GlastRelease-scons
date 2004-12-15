b87f7dbc7fa908de9a0659969c9afdb3641f62f8
atwood <>
2004-12-15 15:26:49 +0000

    Implemented first track kink method

--- TkrRecon/src/Track/TrackFitUtils.h
+++ TkrRecon/src/Track/TrackFitUtils.h
@@ -58,6 +58,9 @@ public:
 	/// Finds the number of shared TkrClusters on the two given tracks
     int compareTracks(Event::TkrTrack& track1, Event::TkrTrack& track2);
 
+	/// Conpute the first first normalize track kink angle
+	double firstKinkNorm(Event::TkrTrack& track);
+
     /// Operations
     void   flagAllHits(Event::TkrTrack& track, int iflag=1);
     void   unFlagAllHits(Event::TkrTrack& track);
