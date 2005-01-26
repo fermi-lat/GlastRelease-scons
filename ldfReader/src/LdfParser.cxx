e615aa307547f318bbb3d68a565a1fd1de2e12d0
heather <>
2005-01-26 22:57:46 +0000

    init run id for LDF

--- ldfReader/src/LdfParser.cxx
+++ ldfReader/src/LdfParser.cxx
@@ -27,9 +27,10 @@ namespace ldfReader {
 
     LdfParser::LdfParser(std::string fileName, bool fitsWrap, 
         const std::string& instrument) :
-    m_fileName(fileName), m_fitsWrap(fitsWrap), m_fitsfile(0),
+    m_fileName(fileName), m_fitsWrap(fitsWrap), m_fitsfile(0), m_ebf(0),
         m_maxRow(0), m_currentRow(0), m_maxHdu(0), m_currentHdu(0),
-        m_rowBuf(0), m_maxSize(0), m_evtCount(0), m_instrument(instrument)
+        m_rowBuf(0), m_maxSize(0), m_evtCount(0), m_instrument(instrument),
+        m_runId(0)
     {
         try {
             // MyTkrIterator::setInstrument(instrument);
