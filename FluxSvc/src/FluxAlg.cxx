6ca9cf2f4447636ec7ead1aacfbd41e8f4644180
burnett <>
2005-04-06 21:15:48 +0000

    fix problem that resulted in extra tuple rows: but allow overriding it, see doxygen

--- FluxSvc/src/FluxAlg.cxx
+++ FluxSvc/src/FluxAlg.cxx
@@ -105,6 +105,8 @@ private:
 
     PointingInfo m_pointing_info;
     StringProperty m_root_tree;
+    BooleanProperty m_save_tuple; // set true to save
+
     INTupleWriterSvc* m_rootTupleSvc;;
 
 };
@@ -130,6 +132,7 @@ FluxAlg::FluxAlg(const std::string& name, ISvcLocator* pSvcLocator)
     declareProperty("pointing_history_input_file",  m_pointing_history_input_file="");
 
     declareProperty("pointing_info_tree_name",  m_root_tree="MeritTuple");
+    declareProperty("save_pointing_info",  m_save_tuple=false);
 
 }
 //------------------------------------------------------------------------
@@ -356,7 +359,7 @@ StatusCode FluxAlg::execute()
     
     // put pointing stuff into the root tree
     if( m_rootTupleSvc!=0 && !m_root_tree.value().empty()){
-        m_rootTupleSvc->storeRowFlag(this->m_root_tree.value(), true);
+        m_rootTupleSvc->storeRowFlag(this->m_root_tree.value(), m_save_tuple);
     }
 
     if( m_initialTime==0) m_initialTime=currentTime;
--- FluxSvc/src/mainpage.h
+++ FluxSvc/src/mainpage.h
@@ -38,6 +38,7 @@ Usage is primarily via the FluxAlg algorithm, which access the service to genera
     @param FluxAlg.rocking_angle [0 deg] Rotation angle for Glast, about x-axis. 
     @param FluxAlg.rocking_angle_z [0 deg] Rotation angle for Glast, about z-axis.
     @param FluxAlg.pointing_info_tree_name ["MeritTuple"] If set, copy "Pt" values to it. See point_info for definitions.
+    @param FluxAlg.save_pointing_info [false] Set true to save all entries in the pointing tuple. Normally saved by merit.
 
   @section exposurealg_jobOptions ExposureAlg jobOptions
     @param ExposureAlg.root_tree ["pointing_history"] name for the root tree to be filled if there are clock ticks
