d34f6c0175c21cbd57247139b1d26c950ebdbc5c
jrb <>
2005-04-13 23:06:54 +0000

    Upgrade for commonRootData namespace

--- CalibSvc/src/CalibROOTCnv/cnv/RootCalGainCnv.cxx
+++ CalibSvc/src/CalibROOTCnv/cnv/RootCalGainCnv.cxx
@@ -118,8 +118,8 @@ StatusCode RootCalGainCnv::fillRoot(CalibData::CalibBase* pTDSObj,
                     dynamic_cast<CalibData::Gain*>(pData);
                   // had better not fail!
                   if (!pGain) continue;
-                  CalXtalId id(m_nCol*iRow+iCol, iLayer, iXtal, iFace,
-                               iRange);
+                  commonRootData::CalXtalId 
+                    id(m_nCol*iRow+iCol, iLayer, iXtal, iFace, iRange);
                   pRootGain->addChannel(id, pGain->getGain(),
                                         pGain->getSig());
                 }
@@ -178,7 +178,7 @@ StatusCode RootCalGainCnv::i_createObj(const std::string& fname,
   for (unsigned i = 0; i < nGains; i++) {
     CalibData::Gain* pGain = 
       new CalibData::Gain(rootGains[i].getGain(), rootGains[i].getSig());
-    CalXtalId id = rootGains[i].getId();
+    commonRootData::CalXtalId id = rootGains[i].getId();
     idents::CalXtalId identsId(id.getTower(), id.getLayer(), id.getColumn());
     pObj->putRange(identsId, id.getRange(), id.getFace(), pGain);
   }
