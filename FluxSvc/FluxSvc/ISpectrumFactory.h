8792f72f9122696dca56355b0da66254ab688c8d
burnett <>
2005-04-28 13:23:41 +0000

    restore for CRflux and userAlg

---
+++ FluxSvc/FluxSvc/ISpectrumFactory.h
@@ -0,0 +1,15 @@
+/** 
+* \class ISpectrumFactory
+*
+* \brief simply inlude flux version of this interface
+* 
+*
+* $Header$
+*/
+#ifndef FluxSvc_ISpectrumFactory_h
+#define FluxSvc_ISpectrumFactory_h
+
+// only pass it on to flux
+#include "flux/ISpectrumFactory.h"
+#include "flux/ISpectrum.h"
+#endif 
