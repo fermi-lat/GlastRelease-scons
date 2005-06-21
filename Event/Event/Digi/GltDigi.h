93fc91d09e591133ca22f50e516e126d8a29450d
burnett <>
2005-06-21 02:38:43 +0000

    *** empty log message ***

--- Event/Event/Digi/GltDigi.h
+++ Event/Event/Digi/GltDigi.h
@@ -1,7 +1,14 @@
+/** @file GltDigi.h
+    @brief Definition and implementation of GltDigi
+
+    $Header$
+
+
+*/
 #ifndef Event_GltDigi_H
 #define Event_GltDigi_H 1
 
-#include "GaudiKernel/ContainedObject.h"
+#include "GaudiKernel/DataObject.h"
 #include "GaudiKernel/IInterface.h"
 #include <vector>
 
@@ -12,23 +19,22 @@
 * \brief TDS class for Glt data
 * TDS Trigger class for interface to filter algorithms
 * 
-* $Header$
 */
 
 static const CLID& CLID_GltDigi = InterfaceID("GltDigi", 1, 0);
 
 namespace Event {
 
-class GltDigi : virtual public ContainedObject {
+class GltDigi : virtual public DataObject {
    
  public:
   GltDigi(){;}
 
   virtual ~GltDigi() {;}  
 
-  inline std::vector<bool> getTkrThreeInRow(){return m_TKR_threeinRow;}
-  inline std::vector<bool> getCAL_LO(){return m_CAL_LO;}
-  inline std::vector<bool> getCAL_HI(){return m_CAL_HI;}
+  inline std::vector<bool> getTkrThreeInRow()const{return m_TKR_threeinRow;}
+  inline const std::vector<bool>& getCAL_LO()const{return m_CAL_LO;}
+  inline const std::vector<bool>& getCAL_HI()const{return m_CAL_HI;}
 
   inline void setTkrThreeInRow(std::vector<bool> value)
     {m_TKR_threeinRow = value;}
