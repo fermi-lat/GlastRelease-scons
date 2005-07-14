6a963177510f2c5b9f7d3708d9b07f90fe5b532a
usher <>
2005-07-14 22:48:26 +0000

    Update to the ntuple variables Pol really wanted in CalValsTool

--- AnalysisNtuple/src/CalValsTool.cxx
+++ AnalysisNtuple/src/CalValsTool.cxx
@@ -136,9 +136,9 @@
       double CAL_cfp_calEffRLn;   // Effective radiation lengths in the Cal
 
       double CAL_lll_energy;      // Energy from the Last Layer Likelihood tool
-      double CAL_lll_chiSquare;   // Chisquare from the Last Layer Likelihood
+      double CAL_lll_energyErr;   // Chisquare from the Last Layer Likelihood
       double CAL_tkl_energy;      // Energy from the tracker Likelihood tool
-      double CAL_tkl_chiSquare;   // Chisquare from the tracker likelihood
+      double CAL_tkl_energyErr;   // Chisquare from the tracker likelihood
       
       //Calimeter items with Recon - Tracks
       double CAL_Track_DOCA;
@@ -262,9 +262,9 @@
       addItem("CalCfpChiSq",   &CAL_cfp_totChiSq);
       addItem("CalCfpEffRLn",  &CAL_cfp_calEffRLn);
       addItem("CalLllEnergy",  &CAL_lll_energy);
-      addItem("CalLllChiSq",   &CAL_lll_chiSquare);
+      addItem("CalLllEneErr",  &CAL_lll_energyErr);
       addItem("CalTklEnergy",  &CAL_tkl_energy);
-      addItem("CalTklChiSq",   &CAL_tkl_chiSquare);
+      addItem("CalTklEneErr",  &CAL_tkl_energyErr);
 
       zeroVals();
       
@@ -332,12 +332,12 @@ StatusCode CalValsTool::calculate()
             else if (corResult.getCorrectionName() == "CalLastLayerLikelihoodTool")
             {
                 CAL_lll_energy    = corResult.getParams().getEnergy();
-                CAL_lll_chiSquare = corResult["GeometricCut"];
+                CAL_lll_energyErr = corResult.getParams().getEnergyErr();
             }
             else if (corResult.getCorrectionName() == "CalTkrLikelihoodTool")
             {
                 CAL_tkl_energy    = corResult.getParams().getEnergy();
-                CAL_tkl_chiSquare = corResult["GeometricCut"];
+                CAL_tkl_energyErr = corResult.getParams().getEnergyErr();
             }
         }
     }
