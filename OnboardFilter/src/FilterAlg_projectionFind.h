433c5d597169a093472200e5438a68189713609a
hughes <>
2005-09-02 19:10:16 +0000

    New flight software version

---
+++ OnboardFilter/src/FilterAlg_projectionFind.h
@@ -0,0 +1,45 @@
+#ifndef FILTERALG_PROJECTIONFIND_H
+#define FILTERALG_PROJECTIONFIND_H
+
+
+/* ---------------------------------------------------------------------- *//*!
+   
+   \file   TFC_projectionFind.h
+   \brief  Defines the interface for finding projections in a single tower/
+   \author JJRussell - russell@slac.stanford.edu
+
+\verbatim
+
+    CVS $Id$
+\endverbatim
+                                                                         */
+/* --------------------------------------------------------------------- */
+
+
+
+/* ---------------------------------------------------------------------- *\
+ * 
+ * HISTORY
+ * -------
+ *
+ * DATE     WHO WHAT
+ * -------- --- ---------------------------------------------------------
+ * 12.22.04 jjr Cloned DFC/DFC/TFC_projectionTowerFind.h
+ *
+\* ---------------------------------------------------------------------- */
+
+/* --------------------------------------------------------------------- */
+
+  extern int FilterAlg_projectionsSizeof   (void);
+
+  extern int FilterAlg_projectionTowerFind (TFC_projection          *prjs,
+                                      EDR_tkrTower     *towerRecord,
+				      unsigned int          xLayers,
+                                      unsigned int          yLayers);
+
+/* --------------------------------------------------------------------- */
+
+
+    
+
+#endif
