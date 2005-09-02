b2c51d9718086548db4b302f46c804da911c2e54
hughes <>
2005-09-02 18:47:19 +0000

    move to current flight software

--- OnboardFilter/src/Dll/OnboardFilter_dll.cxx
+++
@@ -1,9 +0,0 @@
-/** @file OnboardFilter_dll.cxx
-
-  $Header$
-*/
-
-// All that is needed in this file is this include
-// and the one line LOAD_FACTORY_ENTRIES(packageName)
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(OnboardFilter)
