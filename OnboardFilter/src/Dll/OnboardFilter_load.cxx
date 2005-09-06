56c981ff509ff672762f99e66b413d4ad3489df3
burnett <>
2005-09-06 16:42:29 +0000

    updates for Windows

---
+++ OnboardFilter/src/Dll/OnboardFilter_load.cxx
@@ -0,0 +1,18 @@
+/** 
+* @file OnboardFilter_load.cxx
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header$
+*/
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// There should be one entry for each component included in 
+// the library for this package.
+DECLARE_FACTORY_ENTRIES(OnboardFilter) {
+    DECLARE_ALGORITHM(OnboardFilter);
+    DECLARE_ALGORITHM(FilterTracks);
+	DECLARE_ALGORITHM(FilterAlg);
+    DECLARE_TOOL(FilterDisplay);
+} 
