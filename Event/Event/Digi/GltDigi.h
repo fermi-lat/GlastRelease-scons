91db2c3cbb5f7efd22752dfa50dff6d5fcb56416
fewtrell <>
2005-10-05 00:17:34 +0000

    Cal trigger info now stored by xtal-face instead of simply by tower. Created Event::GltDigi::CalTriggerMap typdef for this.

--- Event/Event/Digi/GltDigi.h
+++ Event/Event/Digi/GltDigi.h
@@ -11,39 +11,45 @@
 #include "GaudiKernel/DataObject.h"
 #include "GaudiKernel/IInterface.h"
 #include <vector>
+#include <map>
 
 /*!
-* \class GltDigi
-* \author Johann Cohen-Tanugi
-*
-* \brief TDS class for Glt data
-* TDS Trigger class for interface to filter algorithms
-* 
-*/
+ * \class GltDigi
+ * \author Johann Cohen-Tanugi
+ *
+ * \brief TDS class for Glt data
+ * TDS Trigger class for interface to filter algorithms
+ * 
+ */
 
 static const CLID& CLID_GltDigi = InterfaceID("GltDigi", 1, 0);
 
 namespace Event {
 
-class GltDigi : virtual public DataObject {
+  class GltDigi : virtual public DataObject {
    
- public:
-  GltDigi(){;}
+  public:
+    GltDigi(){;}
 
-  virtual ~GltDigi() {;}  
+    virtual ~GltDigi() {;}  
 
-  inline std::vector<bool> getTkrThreeInRow()const{return m_TKR_threeinRow;}
-  inline const std::vector<bool>& getCAL_LO()const{return m_CAL_LO;}
-  inline const std::vector<bool>& getCAL_HI()const{return m_CAL_HI;}
+    inline std::vector<bool> getTkrThreeInRow()const{return m_TKR_threeinRow;}
 
-  inline void setTkrThreeInRow(std::vector<bool> value)
-    {m_TKR_threeinRow = value;}
+    /** \brief represents per xtal face Cal trigger info
+        \note CalXtalId indeces should have optional face info set.
+    */
+    typedef std::map<idents::CalXtalId,bool> CalTriggerMap;
+    inline const CalTriggerMap& getCAL_LO()const{return m_CAL_LO;}
+    inline const CalTriggerMap& getCAL_HI()const{return m_CAL_HI;}
 
-  inline void setCAL_LO(std::vector<bool> value)
-    {m_CAL_LO = value;}
+    inline void setTkrThreeInRow(std::vector<bool> value)
+      {m_TKR_threeinRow = value;}
 
-  inline void setCAL_HI(std::vector<bool> value)
-    {m_CAL_HI = value;}
+    inline void setCAL_LO(const CalTriggerMap &value)
+      {m_CAL_LO = value;}
+
+    inline void setCAL_HI(const CalTriggerMap &value)
+      {m_CAL_HI = value;}
 
 
     inline bool getCALLOtrigger()const { return triggerOR(m_CAL_LO);}
@@ -51,19 +57,20 @@ class GltDigi : virtual public DataObject {
 
     inline bool getCALHItrigger()const {return triggerOR(m_CAL_HI);}
 
- private:
-     inline bool triggerOR(const std::vector<bool>& bits)const{
-        for( std::vector<bool>::const_iterator bit = bits.begin(); 
-            bit!=bits.end(); ++ bit){
-                if( *bit) { return true;}
-            }
-        return false;
-     }
+  private:
+    inline bool triggerOR(const CalTriggerMap& bits) const {
+      for( CalTriggerMap::const_iterator bit = bits.begin(); 
+           bit!=bits.end(); 
+           ++ bit)
+        {if( bit->second) { return true;}}
 
-  std::vector<bool> m_TKR_threeinRow;
-  std::vector<bool> m_CAL_LO;
-  std::vector<bool> m_CAL_HI;
-};
+      return false;
+    }
+
+    std::vector<bool> m_TKR_threeinRow;
+    CalTriggerMap m_CAL_LO;
+    CalTriggerMap m_CAL_HI;
+  };
   
 } //Namespace Event
 #endif
