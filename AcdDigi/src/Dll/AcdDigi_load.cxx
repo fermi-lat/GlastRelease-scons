0bfdef34da51295fed5f4b712898482bdee058ef
heather <>
2005-10-21 22:49:30 +0000

    patch to completely remove AcdDigiMcIntAlg

--- AcdDigi/src/Dll/AcdDigi_load.cxx
+++ AcdDigi/src/Dll/AcdDigi_load.cxx
@@ -10,7 +10,6 @@
 
 DECLARE_FACTORY_ENTRIES(AcdDigi) {
     DECLARE_ALGORITHM( AcdDigiAlg );
-    DECLARE_ALGORITHM( AcdDigiMcIntHitAlg );
     DECLARE_ALGORITHM( AcdDigiOrgAlg );
     DECLARE_TOOL( AcdDigiRandom );
 } 
