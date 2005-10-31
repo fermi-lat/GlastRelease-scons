0e521ee63d76008661ebc9042b3ecb5d84fd95f2
burnett <>
2005-10-31 17:29:24 +0000

    implement connection to new energy analysis trees

--- merit/src/ClassificationTree.h
+++ merit/src/ClassificationTree.h
@@ -55,12 +55,18 @@ private:
     const TupleItem*  m_calLRmsRatio;
     const TupleItem*  m_vtxAngle;
 
+    const TupleItem * m_CalEnergyCorr;
+    const TupleItem * m_CalCfpEnergy;
+    const TupleItem  *m_CalLllEnergy;
+    const TupleItem * m_CalTklEnergy;
+
     // output quantities: pointers to corresponding tuple items
     double* m_goodCalProb; 
     double* m_goodPsfProb; 
     double* m_vtxProb ; // vertex or track choice
     double* m_gammaProb ;
     double* m_gammaType;
+    double* m_EvtEnergyCorr;
     
 
     std::ostream& m_log;
