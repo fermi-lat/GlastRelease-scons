ffa357e263082bae82cbf28086d0bb3829e4186d
burnett <>
2005-12-17 22:49:28 +0000

    split off code to manage backgrouind souice, selection according to magnetic latitude

--- Interleave/src/mainpage.h
+++ Interleave/src/mainpage.h
@@ -4,12 +4,13 @@
 /*! \mainpage package Interleave
 
    \author Toby Burnett <tburnett@u.washington.edu>
-
+   
 
   This package manages the insertion of interleaved background events into a Gleam simulation.
 
-  - InterleaveAlg   
-  - SampledBackground
+  - InterleaveAlg   Gaudi algorithm that manages the insertion
+  - SampledBackground Spectrum to insert flag particles at the trigger rate
+  - BackgroundSelection Manage selection of background events from external database
 
   <hr>
   \section notes release notes
