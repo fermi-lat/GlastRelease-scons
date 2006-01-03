8d8a4bedf4a0e0df3ed6117ccdd85d6375667e5b
richard <>
2006-01-03 18:48:05 +0000

    one line per event

--- GlastSvc/src/CurrentEventAlg/CurrentEventAlg.cxx
+++ GlastSvc/src/CurrentEventAlg/CurrentEventAlg.cxx
@@ -67,14 +67,8 @@ StatusCode CurrentEventAlg::initialize(){
 
     facilities::Util::expandEnvVar(&m_outputFileName);
 
-    m_eventFile = new std::ofstream(m_outputFileName.c_str());
-    if (!m_eventFile)  {
-        MsgStream log( msgSvc(), name() );
-        log << MSG::WARNING << "Failed to create output event file" << endreq;
-        return StatusCode::FAILURE;
-    }
+    m_eventFile = 0;
 
-   
     return sc;
 }
 
@@ -83,12 +77,13 @@ StatusCode CurrentEventAlg::execute()
 {
     StatusCode  sc = StatusCode::SUCCESS;
     
-    if (!m_eventFile->good()) {
-        MsgStream   log( msgSvc(), name() );
-        log << MSG::WARNING << "Output file is bad, not writing" << endreq;
-        return sc;
+    m_eventFile = new std::ofstream(m_outputFileName.c_str(),std::ios_base::trunc);
+    if (!m_eventFile)  {
+        MsgStream log( msgSvc(), name() );
+        log << MSG::WARNING << "Failed to create output event file" << endreq;
+        return StatusCode::FAILURE;
     }
-    
+
     SmartDataPtr<Event::EventHeader> header(eventSvc(), EventModel::EventHeader);
     if(0==header) {
         MsgStream   log( msgSvc(), name() );
@@ -96,16 +91,18 @@ StatusCode CurrentEventAlg::execute()
         return StatusCode::SUCCESS;
     }
 
+
     *m_eventFile << "run=" << header->run() << " event=" << header->event() << "  ";
 
     m_eventFile->flush();
+    m_eventFile->close();
+
     return sc; 
 }
 
 StatusCode CurrentEventAlg::finalize(){
     StatusCode  sc = StatusCode::SUCCESS;
-    m_eventFile->close();
-    delete m_eventFile;
+    if (m_eventFile) delete m_eventFile;
     return sc;
 }
 
