a9d04b20eb772a1a0ac091f6d21d812b3b44164b
burnett <>
2006-01-08 01:56:08 +0000

    clean up documentation, style a bit

--- Interleave/src/SampledBackground.cxx
+++ Interleave/src/SampledBackground.cxx
@@ -6,7 +6,7 @@ $Header$
 
 */
 #include "SampledBackground.h"
-#include <cmath>
+
 #include "InterleaveAlg.h" // for static rate to use
 
 #include "flux/EventSource.h"
@@ -15,23 +15,23 @@ $Header$
 SampledBackground::SampledBackground(const std::string& /*params*/)
 {}
 
-  // Gives back energy
+// Gives back energy
 double SampledBackground::energy(double /*time*/){ return 0.;}
 
-  // Gives back paticle direction in cos(theta) and phi[rad]
-   std::pair<double,double> SampledBackground::dir(double /*energy*/)
-   { 
-       return std::make_pair(0., 0.);
-   }
+// Gives back paticle direction in cos(theta) and phi[rad]
+std::pair<double,double> SampledBackground::dir(double /*energy*/)
+{ 
+    return std::make_pair(0., 0.);
+}
 
-   // Gives back the total flux into the standard area 
-   double SampledBackground::flux(double /*time*/) const // calculate the flux [c/s/m^2/sr]
-   { 
-       return  InterleaveAlg::triggerRate()/(EventSource::totalArea() * solidAngle());
-   }
+// Gives back the total flux into the standard area 
+double SampledBackground::flux(double /*time*/) const // calculate the flux [c/s/m^2/sr]
+{ 
+    return  InterleaveAlg::rate()/(EventSource::totalArea() * solidAngle());
+}
 
 
-  // Gives back solid angle from which particles come
-   double SampledBackground::solidAngle() const
-   { return 4*M_PI;}
-  
+// Gives back solid angle from which particles come
+double SampledBackground::solidAngle() const
+{ return 4*M_PI;}
+
