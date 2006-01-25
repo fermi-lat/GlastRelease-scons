c1406c6f0d227d68c629a9f390ea5bb1ebf6d70f
dwood <>
2006-01-25 23:28:53 +0000

    Added charVal application to validate DAC/ADC characterization table files; currently works for FLE, FHE, and LAC; still needs work for ULD.

---
+++ calibGenCAL/python/charVal.bat
@@ -0,0 +1,16 @@
+REM $Header$
+@echo off
+
+if not defined CALIBGENCALROOT goto :ERROR
+
+setlocal
+set PYTHONROOT=%CALIBGENCALROOT%\python;%ROOTSYS%\bin;%PYTHONROOT%;
+python %CALIBGENCALROOT%\python\charVal.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
+goto EXIT
+
+:ERROR
+echo ERROR: charVal: CALIBGENCALROOT must be defined
+
+:EXIT
+
