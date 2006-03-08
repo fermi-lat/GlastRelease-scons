4c03e26b6cc19856ebf16acc44f3944f31414fba
fewtrell <>
2006-03-08 16:16:18 +0000

    adding new .bat launcher scripts.

---
+++ calibGenCAL/python/asymXML2TXT.bat
@@ -0,0 +1,15 @@
+REM $Header$
+@echo off
+
+if not defined CALIBGENCALROOT goto :ERROR
+
+setlocal
+set PYTHONROOT=%CALIBGENCALROOT%\python;%PYTHONROOT%;
+python %CALIBGENCALROOT%\python\asymXML2TXT.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
+goto :EXIT
+
+:ERROR
+echo ERROR: asymXML2TXT: CALIBGENCALROOT must be defined
+
+:EXIT
