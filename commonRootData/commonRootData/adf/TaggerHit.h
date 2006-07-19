a72f68a40ddac452997edcbf408e77f96737ace1
heather <>
2006-07-19 22:17:53 +0000

    prepare for HEAD of AncillaryDataEvent uses doubles for pulseheight

--- commonRootData/commonRootData/adf/TaggerHit.h
+++ commonRootData/commonRootData/adf/TaggerHit.h
@@ -19,12 +19,12 @@ public:
     TaggerHit();
 
     TaggerHit(UInt_t moduleId, UInt_t layerId, UInt_t stripId,
-             UInt_t pulseHeight, Bool_t isPedSubtracted);
+             Double_t pulseHeight, Bool_t isPedSubtracted);
 
     TaggerHit(const TaggerHit& copy);
 
     void initialize(UInt_t moduleId, UInt_t layerId, UInt_t stripId,
-             UInt_t pulseHeight, Bool_t isPedSubtracted);
+             Double_t pulseHeight, Bool_t isPedSubtracted);
     
     TaggerHit& operator=(const TaggerHit& copy);
 
@@ -37,7 +37,7 @@ public:
     UInt_t getModuleId() const { return m_moduleId; }
     UInt_t getLayerId() const { return m_layerId; }
     UInt_t getStripId() const { return m_stripId; }
-    UInt_t getPulseHeight() const { return m_pulseHeight; }
+    Double_t getPulseHeight() const { return m_pulseHeight; }
     Bool_t isPedestalSubtracted() const { return m_isPedestalSubtracted; } 
 
     Bool_t CompareInRange( const TaggerHit &ref, const std::string& name="" )const;
@@ -47,7 +47,7 @@ private:
     UInt_t m_moduleId;
     UInt_t m_layerId;
     UInt_t m_stripId;
-    UInt_t m_pulseHeight;
+    Double32_t m_pulseHeight;
     Bool_t m_isPedestalSubtracted;
 
 
