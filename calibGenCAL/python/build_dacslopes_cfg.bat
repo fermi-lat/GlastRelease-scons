70bd9fd6a8048192ab33582b32a5fe203f042bd2
dwood <>
2006-07-20 17:23:22 +0000

    Added windows launch script.

---
+++ calibGenCAL/python/build_dacslopes_cfg.bat
@@ -0,0 +1,16 @@
+REM $Header$
+@echo off
+
+if not defined CALIBGENCALROOT goto :ERROR
+
+setlocal
+set PYTHONPATH=%CALIBGENCALROOT%\python\lib;%PYTHONPATH%;
+python %CALIBGENCALROOT%\python\build_dacslopes_cfg.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
+goto :EXIT
+
+:ERROR
+echo ERROR: asymTXT2XML: CALIBGENCALROOT must be defined
+
+:EXIT
+
