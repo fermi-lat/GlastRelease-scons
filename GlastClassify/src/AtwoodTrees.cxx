df096d835d46c6d2854a6f95bed9f1993f34de57
usher <>
2006-08-03 16:21:41 +0000

    Apply fix to zero CTB variables before running IM analysis. This will fix Bill's current problem with CTBGAM > 0 on events that should have been rejected.

--- GlastClassify/src/AtwoodTrees.cxx
+++ GlastClassify/src/AtwoodTrees.cxx
@@ -186,7 +186,7 @@ bool AtwoodTrees::execute()
     int    eventId      = *m_eventId;
     int    run          = *m_run;
 
-    if ((run == 1 && eventId == 59) || (run == 1 && eventId == 176))
+    if ((run == 25312 && eventId == 67710) || (run == 25888 && eventId == 72459))
     {
         int j = 0;
     }
@@ -221,14 +221,6 @@ bool AtwoodTrees::execute()
     double FilterStatus_HI = *m_FilterStatus_HI;
 
     FilterStatus_HI = 0;
-
-    float  eventid           = m_treeAnalysis->getTupleVal("EvtEventId");
-    double ctbBestEnergyProb = m_treeAnalysis->getTupleVal("CTBBestEnergyProb");
-    double ctbCore           = m_treeAnalysis->getTupleVal("CTBCORE");
-    double ctbGam            = m_treeAnalysis->getTupleVal("CTBGAM");
-    double ctbParamProb      = m_treeAnalysis->getTupleVal("CTBParamProb");
-    double ctbProfileProb    = m_treeAnalysis->getTupleVal("CTBProfileProb");
-    double ctbVtx            = m_treeAnalysis->getTupleVal("CTBVTX");
 
     // First cuts on Filter status and failures for energy and tails
     if (FilterStatus_HI == 0 && m_bestEnergyProb > 0.1 && m_CORE > 0.1)
