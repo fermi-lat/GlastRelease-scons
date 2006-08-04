2a5bd65dd5cf437ca587f140951c4e079472a939
usher <>
2006-08-04 21:53:51 +0000

    Update to the latest latest IM analysis sheet: Pass4_Analysis_Complete_v5.xml

--- GlastClassify/src/AtwoodTrees.cxx
+++ GlastClassify/src/AtwoodTrees.cxx
@@ -17,7 +17,7 @@ $Header$
 
 namespace {
     // This should be changed each time there is a new file, to make it defautl
-    std::string default_xml("$(GLASTCLASSIFYROOT)/xml/Pass4_Analysis_Complete_v4.xml");
+    std::string default_xml("$(GLASTCLASSIFYROOT)/xml/Pass4_Analysis_Complete_v5.xml");
 }
 /* 
 */
