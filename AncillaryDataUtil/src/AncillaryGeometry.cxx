10e05c1875c30c3d247b5a29d6b40779f87f5bf4
omodei <>
2006-08-11 10:53:33 +0000

    Ready to be tagged as final version (?)

--- AncillaryDataUtil/src/AncillaryGeometry.cxx
+++ AncillaryDataUtil/src/AncillaryGeometry.cxx
@@ -9,27 +9,6 @@ AncillaryGeometry::AncillaryGeometry(std::string GeometryfileName,std::string rc
 {
   facilities::Util::expandEnvVar(&GeometryfileName);
   facilities::Util::expandEnvVar(&rcReportPath);
-  std::cout<<GeometryfileName<<std::endl;
-
-  /*
-    std::ifstream geometryFile(GeometryfileName.c_str());
-  if(!geometryFile.is_open())
-    {
-      std::cerr<<"Error: could not open "<<GeometryfileName<<"\n";
-      exit(8);
-    }
-  std::string input;
-  getline(geometryFile,input, '\n');
-  std::cout<<input<<std::endl;
-  geometryFile >> BL0;
-  geometryFile >> BL1;
-  std::cout<<BL0<<" "<<BL1<<std::endl;
-  getline(geometryFile,input, '\n');
-  std::cout<<input<<std::endl;
-  getline(geometryFile,input, '\n');
-  std::cout<<input<<std::endl;
-  exit(0);
-  */
   FILE *infile;
   infile=fopen(GeometryfileName.c_str(),"r");
   if (infile==NULL)
@@ -38,13 +17,11 @@ AncillaryGeometry::AncillaryGeometry(std::string GeometryfileName,std::string rc
       exit(8);
     }
   fgets(title,sizeof(title),infile);
-  std::cout<<title<<std::endl;
   fscanf(infile,"%lf %lf",&BL0,&BL1);
-  fgets(title,sizeof(title),infile);
-  std::cout<<title<<std::endl;
-  fgets(title,sizeof(title),infile);
-  std::cout<<title<<std::endl;
-  //  std::cout<<BL0<<" "<<BL1<<std::endl;
+  char tmp[200];
+  fgets(tmp,sizeof(tmp),infile);
+  fgets(tmp,sizeof(tmp),infile);
+
   unsigned int id;
   double x,y,z;
   char v1,v2,d1,d2;
@@ -56,7 +33,7 @@ AncillaryGeometry::AncillaryGeometry(std::string GeometryfileName,std::string rc
       X[M]=x;
       Y[M]=y;
       Z[M]=z;
-      std::cout<<id<<" "<<x<<" "<<y<<" "<<z<<" "<<v1<<" "<<v2<<" "<<d1<<" "<<d2<<std::endl;
+      //      std::cout<<id<<" "<<x<<" "<<y<<" "<<z<<" "<<v1<<" "<<v2<<" "<<d1<<" "<<d2<<std::endl;
       if (v1=='Y'|| v1=='y') V1[M]=0;
       else if (v1=='Z'|| v1=='z') V1[M]=1;
       else std::cout<<"Wrong geometry file format"<<std::endl;
@@ -73,28 +50,34 @@ AncillaryGeometry::AncillaryGeometry(std::string GeometryfileName,std::string rc
   std::ifstream rcReport(rcReportPath.c_str());
   if(!rcReport.is_open())
     {
-      std::cerr<<"Error: could not open "<<rcReportPath<<"\n";
       m_MagnetCurrent=600.0;
+      std::cerr<<"WARNING: AncillaryUtil:: could not open "<<rcReportPath<<"\n";
+      std::cout<<" set the Magnet Current to: "<<m_MagnetCurrent<<" and the Beam Momentum to: "<<m_BeamMomentum<<" GeV/c"<<std::endl;
     }
-  std::string input;
-  getline(rcReport, input, '\n');
-  int pos1=input.rfind("<MagnetCurrent>");
-  int pos2=input.rfind("</MagnetCurrent>");
-  std::string magnetic_current_value = input.substr(pos1+15, pos2-pos1);
-  pos1=input.rfind("<BeamMomentum>");
-  pos2=input.rfind("</BeamMomentum>");
-  std::string beam_momuentum = input.substr(pos1+14, pos2-pos1);
-  m_MagnetCurrent = atof(magnetic_current_value.c_str());
-  m_BeamMomentum = atof(beam_momuentum.c_str());
-  //  std::cout<<magnetic_current_value<<" "<<m_MagnetCurrent<<std::endl;
+  else
+    {
+      std::string input;
+      getline(rcReport, input, '\n');
+      int pos1=input.rfind("<MagnetCurrent>");
+      int pos2=input.rfind("</MagnetCurrent>");
+      std::string magnetic_current_value = input.substr(pos1+15, pos2-pos1);
+      pos1=input.rfind("<BeamMomentum>");
+      pos2=input.rfind("</BeamMomentum>");
+      std::string beam_momuentum = input.substr(pos1+14, pos2-pos1);
+      m_MagnetCurrent = atof(magnetic_current_value.c_str());
+      m_BeamMomentum = atof(beam_momuentum.c_str());
+    }
   BL = (m_MagnetCurrent==0 ? 600 : BL0 * m_MagnetCurrent + BL1 * m_MagnetCurrent*m_MagnetCurrent);
 }
 
 void AncillaryGeometry::print()
 {
-  std::cout<<"B*L (field times distance in Tm) =  "<<BL0<<" * "<<m_MagnetCurrent<<" + "<<BL1<<" *("<<m_MagnetCurrent<<")^2 = "<<BL<<std::endl;
-  std::cout<<" Beam Momentum: "<<m_BeamMomentum<<" GeV"<<std::endl;
+  std::cout<<title<<std::endl;
   for(unsigned int M=0; M < N_MODULES; M++) 
     std::cout<<M<<"\t "<<getX(M)<<"\t"<<getY(M)<<"\t"<<getZ(M)<<"\t"<<getView1(M)<<"\t"<<getView2(M)<<"\t"<<getDirection1(M)<<"\t"<<getDirection2(M)<<std::endl;
+  std::cout<<"------------------------------------------------------------------------"<<std::endl;
+  std::cout<<"B*L (field times distance in Tm) =  "<<BL0<<" * "<<m_MagnetCurrent<<" + "<<BL1<<" *("<<m_MagnetCurrent<<")^2 = "<<BL<<std::endl;
+  std::cout<<" Beam Momentum: "<<m_BeamMomentum<<" GeV"<<std::endl;
   
 }
+
