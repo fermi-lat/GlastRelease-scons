a16900e0f467a2825ddd9610f4a074e47df71030
usher <>
2006-10-23 21:32:19 +0000

    files to make GlastClassify a Gaudi package

---
+++ GlastClassify/src/Dll/GlastClassify.cxx
@@ -0,0 +1,16 @@
+/*
+* @file GlastClassify_load.cxx
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header$
+*/
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_FACTORY_ENTRIES(GlastClassify) {
+    DECLARE_ALGORITHM( ClassifyAlg);
+} 
+
+
+
---
+++ GlastClassify/src/Dll/GlastClassify_dll.cxx
@@ -0,0 +1,9 @@
+/** @file GlastClassify_dll.cxx
+ @brief Gaudi interface
+
+  $Header$
+*/
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(GlastClassify)
+
