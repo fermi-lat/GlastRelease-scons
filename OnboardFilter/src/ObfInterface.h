d3a2cd18dded07fad9d445607511b8fc584706ab
usher <>
2006-12-20 18:59:00 +0000

    Updates to enable/disable veto mask for gamma filter

--- OnboardFilter/src/ObfInterface.h
+++ OnboardFilter/src/ObfInterface.h
@@ -50,7 +50,7 @@ public:
 
     ///@name access methods
     /// Set up a filter specified by its name
-    int  setupFilter(const std::string& filterName, int priority, void* prm);
+    int  setupFilter(const std::string& filterName, int priority, bool clearVetoBits);
 
     /// Set up the specific passthrough filter
     bool setupPassThrough(void* prm);
