90c721e4d7581758f984091d3c01deb879b31935
lsrea <>
2007-03-14 22:15:11 +0000

    Load ObfValsTool

--- AnalysisNtuple/src/Dll/AnalysisNtuple_load.cxx
+++ AnalysisNtuple/src/Dll/AnalysisNtuple_load.cxx
@@ -21,14 +21,13 @@ DECLARE_FACTORY_ENTRIES(AnalysisNtuple) {
     DECLARE_TOOL(      TkrHitValsTool   );
     DECLARE_TOOL(      VtxValsTool      );
     DECLARE_TOOL(      EvtValsTool      );
-    //@@@FP 07/08/05
-    DECLARE_TOOL(      CalMipValsTool   );
-    //@@@FP 07/08/05
-    // removed 12/19/06 LSR
+    DECLARE_TOOL(      CalMipValsTool   );   //@@@FP 07/08/05
+    DECLARE_TOOL(      ObfValsTool      );   //@@@LSR 03/14/07
+ 
+   // removed 12/19/06 LSR
     //@@@ CL 06/26/06
     //DECLARE_TOOL(      GcrSelectValsTool  );
     //DECLARE_TOOL(      GcrReconValsTool   );
-    //@@@ CL 06/26/06
 
 
 } 
