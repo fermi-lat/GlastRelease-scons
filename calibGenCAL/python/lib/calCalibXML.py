ffb728fe14bdafa27bb3f434a9b92ab9becd2aab
dwood <>
2007-03-23 17:41:38 +0000

    Exposed 'validating' parameter in calCalibXML.__init__(); this allows offline calib files to be read in environments (e.g. ConfigSystem) where the validating parser is not supported.

--- calibGenCAL/python/lib/calCalibXML.py
+++ calibGenCAL/python/lib/calCalibXML.py
@@ -50,7 +50,7 @@ class calCalibXML(calXML.calXML):
     in XML format.
     """
   
-    def __init__(self, fileName, mode = MODE_READONLY):
+    def __init__(self, fileName, mode = MODE_READONLY, validating = True):
         """
         Open a CAL calibration XML file
 
@@ -58,7 +58,7 @@ class calCalibXML(calXML.calXML):
         \param mode The file access mode (MODE_READONLY or MODE_CREATE).
         """
 
-        calXML.calXML.__init__(self, fileName, mode, validating = True)
+        calXML.calXML.__init__(self, fileName, mode, validating)
 
 
     def info(self):
