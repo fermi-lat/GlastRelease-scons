d3df271944e61aa7337baf50fdc701fdd37d12a2
dwood <>
2007-03-27 15:29:52 +0000

    Fixed typos in log output messages.

--- calibGenCAL/python/dacSlopesVal.py
+++ calibGenCAL/python/dacSlopesVal.py
@@ -975,7 +975,7 @@ if __name__ == '__main__':
     log.info("ULD LEX1 MeV saturation average = %f, stddev=%f", av, sd)
     
     mn = min(Numeric.ravel(uldData[calConstant.CRNG_LEX1,...,2]))
-    log.info("ULD LEX8 MeV saturation minimum = %f", mn)
+    log.info("ULD LEX1 MeV saturation minimum = %f", mn)
     
     # ULD HEX8
     
@@ -985,7 +985,7 @@ if __name__ == '__main__':
     mn = min(data)
     mx = max(data)
     log.info("ULD HEX8 Mev/DAC slope average = %f, stddev=%f", av, sd) 
-    log.info("ULD HEX1 Mev/DAC slope minumum = %f, maximum = %f", mn, mx)
+    log.info("ULD HEX8 Mev/DAC slope minumum = %f, maximum = %f", mn, mx)
     
     av = Numeric.average(uldData[calConstant.CRNG_HEX8,...,1], axis = None) 
     sd = MLab.std(Numeric.ravel(uldData[calConstant.CRNG_HEX8,...,1]))
@@ -996,7 +996,7 @@ if __name__ == '__main__':
     log.info("ULD HEX8 MeV saturation average = %f, stddev=%f", av, sd)
     
     mn = min(Numeric.ravel(uldData[calConstant.CRNG_HEX8,...,2]))
-    log.info("ULD LEX8 MeV saturation minimum = %f", mn)
+    log.info("ULD HEX8 MeV saturation minimum = %f", mn)
 
     # report results
 
