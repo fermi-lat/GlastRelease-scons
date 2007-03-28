a9159822dda0709412abd1ac1755486081cf266e
kocian <>
2007-03-28 21:21:02 +0000

    added doxygen comments

--- configData/configData/gem/TrgConfiguration.h
+++ configData/configData/gem/TrgConfiguration.h
@@ -15,6 +15,12 @@
 //      Copyright (C) 2005      Stanford Linear Accelerator Center
 //
 //---------------------------------------------------------------------------
+/** @class TrgConfiguration
+    @author M. Kocian
+
+    Class to hold GEM configuration register information.
+*/
+
 
 #ifndef TRGCONFIGURATION_HH
 #define TRGCONFIGURATION_HH
@@ -24,20 +30,28 @@
 
 class TrgConfiguration : public ConfigBranch {
 public:
+  /// Default constructor
   TrgConfiguration();
+  /// Constructor with a register value as initializer
   TrgConfiguration(unsigned long i);
+  /// Default destructor
   virtual ~TrgConfiguration(){}
+  /// Set configuration to register value i
   void setConfiguration(unsigned long i);
+  /// Retrieve configuration register value
   unsigned long configuration() const{return _configuration;}
+  /// Set bit so ACD is used as trigger 
   void useAcdAsTrigger(bool on);
+  /// Is ACD used as trigger?
   bool acdUsedAsTrigger() const;
-  // Reset the cached and output values
+  /// Reset the cached and output values
   virtual void reset(){_configuration=0;}
-  // Attach this value to a TTree
+  /// Attach this value to a TTree
   virtual void makeBranch(TTree& tree, const std::string& prefix) const;
   virtual void attach(TTree& tree, const std::string& prefix) const;
 private:
   unsigned long _configuration;
 };
 #endif
+/// printout function
 std::ostream& operator <<(std::ostream& os, const TrgConfiguration& tc);
