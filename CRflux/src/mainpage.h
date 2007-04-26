aec58753fccaefc7d9d8f87794099db5bd94c552
burnett <>
2007-04-26 17:59:03 +0000

    add a few comments about Heavy ion source names

--- CRflux/src/mainpage.h
+++ CRflux/src/mainpage.h
@@ -14,6 +14,39 @@
   - CrElectron
   - CrPositron
   - CrGamma
+  - CrHeavyIon
+  - CrHeavyIonVertical
+
+The following heavy ion sources are defined:
+@verbatum
+    CrHeavyIon                 
+    CrHeavyIonVertical                 
+    CrHeavyIonZ6                 
+    CrHeavyIonZ14                 
+    CrHeavyIonZ15                 
+    CrHeavyIonZ26                 
+    CrHeavyIonVert6                 
+    CrHeavyIonVert7                 
+    CrHeavyIonVert8                 
+    CrHeavyIonVert9                 
+    CrHeavyIonVert10                 
+    CrHeavyIonVert11                 
+    CrHeavyIonVert12                 
+    CrHeavyIonVert13                 
+    CrHeavyIonVert14                 
+    CrHeavyIonVert15                 
+    CrHeavyIonVert16                 
+    CrHeavyIonVert17                 
+    CrHeavyIonVert18                 
+    CrHeavyIonVert19                 
+    CrHeavyIonVert20                 
+    CrHeavyIonVert21                 
+    CrHeavyIonVert22                 
+    CrHeavyIonVert23                 
+    CrHeavyIonVert24                 
+    CrHeavyIonVert25                 
+    CrHeavyIonVert26                 
+@endverbatum
 
   \section references References
     - Tsunefumi Mizuno et al.  (astro-ph/0406684)
