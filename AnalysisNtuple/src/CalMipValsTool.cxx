cf0e67eb46e1627a5ce8b2131fe3e09484316b51
guiriec <>
2007-06-06 10:00:04 +0000

    Computation of three new MipFinder's variables (CalMipErmc, CalMipDerr and CalMipBarDist) to add in the MeritTuple file

--- AnalysisNtuple/src/CalMipValsTool.cxx
+++ AnalysisNtuple/src/CalMipValsTool.cxx
@@ -57,30 +57,35 @@
 class CalMipValsTool :   public ValBase
 {
 public:  
-    CalMipValsTool( const std::string& type, 
-                    const std::string& name, 
-                    const IInterface* parent);
-      
-    virtual ~CalMipValsTool() { }
-      
-    StatusCode initialize();
-      
-    StatusCode calculate();
-      
+  CalMipValsTool( const std::string& type, 
+		  const std::string& name, 
+		  const IInterface* parent);
+  
+  virtual ~CalMipValsTool() { }
+  
+  StatusCode initialize();
+  
+  StatusCode calculate();
+  
 private:
-    float m_num;
-    float m_x0;
-    float m_y0;
-    float m_z0;
-    float m_xDir;
-    float m_yDir;
-    float m_zDir;
-    float m_d2edge;
-    float m_arcLen;
-    float m_ecor;
-    float m_ecorRms;
-    float m_chi2;
-    float m_erm;     
+  float m_num;
+  float m_x0;
+  float m_y0;
+  float m_z0;
+  float m_xDir;
+  float m_yDir;
+  float m_zDir;
+  float m_d2edge;
+  float m_arcLen;
+  float m_ecor;
+  float m_ecorRms;
+  float m_chi2;
+  float m_erm;
+  //2007-04-05 Sylvain Guiriec
+  float m_ermc;
+  float m_derr;
+  float m_barDist;
+  //2007-04-05 Sylvain Guiriec End 
 };
   
 // Static factory for instantiation of algtool objects
@@ -148,7 +153,13 @@ StatusCode CalMipValsTool::initialize()
     addItem("CalMipEcor",    &m_ecor);    
     addItem("CalMipEcorRms", &m_ecorRms); 
     addItem("CalMipChi2",    &m_chi2);    
-    addItem("CalMipErm",     &m_erm);     
+    addItem("CalMipErm",     &m_erm);
+
+    //2007-04-05 Sylvain Guiriec
+    addItem("CalMipErmc",    &m_ermc);
+    addItem("CalMipDerr",    &m_derr);
+    addItem("CalMipBarDist", &m_barDist);
+    //2007-04-05 Sylvain Guiriec End 
     
     return sc;
 }
@@ -282,7 +293,81 @@ StatusCode CalMipValsTool::calculate()
     m_ecorRms=ecorRms;
     m_chi2=chi2;
     m_erm=erm;
-    
+
+    //2007-04-05 Sylvain Guiriec
+
+    double ermc=0.;
+    double derr=0.;
+    double barDist=0.;
+
+    m_ermc=-100.;
+    m_derr=-100.;
+    m_barDist=-100.;
+
+    //Recover EventHeader Pointer
+    //SmartDataPtr<Event::EventHeader> pEvent(m_pEventSvc, EventModel::EventHeader);
+
+    // Recover Track associated info. 
+    SmartDataPtr<Event::TkrTrackCol>   pTracks(m_pEventSvc,EventModel::TkrRecon::TkrTrackCol);
+    if(!pTracks) return sc;
+
+    //SmartDataPtr<Event::TkrVertexCol>  pVerts(m_pEventSvc,EventModel::TkrRecon::TkrVertexCol);
+    //SmartDataPtr<Event::TkrClusterCol> pClusters(m_pEventSvc,EventModel::TkrRecon::TkrClusterCol);
+
+    // all variable values are preset to zero. Be sure to re-initialize the ones you care about  
+
+    //double die_width = m_tkrGeom->ladderPitch();
+    //int nDies = m_tkrGeom->nWaferAcross();
+
+    if (!pTracks || n1<=0)
+      return sc;
+    else
+      {
+	// Count number of tracks
+	int nTracks = pTracks->size();
+	int Tkr_No_Tracks   = nTracks;
+	
+	if(nTracks < 1) return sc;
+	
+	// Get the first Track - it should be the "Best Track"
+	Event::TkrTrackColConPtr pTrack = pTracks->begin();
+	
+	const Event::TkrTrack* track_1 = *pTrack;
+	
+	Point  x1 = track_1->getInitialPosition();
+	Vector t1 = track_1->getInitialDirection();
+	
+	float Tkr_1_xdir        = t1.x();
+	float Tkr_1_ydir        = t1.y();
+	float Tkr_1_zdir        = t1.z();
+	
+	float Tkr_1_x0          = x1.x();
+	float Tkr_1_y0          = x1.y();
+	float Tkr_1_z0          = x1.z();
+
+	if (m_arcLen>0)
+	  ermc=m_erm/m_arcLen;
+	else if (m_arcLen<=0)
+	  ermc=-100.;
+	//@@@@@
+	derr=TMath::ACos(m_xDir*Tkr_1_xdir+
+				m_yDir*Tkr_1_ydir+
+				m_zDir*Tkr_1_zdir);
+	
+	//@@@@@ Distance between tracker track & calorimeter track's centroid point
+	double l=(Tkr_1_xdir*(m_x0-Tkr_1_x0)+Tkr_1_ydir*(m_y0-Tkr_1_y0)+Tkr_1_zdir*(m_z0-Tkr_1_z0))/(TMath::Power(Tkr_1_xdir,2)+TMath::Power(Tkr_1_ydir,2)+TMath::Power(Tkr_1_zdir,2));
+	double Bx   = l*Tkr_1_xdir+Tkr_1_x0;
+	double By   = l*Tkr_1_ydir+Tkr_1_y0;
+	double Bz   = l*Tkr_1_zdir+Tkr_1_z0;
+	barDist = TMath::Sqrt(TMath::Power(m_x0-Bx,2)+TMath::Power(m_y0-By,2)+TMath::Power(m_z0-Bz,2));
+      }
+
+    m_ermc=ermc;
+    m_derr=derr;
+    m_barDist=barDist;
+
+    //2007-04-05 Sylvain Guiriec End  
+
     log << MSG::DEBUG << " FP : readCalMipTrackCol in CalMipValsTool End  " << m_num << endreq;
     return sc;
 }
