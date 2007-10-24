0c886f5ec3b31d6c4e61c94fb2024e278bbda94b
heather <>
2007-10-24 19:12:05 +0000

    add some release.notes and mainpage.h

---
+++ gr_app/src/mainpage.h
@@ -0,0 +1,15 @@
+//$Header$
+
+/*! @mainpage  package gr_app 
+
+
+<hr>
+  \section requirements requirements
+  \include requirements
+
+<hr>
+    @todo  
+    <ul> <li> Introduce use of facilities and start using commonUtilities 
+    </ul>
+
+*/
