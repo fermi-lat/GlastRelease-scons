a7ffd49b292cc3fea028745664c2e23d0dc22b00
jrb <>
2008-02-20 19:44:11 +0000

    Add new entry in m_calibCLIDs for ACD ribbon calibration

--- CalibSvc/src/CalibDataSvc/CalibDataSvc.cxx
+++ CalibSvc/src/CalibDataSvc/CalibDataSvc.cxx
@@ -238,6 +238,7 @@ void CalibDataSvc::initPathArrays() {
   m_calibCLIDs[Calib_ACD_Range] = CLID_Calib_ACD_Range;
   m_calibCLIDs[Calib_ACD_HighRange] = CLID_Calib_ACD_HighRange;
   m_calibCLIDs[Calib_ACD_CoherentNoise] = CLID_Calib_ACD_CoherentNoise;
+  m_calibCLIDs[Calib_ACD_Ribbon] = CLID_Calib_ACD_Ribbon;
   m_calibCLIDs[Calib_CalibTest1] = CLID_Calib_CalibTest1;
   m_calibCLIDs[Calib_NAS_TowerCfg] = CLID_Calib_NAS_TowerCfg;
   m_calibCLIDs[Calib_NAS_SAABoundary] = CLID_Calib_NAS_SAABoundary;
