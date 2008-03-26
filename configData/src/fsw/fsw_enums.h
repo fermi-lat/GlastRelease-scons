ecb1f33b217aa77201442f0d310e65243bcf3f98
echarles <>
2008-03-26 03:15:52 +0000

    added fsw printout directory

---
+++ configData/src/fsw/fsw_enums.h
@@ -0,0 +1,35 @@
+// -*- Mode: c++ -*-
+#ifndef fsw_enums_HH
+#define fsw_enums_HH
+//---------------------------------------------------------------------------
+// File and Version Information:
+//      $Id$
+//
+// Description:
+//      Base class for converting FSW headers to XML
+//
+// Environment:
+//      Software developed for GLAST.
+//
+// Author List:
+//      Eric Charles
+//
+// Copyright Information:
+//      Copyright (C) 2008      Stanford Linear Accelerator Center
+//
+//---------------------------------------------------------------------------
+/** @namespace configData::fsw_enums
+    @author E. Charles
+
+*/
+
+
+namespace configData {
+
+  namespace fsw_enums {
+    enum CDM_TYPE { INVALID = 0, GFC, HFC, MFC, DFC };
+  }
+
+}
+
+#endif
