9232c4ae0b8cc954e466416be70a267c660a3cfa
kocian <>
2008-04-04 17:20:08 +0000

    added example steering files

---
+++ calibGenTRG/workdir/runTreqACD.C
@@ -0,0 +1,26 @@
+
+//
+// Example of using RTAutil to dump event info 
+//
+ {
+gROOT->Reset();    // roll back CINT context to last Save
+
+
+const char digifile[200], reconfile[200];
+
+sprintf(digifile,"/nfs/farm/g/glast/u52/Integration/rootData/077017741/v13r6p1/digi/digitization-v4r0p2_077017741_digi_DIGI.root");
+sprintf(reconfile,"/nfs/farm/g/glast/u52/Integration/rootData/077017741/v13r6p1/recon/recon-v4r0p2_077017741_recon_RECON.root");
+
+// Creating an empty TimeStruct Class
+treqACD *r = new treqACD();
+// Initialize the input files (Note: this doesn't initialize histograms)
+r->Init(digifile,reconfile);
+// No need to initialize histograms for this job
+r->inithistos();
+r->setParameters(5,31);
+
+// Set the tower mask 
+
+ r->Go(1000000);
+ r->writeoutresults("testhist.html","testhist.root");
+}
