331a5788135a3ba42b4527a87f004173e0bd102b
kocian <>
2008-04-08 22:17:20 +0000

    example to run takedata_mt

---
+++ calibGenTRG/workdir/runTakedata_1.C
@@ -0,0 +1,23 @@
+//
+// Example of using RTAutil to dump event info 
+//
+{
+gROOT->Reset();    // roll back CINT context to last Save
+
+Int_t irun = 77016135;
+
+const char digifile[200], reconfile[200];
+sprintf(digifile, "/nfs/farm/g/glast/u52/Integration/rootData/077016874/v13r6p1/digi/digitization-v4r0p2_077016874_digi_DIGI.root");
+sprintf(reconfile, "/nfs/farm/g/glast/u52/Integration/rootData/077016874/v13r6p1/recon/recon-v4r0p2_077016874_recon_RECON.root");
+
+// Creating an empty TimeStruct Class
+takedata_mt *r = new takedata_mt();
+
+// Initialize the input files (Note: this doesn't initialize histograms)
+r->Init(digifile,reconfile);
+r->setParameters(1,45,7,11);
+ r->inithistos();
+r->Go(120000);
+r->savehistos("resultstack/tackscan2-tvac-1.root");
+r->writeoutresults("resultstack/tackscan2-tvac_1.txt");
+}
