a3572fec415a6543635fac9fa85c2cf13b57c248
kocian <>
2008-06-24 17:17:53 +0000

    added cal tuple and cuts

--- calibGenTRG/workdir/runTakedata_1.C
+++ calibGenTRG/workdir/runTakedata_1.C
@@ -6,18 +6,23 @@ gROOT->Reset();    // roll back CINT context to last Save
 
 Int_t irun = 77016135;
 
-const char digifile[200], reconfile[200];
+const char digifile[200], reconfile[200], calfile[200];
 sprintf(digifile, "/nfs/farm/g/glast/u52/Integration/rootData/077016874/v13r6p1/digi/digitization-v4r0p2_077016874_digi_DIGI.root");
 sprintf(reconfile, "/nfs/farm/g/glast/u52/Integration/rootData/077016874/v13r6p1/recon/recon-v4r0p2_077016874_recon_RECON.root");
+sprintf(calfile,"/nfs/farm/g/glast/u52/Integration/rootData/077016874/v13r6p1/recon/recon-v4r0p2_077016874_cal_ntuple.root");
 
+ 
 // Creating an empty TimeStruct Class
 takedata_mt *r = new takedata_mt();
 
 // Initialize the input files (Note: this doesn't initialize histograms)
 r->Init(digifile,reconfile);
+r->initCalTuple(calfile);
 r->setParameters(1,45,7,11);
+r->useKalmanCut(true);
+r->useToTCut(true);
  r->inithistos();
-r->Go(120000);
-r->savehistos("resultstack/tackscan2-tvac-1.root");
-r->writeoutresults("resultstack/tackscan2-tvac_1.txt");
+r->Go(10000000);
+r->savehistos("acdtest-1.root");
+r->writeoutresults("acdtest_1.txt");
 }
--- calibGenTRG/workdir/runTreqCALMIP.C
+++ calibGenTRG/workdir/runTreqCALMIP.C
@@ -6,15 +6,20 @@
 gROOT->Reset();    // roll back CINT context to last Save
 
 
-const char digifile[200], reconfile[200];
+const char digifile[200], reconfile[200],calfile[200];
 
 sprintf(digifile,"/nfs/farm/g/glast/u52/Integration/rootData/077017740/v13r6p1/digi/digitization-v4r0p2_077017740_digi_DIGI.root");
 sprintf(reconfile,"/nfs/farm/g/glast/u52/Integration/rootData/077017740/v13r6p1/recon/recon-v4r0p2_077017740_recon_RECON.root");
+sprintf(calfile,"/nfs/farm/g/glast/u52/Integration/rootData/077017740/v13r6p1/recon/recon-v4r0p2_077017740_cal_ntuple.root");
 
 // Creating an empty TimeStruct Class
 treqCAL *r = new treqCAL();
 // Initialize the input files (Note: this doesn't initialize histograms)
 r->Init(digifile,reconfile);
+r->initCalTuple(calfile);
+r->useMipCut(true);
+r->useKalmanCut(true);
+r->useToTCut(true);
 // No need to initialize histograms for this job
 r->inithistos();
 r->setParameters(5,0);
