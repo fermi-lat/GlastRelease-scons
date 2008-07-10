019414772b54760f92988a067e147ace1f900f26
usher <>
2008-07-10 16:03:29 +0000

    Update to delete tracks and vertices in case that an exception is caught by the fit/vertex code.

--- TkrRecon/src/GaudiAlg/TkrReconAlg.cxx
+++ TkrRecon/src/GaudiAlg/TkrReconAlg.cxx
@@ -23,6 +23,8 @@
 #include "Event/TopLevel/Event.h"
 #include "Event/TopLevel/EventModel.h"
 #include "Event/Recon/TkrRecon/TkrCluster.h"
+#include "Event/Recon/TkrRecon/TkrTrack.h"
+#include "Event/Recon/TkrRecon/TkrVertex.h"
 #include "LdfEvent/EventSummaryData.h"
 #include "TkrRecon/Services/TkrInitSvc.h"
 
@@ -446,6 +448,21 @@ StatusCode TkrReconAlg::handleError(std::string errorString)
     log << endreq;
 
     m_errorArray.push_back(errorRec);
+
+    // Clean out tracks and vertices in the TDS so we don't confuse downstream algorithms
+    // Start with the tracks
+    SmartDataPtr<Event::TkrTrackCol> trackCol(eventSvc(),EventModel::TkrRecon::TkrTrackCol);
+    if (trackCol) 
+    {
+        while(trackCol->size()) trackCol->pop_back();
+    }
+
+    // Now get rid of the  vertices
+    SmartDataPtr<Event::TkrVertexCol> vertexCol(eventSvc(), EventModel::TkrRecon::TkrVertexCol);
+    if (vertexCol)
+    {
+        while(vertexCol->size()) vertexCol->pop_back();
+    }
 
     StatusCode sc = StatusCode::FAILURE;
     if(s_saveBadEvents) sc = StatusCode::SUCCESS;
