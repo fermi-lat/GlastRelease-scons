848c607edbed4411fe4ccf3abe752cf0d35624d3
ecephas <>
2008-08-25 20:58:10 +0000

    changed 	library = 'mootCore' to 	library = ['mootCore']
    Added env.Tool('rdbModelLib')

--- mootCore/mootCoreLib.py
+++ mootCore/mootCoreLib.py
@@ -1,8 +1,7 @@
 # $Header$
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'mootCore')
-
-
+        env.Tool('addLibrary', library = ['mootCore'])
+    env.Tool('rdbModelLib')
 def exists(env):
     return 1;
