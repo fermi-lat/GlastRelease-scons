5c4fdc701da91b79ab625c4ff28afe3c00634cee
ecephas <>
2008-08-25 19:37:58 +0000

    Added the following calls to CalDigiLib.py
         env.Tool('configDataLib')
         env.Tool('EventLib')
         env.Tool('CalUtilLib')
         env.Tool('xmlBaseLib')

--- CalDigi/CalDigiLib.py
+++ CalDigi/CalDigiLib.py
@@ -2,6 +2,9 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalDigi'])
-
+    env.Tool('configDataLib')
+    env.Tool('EventLib')
+    env.Tool('CalUtilLib')
+    env.Tool('xmlBaseLib')
 def exists(env):
     return 1;
