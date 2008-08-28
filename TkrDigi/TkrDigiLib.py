aff783671d71914ca3820ea71b77632f484587cc
ecephas <>
2008-08-28 20:21:21 +0000

    Added the following libraries:
       env.Tool('facilitiesLib')
       env.Tool('EventLib')
       env.Tool('GuiSvcLib')
       env.Tool('TkrUtilLib')

--- TkrDigi/TkrDigiLib.py
+++ TkrDigi/TkrDigiLib.py
@@ -2,6 +2,9 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['TkrDigi'])
-
+    env.Tool('facilitiesLib')
+    env.Tool('EventLib')
+    env.Tool('GuiSvcLib')
+    env.Tool('TkrUtilLib')
 def exists(env):
     return 1;
