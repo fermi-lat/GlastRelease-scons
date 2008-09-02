7665575fbe79a56ac97da1d9168f5a54c4b20b74
ecephas <>
2008-09-02 11:22:07 +0000

    Changed formatting.

--- HepRepCorba/HepRepCorbaLib.py
+++ HepRepCorba/HepRepCorbaLib.py
@@ -2,8 +2,9 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['HepRepCorba'])
-	
+    env.Tool('HepRepSvcLib')	
     if env['PLATFORM'] == 'win32':
 	env.Tool('addLibrary', library = ['oldnames'])
+    
 def exists(env):
     return 1;
