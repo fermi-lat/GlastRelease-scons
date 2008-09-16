6c4d226d944f05ed7a40abb15610e0ad887f5431
ecephas <>
2008-09-16 19:37:11 +0000

    Added the following tools:
         env.Tool('facilitiesLib')
         env.Tool('CalibDataLib')
         env.Tool('AncillaryDataEventLib')
         env.Tool('AncillaryDataUtilLib')

--- AncillaryDataRecon/AncillaryDataReconLib.py
+++ AncillaryDataRecon/AncillaryDataReconLib.py
@@ -2,6 +2,9 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AncillaryDataRecon'])
-
+    env.Tool('facilitiesLib')
+    env.Tool('CalibDataLib')
+    env.Tool('AncillaryDataEventLib')
+    env.Tool('AncillaryDataUtilLib')
 def exists(env):
     return 1;
