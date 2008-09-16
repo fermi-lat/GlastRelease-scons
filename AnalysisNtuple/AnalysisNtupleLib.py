4515b55ff726f7e0b78475351778f48b78b9bf62
ecephas <>
2008-09-16 19:34:30 +0000

    Replaced
          env.Tool('addLibrary', library = ['dl'])
          env.Tool('addLibrary', library = ['pthread'])
    
    With
          env.Tool('TkrUtilLib')
          env.Tool('ntupleWriterSvcLib')
          env.Tool('OnboardFilterLib')
          env.Tool('GlastSvcLib')
          env.Tool('FluxSvcLib')
          env.Tool('G4PropagatorLib')

--- AnalysisNtuple/AnalysisNtupleLib.py
+++ AnalysisNtuple/AnalysisNtupleLib.py
@@ -2,8 +2,11 @@
 def generate(env, **kw):
      if not kw.get('depsOnly', 0):
           env.Tool('addLibrary', library = ['AnalysisNtuple'])
-     env.Tool('addLibrary', library = ['dl'])
-     env.Tool('addLibrary', library = ['pthread'])
-
+     env.Tool('TkrUtilLib')
+     env.Tool('ntupleWriterSvcLib')
+     env.Tool('OnboardFilterLib')
+     env.Tool('GlastSvcLib')
+     env.Tool('FluxSvcLib')
+     env.Tool('G4PropagatorLib')
 def exists(env):
     return 1;
