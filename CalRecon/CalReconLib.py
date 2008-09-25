6b0c1d9f3d6de0c972b6403c27071a442a5699f7
ecephas <>
2008-09-25 21:41:14 +0000

    Added the following library calls:
         env.Tool('addLibrary', library = env['minuitLibs'])
         env.Tool('CalUtilLib')
         env.Tool('GuiSvcLib')
         env.Tool('CalXtalResponseLib')
         env.Tool('RootIoLib')
         env.Tool('mcRootDataLib')

--- CalRecon/CalReconLib.py
+++ CalRecon/CalReconLib.py
@@ -2,6 +2,11 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalRecon'])
-
+    env.Tool('addLibrary', library = env['minuitLibs'])
+    env.Tool('CalUtilLib')
+    env.Tool('GuiSvcLib')
+    env.Tool('CalXtalResponseLib')
+    env.Tool('RootIoLib')
+    env.Tool('mcRootDataLib')
 def exists(env):
     return 1;
