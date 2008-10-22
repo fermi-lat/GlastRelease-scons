c163c734f15e114c5c4cdfc1a1f1363b874da8e8
ecephas <>
2008-10-22 17:12:28 +0000

    Added the following libraries:
         env.Tool('addLibrary', library = env['obfLibs'])
         env.Tool('addLibrary', library = env['gaudiLibs'])
         env.Tool('OnboardFilterTdsLib')
         env.Tool('addLibrary', library = env['rootLibs'])

--- OnboardFilter/OnboardFilterLib.py
+++ OnboardFilter/OnboardFilterLib.py
@@ -5,6 +5,9 @@ def generate(env, **kw):
  
     env.Tool('addLibrary', library = ['dl'])
     env.Tool('addLibrary', library = ['pthread'])
-
+    env.Tool('addLibrary', library = env['obfLibs'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('OnboardFilterTdsLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
