9dfda64437fcb50eebb90c903a547245932c32a3
ecephas <>
2008-10-27 19:05:56 +0000

    Added the following:
         env.Tool('commonRootDataLib')
         env.Tool('rootUtilLib')
         env.Tool('digiRootDataLib')
         env.Tool('RootConvertLib')
         env.Tool('TriggerLib')
         env.Tool('OnboardFilterTdsLib')
         env.Tool('LdfEventLib')
         env.Tool('AncillaryDataEventLib')
         env.Tool('ntupleWriterSvcLib')
         env.Tool('gcrSelectRootDataLib')

--- RootIo/RootIoLib.py
+++ RootIo/RootIoLib.py
@@ -4,6 +4,15 @@ def generate(env, **kw):
         env.Tool('addLibrary', library = ['RootIo'])	
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
-
+    env.Tool('commonRootDataLib')
+    env.Tool('rootUtilLib')
+    env.Tool('digiRootDataLib')
+    env.Tool('RootConvertLib')
+    env.Tool('TriggerLib')
+    env.Tool('OnboardFilterTdsLib')
+    env.Tool('LdfEventLib')
+    env.Tool('AncillaryDataEventLib')
+    env.Tool('ntupleWriterSvcLib')
+    env.Tool('gcrSelectRootDataLib')
 def exists(env):
     return 1;
