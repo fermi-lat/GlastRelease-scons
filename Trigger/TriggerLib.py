420fff9b9421dde9028e0c5f613cb367eb04d423
ecephas <>
2008-10-27 18:42:03 +0000

    Added the following libraries:
    	env.Tool('addLibrary', library = env['gaudiLibs'])
         	env.Tool('addLibrary', library = env['clhepLibs'])
         	env.Tool('EventLib')
         	env.Tool('configDataLib')
         	env.Tool('CalXtalResponseLib')

--- Trigger/TriggerLib.py
+++ Trigger/TriggerLib.py
@@ -2,6 +2,10 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['Trigger'])
-
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('EventLib')
+    env.Tool('configDataLib')
+    env.Tool('CalXtalResponseLib')
 def exists(env):
     return 1;
