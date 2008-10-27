fb3ea71e2d23fa942173fec336cc7ade589509ad
ecephas <>
2008-10-27 17:49:11 +0000

    Added env.Tool('addLibrary', library = env['rootLibs'])

--- rootUtil/rootUtilLib.py
+++ rootUtil/rootUtilLib.py
@@ -2,7 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['rootUtil'])
-    env.Tool('addLibrary', library = env['rootGuiLibs'])
+    env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['minuitLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
 def exists(env):
     return 1;
