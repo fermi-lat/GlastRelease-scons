8396ed95faac0251427d4887a1036deb57cdfd24
ecephas <>
2008-10-28 16:59:34 +0000

    Changed 	env.Tool('addLibrary', library = 'ntupleWriterSvc') to         env.Tool('addLibrary', library = ['ntupleWriterSvc'])
    Added the following:
         env.Tool('facilitiesLib')
         env.Tool('addLibrary', library = env['gaudiLibs'])
         env.Tool('addLibrary', library = env['rootLibs'])

--- ntupleWriterSvc/ntupleWriterSvcLib.py
+++ ntupleWriterSvc/ntupleWriterSvcLib.py
@@ -1,8 +1,9 @@
 # $Header$
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'ntupleWriterSvc')
-
-
+        env.Tool('addLibrary', library = ['ntupleWriterSvc'])
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
