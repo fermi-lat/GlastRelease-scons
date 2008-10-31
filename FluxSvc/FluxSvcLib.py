3bc1ae1db2d9e0d06f58606b9450c30c0f3b7fce
ecephas <>
2008-10-31 15:58:41 +0000

    Added the following shared library:
    	env.Tool('celestialSourcesLib')

--- FluxSvc/FluxSvcLib.py
+++ FluxSvc/FluxSvcLib.py
@@ -2,6 +2,6 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['FluxSvc'])
-
+    env.Tool('celestialSourcesLib')
 def exists(env):
     return 1;
