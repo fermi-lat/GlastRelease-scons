b394c87128da3ca6c47d6a8c4120839fc9c8b66a
ecephas <>
2008-11-04 14:22:56 +0000

    Added the following:
         env.Tool('addLibrary', library = env['cfitsioLibs'])
         env.Tool('addLibrary', library = env['clhepLibs'])
         env.Tool('EventLib')
         env.Tool('addLibrary', library = env['xercesLibs'])
         env.Tool('astroLib')
         env.Tool('xmlBaseLib')
         env.Tool('fluxLib')

--- CRflux/CRfluxLib.py
+++ CRflux/CRfluxLib.py
@@ -2,6 +2,12 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CRflux'])
-
+    env.Tool('addLibrary', library = env['cfitsioLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('EventLib')
+    env.Tool('addLibrary', library = env['xercesLibs'])
+    env.Tool('astroLib')
+    env.Tool('xmlBaseLib')
+    env.Tool('fluxLib')
 def exists(env):
     return 1;
