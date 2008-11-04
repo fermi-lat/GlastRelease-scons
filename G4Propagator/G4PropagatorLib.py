e0940ff8e8620ffbe32930c4ce1b7ea6c12362eb
ecephas <>
2008-11-04 15:22:04 +0000

    Added env.Tool('G4GeneratorLib')

--- G4Propagator/G4PropagatorLib.py
+++ G4Propagator/G4PropagatorLib.py
@@ -2,6 +2,6 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['G4Propagator'])
-
+    env.Tool('G4GeneratorLib')
 def exists(env):
     return 1;
