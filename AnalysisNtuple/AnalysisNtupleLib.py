d45f139f86fff5295d17b3c783ca85830bd2c0cc
ecephas <>
2008-11-06 22:09:30 +0000

    Added 	env.Tool('EventLib') 	env.Tool('addLibrary', library=env['obfLibs'])       	env.Tool('RootIoLib')       	env.Tool('facilitiesLib')       	env.Tool('GlastSvcLib')       	env.Tool('AcdUtilLib')

--- AnalysisNtuple/AnalysisNtupleLib.py
+++ AnalysisNtuple/AnalysisNtupleLib.py
@@ -2,11 +2,17 @@
 def generate(env, **kw):
      if not kw.get('depsOnly', 0):
           env.Tool('addLibrary', library = ['AnalysisNtuple'])
+     env.Tool('EventLib')
      env.Tool('TkrUtilLib')
      env.Tool('ntupleWriterSvcLib')
      env.Tool('OnboardFilterLib')
      env.Tool('GlastSvcLib')
      env.Tool('FluxSvcLib')
      env.Tool('G4PropagatorLib')
+     env.Tool('addLibrary', library=env['obfLibs'])
+     env.Tool('RootIoLib')
+     env.Tool('facilitiesLib')
+     env.Tool('GlastSvcLib')
+     env.Tool('AcdUtilLib')
 def exists(env):
     return 1;
