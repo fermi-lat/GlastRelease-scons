bbac0b4cbc4e85010889ede82a1595628dd5db0a
ecephas <>
2008-11-07 16:03:00 +0000

    Added the following libraries:
    	     	env.Tool('facilitiesLib')
         		env.Tool('addLibrary', library = env['gaudiLibs'])

--- AdfEvent/AdfEventLib.py
+++ AdfEvent/AdfEventLib.py
@@ -2,7 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AdfEvent'])
-
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
 
