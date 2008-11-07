2d1b6b5145c4b65e2f1fb1f502696e98c3777fd8
ecephas <>
2008-11-07 16:24:36 +0000

    env.Tool('addLibrary', library = env['gaudiLibs'])

--- GuiSvc/GuiSvcLib.py
+++ GuiSvc/GuiSvcLib.py
@@ -3,6 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['GuiSvc'])
     env.Tool('guiLib')
-
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
