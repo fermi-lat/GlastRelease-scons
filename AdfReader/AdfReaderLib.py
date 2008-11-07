7657230ebbe81603a82711f6c59f37333916da9e
ecephas <>
2008-11-07 16:07:51 +0000

    added  env.Tool('AdfEventLib')

--- AdfReader/AdfReaderLib.py
+++ AdfReader/AdfReaderLib.py
@@ -5,6 +5,6 @@ def generate(env, **kw):
     env.Tool('facilitiesLib')
     env.Tool('AncillaryDataEventLib')
     env.Tool('AncillaryDataUtilLib')
-
+    env.Tool('AdfEventLib')
 def exists(env):
     return 1;
