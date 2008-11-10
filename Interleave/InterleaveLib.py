e77acc9e06eaf37ded492a5eb36c0fa92597fdb5
ecephas <>
2008-11-10 16:04:51 +0000

    Added      env.Tool('addLibrary', library = env['clhepLibs'])      env.Tool('FluxSvcLib')      env.Tool('CalUtilLib')      env.Tool('CalibDataLib')      env.Tool('GlastSvcLib')      env.Tool('EventLib')

--- Interleave/InterleaveLib.py
+++ Interleave/InterleaveLib.py
@@ -2,6 +2,11 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['Interleave'])
-	
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('FluxSvcLib')
+    env.Tool('CalUtilLib')
+    env.Tool('CalibDataLib')
+    env.Tool('GlastSvcLib')	
+    env.Tool('EventLib')
 def exists(env):
     return 1;
