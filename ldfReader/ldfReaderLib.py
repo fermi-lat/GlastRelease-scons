14ca8b0050eaf879ba51e2921c1d54e6214365ef
ecephas <>
2008-11-10 16:20:39 +0000

    Added      env.Tool('addLibrary', library = env['ldfLibs'])      env.Tool('addLibrary', library = env['cfitsioLibs'])      env.Tool('addLibrary', library = env['obfLibs'])      env.Tool('facilitiesLib')      env.Tool('astroLib')      env.Tool('lsfDataLib')

--- ldfReader/ldfReaderLib.py
+++ ldfReader/ldfReaderLib.py
@@ -2,6 +2,11 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = 'ldfReader')
-
+    env.Tool('addLibrary', library = env['ldfLibs'])
+    env.Tool('addLibrary', library = env['cfitsioLibs'])
+    env.Tool('addLibrary', library = env['obfLibs'])
+    env.Tool('facilitiesLib')
+    env.Tool('astroLib')
+    env.Tool('lsfDataLib')
 def exists(env):
     return 1;
