8a7ad6947cb3b69b03d683ed609024a3a10e1abe
ecephas <>
2008-11-12 19:39:29 +0000

    Added 	env.Tool('addLibrary', library = env['rootLibs'])      	env.Tool('xmlBaseLib')

--- CalUtil/CalUtilLib.py
+++ CalUtil/CalUtilLib.py
@@ -3,5 +3,7 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalUtil'])
     env.Tool('identsLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('xmlBaseLib')
 def exists(env):
     return 1;
