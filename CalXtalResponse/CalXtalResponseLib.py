e0ae527e7fd56dea0907507e600b48c8f89f67ea
ecephas <>
2008-11-12 19:41:49 +0000

    Added the following      env.Tool('CalUtilLib')      env.Tool('CalibDataLib')      env.Tool('GlastSvcLib')      env.Tool('addLibrary', library=env['rootGuiLibs'])      env.Tool('addLibrary', library=env['gaudiLibs'])      env.Tool('ntupleWriterSvcLib')      env.Tool('configDataLib')      env.Tool('LdfEventLib')      env.Tool('CalibSvcLib')      env.Tool('CalDigiLib')      env.Tool('addLibrary', library = env['obfLibs'])      env.Tool('addLibrary', library = env['clhepLibs'])

--- CalXtalResponse/CalXtalResponseLib.py
+++ CalXtalResponse/CalXtalResponseLib.py
@@ -2,6 +2,17 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalXtalResponse'])
-
+    env.Tool('CalUtilLib')
+    env.Tool('CalibDataLib')
+    env.Tool('GlastSvcLib')
+    env.Tool('addLibrary', library=env['rootGuiLibs'])
+    env.Tool('addLibrary', library=env['gaudiLibs'])
+    env.Tool('ntupleWriterSvcLib')
+    env.Tool('configDataLib')
+    env.Tool('LdfEventLib')
+    env.Tool('CalibSvcLib')
+    env.Tool('CalDigiLib')
+    env.Tool('addLibrary', library = env['obfLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
