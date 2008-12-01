acfb2ea562018991c3c7c3c0a7bece35510be107
usher <>
2008-12-01 19:21:52 +0000

    Initial import of the Overlay TDS data classes

---
+++ OverlayEvent/src/OverlayEventModel.cpp
@@ -0,0 +1,26 @@
+// File and Version Information:
+// $Header$
+
+#define _OverlayEventModel_CPP_
+
+// Define this in order to export symbols
+#define OEVT_DLL_EXPORTS
+
+#include "OverlayEvent/OverlayEventModel.h"
+#include "GaudiKernel/ClassID.h"
+
+std::string OverlayEventModel::OverlayEventHeader = "/Event";
+
+/** 
+ *  @class MC
+ *
+*/
+
+// Digi Overlay 
+std::string OverlayEventModel::Overlay::EventOverlay  = OverlayEventModel::OverlayEventHeader    + "/Overlay";
+std::string OverlayEventModel::Overlay::SrcOverlay    = OverlayEventModel::Overlay::EventOverlay + "/Src";
+std::string OverlayEventModel::Overlay::AcdOverlayCol = OverlayEventModel::Overlay::EventOverlay + "/AcdOverlayCol";
+std::string OverlayEventModel::Overlay::TkrOverlayCol = OverlayEventModel::Overlay::EventOverlay + "/TkrOverlayCol";
+std::string OverlayEventModel::Overlay::CalOverlayCol = OverlayEventModel::Overlay::EventOverlay + "/CalOverlayCol";
+std::string OverlayEventModel::Overlay::GemOverlay    = OverlayEventModel::Overlay::EventOverlay + "/GemOverlay";
+
