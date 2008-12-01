1b24679b8c9e82e5a6fdcce4e0d6686b0d2068da
usher <>
2008-12-01 22:52:36 +0000

    Update for new code and include rudimentary documentation

--- Overlay/src/Dll/Overlay_load.cxx
+++ Overlay/src/Dll/Overlay_load.cxx
@@ -9,8 +9,24 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(Overlay) {
-    DECLARE_ALGORITHM( overlayRootReaderAlg );
-    DECLARE_TOOL( BackgroundSelectTool );
+    DECLARE_ALGORITHM( AcdOverlayMergeAlg );
+    DECLARE_ALGORITHM( CalOverlayMergeAlg );
+    DECLARE_ALGORITHM( TkrOverlayMergeAlg );
+    DECLARE_ALGORITHM( DigiToOverlayAlg );
+    DECLARE_SERVICE( OverlayInputSvc );
+    DECLARE_SERVICE( OverlayOutputSvc );
     DECLARE_TOOL( McIlwain_L_Tool );
+    DECLARE_TOOL( EventToOverlayTool );
+    DECLARE_TOOL( CalXtalToOverlayTool );
+    DECLARE_TOOL( TkrDigiToOverlayTool );
+    DECLARE_TOOL( AcdHitToOverlayTool );
+    DECLARE_TOOL( GemToOverlayTool );
+    DECLARE_CONVERTER( SrcOverlayCnv );
+    DECLARE_CONVERTER( EventOverlayCnv );
+    DECLARE_CONVERTER( TkrOverlayCnv );
+    DECLARE_CONVERTER( CalOverlayCnv );
+    DECLARE_CONVERTER( AcdOverlayCnv );
+    DECLARE_CONVERTER( GemOverlayCnv );
+//    DECLARE_CONVERTER( LdfGemCnv );
 }
   
