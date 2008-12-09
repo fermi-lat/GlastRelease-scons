798e1cbaf0131a7ebac7423c296c17d6e7a68216
ecephas <>
2008-12-09 00:08:38 +0000

    Check in TkrReconLib.py for TkrRecon v10r16p2 (branch-tag, GlastReleasev15r49).

--- TkrRecon/TkrReconLib.py
+++ TkrRecon/TkrReconLib.py
@@ -2,9 +2,16 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['TkrRecon'])
+    env.Tool('GuiSvcLib')
     env.Tool('CalUtilLib')
     env.Tool('CalibDataLib')
     env.Tool('GlastSvcLib')
     env.Tool('guiLib')
+    env.Tool('EventLib')
+    env.Tool('TkrUtilLib')
+    env.Tool('LdfEventLib')
+    env.Tool('geometryLib')
+    env.Tool('RootIoLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
