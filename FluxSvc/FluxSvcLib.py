ccc7ef4bd41319963359a10dee26f1b5b1a299ed
ecephas <>
2008-12-09 00:08:21 +0000

    Check in FluxSvcLib.py for FluxSvc v6r53 (branch-tag, GlastReleasev15r49).

--- FluxSvc/FluxSvcLib.py
+++ FluxSvc/FluxSvcLib.py
@@ -3,5 +3,10 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['FluxSvc'])
     env.Tool('celestialSourcesLib')
+    env.Tool('f2cLib')
+    env.Tool('tipLib')
+    env.Tool('fluxLib')
+    env.Tool('TriggerLib')
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
