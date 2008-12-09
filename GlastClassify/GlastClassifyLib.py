649b6cc563dac85c074bf26e935c62b879f97680
ecephas <>
2008-12-09 00:08:27 +0000

    Check in GlastClassifyLib.py for GlastClassify v6r5p2 (branch-tag, GlastReleasev15r49).

--- GlastClassify/GlastClassifyLib.py
+++ GlastClassify/GlastClassifyLib.py
@@ -2,6 +2,9 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['GlastClassify'])
-
+    env.Tool('facilitiesLib')
+    env.Tool('classifierLib')
+    env.Tool('ntupleWriterSvcLib')
+    env.Tool('xmlBaseLib')
 def exists(env):
     return 1;
