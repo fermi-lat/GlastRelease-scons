c259120819096e479e275539be0356d0529f2cf8
ecephas <>
2008-12-09 00:08:43 +0000

    Check in configDataLib.py for configData v1r10p2 (branch-tag, GlastReleasev15r49).

--- configData/configDataLib.py
+++ configData/configDataLib.py
@@ -4,5 +4,8 @@ def generate(env, **kw):
         env.Tool('addLibrary', library = ['configData'])
     env.Tool('mootCoreLib')
     env.Tool('commonRootDataLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['pythonLibs'])
+    env.Tool('lsfDataLib')
 def exists(env):
     return 1;
