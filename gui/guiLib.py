e91dbf0d1d9d39829d62a7572aa9ca6ea7a67ec9
ecephas <>
2008-12-09 00:08:49 +0000

    Check in guiLib.py for gui v3r7p2 (branch-tag, GlastReleasev15r49).

--- gui/guiLib.py
+++ gui/guiLib.py
@@ -2,7 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['gui'])
-	env.Tool('addLibrary', library = ['guisystem'])
-
+    env.Tool('addLibrary', library = ['guisystem'])
+    env.Tool('addLibrary', library = env['x11Libs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
