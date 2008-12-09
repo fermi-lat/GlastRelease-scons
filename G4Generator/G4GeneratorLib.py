e25e6d424a840ecb13d2adccffde2abf3b4818b7
ecephas <>
2008-12-09 00:08:23 +0000

    Check in G4GeneratorLib.py for G4Generator v5r24 (branch-tag, GlastReleasev15r49).

--- G4Generator/G4GeneratorLib.py
+++ G4Generator/G4GeneratorLib.py
@@ -2,6 +2,13 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['G4Generator'])
-
+    env.Tool('fluxLib')
+    env.Tool('G4HadronSimLib')
+    env.Tool('EventLib')
+    env.Tool('addLibrary', library = env['geant4Libs'])
+    env.Tool('GlastSvcLib')
+    env.Tool('geometryLib')
+    env.Tool('GlastMSLib')
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
