37bb039b66d6ccd76b2dd4cfb80faac09ab86c1e
ecephas <>
2009-01-24 17:46:06 +0000

    Added the following:
         env.Tool('addLibrary', library = env['ldfLibs'])
         env.Tool('facilitiesLib')
         env.Tool('eventFileLib')
         env.Tool('lsfDataLib')
         env.Tool('astroLib')

--- ldfReader/ldfReaderLib.py
+++ ldfReader/ldfReaderLib.py
@@ -1,12 +1,11 @@
 # $Header$
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'ldfReader')
+        env.Tool('addLibrary', library = ['ldfReader'])
     env.Tool('addLibrary', library = env['ldfLibs'])
-    env.Tool('addLibrary', library = env['cfitsioLibs'])
-    env.Tool('addLibrary', library = env['obfLibs'])
     env.Tool('facilitiesLib')
-    env.Tool('astroLib')
+    env.Tool('eventFileLib')
     env.Tool('lsfDataLib')
+    env.Tool('astroLib')
 def exists(env):
     return 1;
