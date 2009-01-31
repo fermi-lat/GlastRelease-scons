7769a0546b8c7a58f60dd26d805ce1dab57f6b0b
lsrea <>
2009-01-31 17:04:11 +0000

    Add a method to recalculate the ToT for existing clusters

--- TkrUtil/TkrUtil/ITkrMakeClustersTool.h
+++ TkrUtil/TkrUtil/ITkrMakeClustersTool.h
@@ -17,7 +17,7 @@
 
 
 // Declaration of the interface ID ( interface id, major version, minor version) 
-static const InterfaceID IID_ITkrMakeClustersTool("ITkrMakeClustersTool", 0 , 0); 
+static const InterfaceID IID_ITkrMakeClustersTool("ITkrMakeClustersTool", 1 , 0); 
 
 /** @class ITkrMakeClustersTool
 * @brief Abstract interface for methods to query TkrClusters
@@ -34,6 +34,7 @@ public:
         Event::TkrIdClusterMap* clusMap,
         Event::TkrDigiCol* pTkrDigiCol,
         ITkrBadStripsSvc::clusterType clType=ITkrBadStripsSvc::STANDARDCLUSTERS) = 0;
+    virtual StatusCode calculateToT() = 0;
 
 };
 
