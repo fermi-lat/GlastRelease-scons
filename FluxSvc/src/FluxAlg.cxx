219506d27efa5080ea8756cc7b4e64e879497d86
lsrea <>
2009-03-18 01:01:15 +0000

    Removed all code that created Pt* variables in the merit tuple This is now done in AnalysisNtuple::PtValsAlg

--- FluxSvc/src/FluxAlg.cxx
+++ FluxSvc/src/FluxAlg.cxx
@@ -116,8 +116,8 @@ private:
     
     StringArrayProperty m_pointingHistory;///< history file name and launch date
 
-    StringProperty m_root_tree;
-    BooleanProperty m_save_tuple; // set true to save
+    //StringProperty m_root_tree;
+    //BooleanProperty m_save_tuple; // set true to save
     BooleanProperty m_avoidSAA;
 
 
@@ -166,8 +166,8 @@ FluxAlg::FluxAlg(const std::string& name, ISvcLocator* pSvcLocator)
     declareProperty("PointingHistory",  m_pointingHistory); // doublet, filename and launch date
 
 // deprecate these
-    declareProperty("pointing_info_tree_name",  m_root_tree="");
-    declareProperty("save_pointing_info",  m_save_tuple=false);
+    //declareProperty("pointing_info_tree_name",  m_root_tree="");
+    //declareProperty("save_pointing_info",  m_save_tuple=false);
 
     declareProperty("AvoidSAA",   m_avoidSAA=false);
     declareProperty("Prescale",   m_prescale=1);
@@ -337,13 +337,14 @@ StatusCode FluxAlg::initialize(){
     }
 
     // get a pointer to RootTupleSvc, use only if available 
-    if( (service("RootTupleSvc", m_rootTupleSvc, true) ). isFailure() ) {
-        log << MSG::WARNING << " RootTupleSvc is not available, will not write Pt tuple" << endreq;
-        m_rootTupleSvc=0;
-    }else if( !m_root_tree.value().empty() ) {
-        
-        m_pointing_info.setPtTuple(m_rootTupleSvc, m_root_tree.value());
-    }
+    // this is done in PtValsAlg now
+    //if( (service("RootTupleSvc", m_rootTupleSvc, true) ). isFailure() ) {
+    //    log << MSG::WARNING << " RootTupleSvc is not available, will not write Pt tuple" << endreq;
+    //    m_rootTupleSvc=0;
+    //}else if( !m_root_tree.value().empty() ) {
+    //    
+    //    m_pointing_info.setPtTuple(m_rootTupleSvc, m_root_tree.value());
+    //}
 
 
     // attach an observer to be notified when orbital position changes
@@ -525,9 +526,10 @@ StatusCode FluxAlg::execute()
     m_pointing_info.set();
     
     // put pointing stuff into the root tree
-    if( m_rootTupleSvc!=0 && !m_root_tree.value().empty()){
-        m_rootTupleSvc->storeRowFlag(this->m_root_tree.value(), m_save_tuple);
-    }
+    //  done in PtValsAlg now
+    //if( m_rootTupleSvc!=0 && !m_root_tree.value().empty()){
+    //    m_rootTupleSvc->storeRowFlag(this->m_root_tree.value(), m_save_tuple);
+    //}
 
     if( m_initialTime==0) m_initialTime=m_currentTime;
     h->setTime(m_currentTime);
