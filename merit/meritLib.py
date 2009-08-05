6f0d2e9d68a2964e767f7ff1be96e0fe6a3271b8
jrb <>
2009-08-05 23:04:20 +0000

    Value for 'library' arg of addLibrary must be a list

--- merit/meritLib.py
+++ merit/meritLib.py
@@ -1,7 +1,7 @@
 # $Header$
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'merit')
+        env.Tool('addLibrary', library = ['merit'])
 
     env.Tool('addLibrary', library = env['rootLibs'])
 
