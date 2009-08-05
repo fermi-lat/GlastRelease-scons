9e1cd202123d5057cf916506826e93f068850cba
jrb <>
2009-08-05 21:03:59 +0000

    Take out unnecessary dependency on f2c

--- FluxSvc/FluxSvcLib.py
+++ FluxSvc/FluxSvcLib.py
@@ -3,7 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['FluxSvc'])
     env.Tool('celestialSourcesLib')
-    env.Tool('f2cLib')
     env.Tool('tipLib')
     env.Tool('fluxLib')
     env.Tool('TriggerLib')
