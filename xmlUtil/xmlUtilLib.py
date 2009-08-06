261098d69853ecc4219420247a7232f3b63d5614
jrb <>
2009-08-06 23:59:48 +0000

    SConscript:  use registerTargets xmlUtilLib.py: explicitly mention everything directly used

--- xmlUtil/xmlUtilLib.py
+++ xmlUtil/xmlUtilLib.py
@@ -3,6 +3,8 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['xmlUtil'])
     env.Tool('xmlBaseLib')
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['xercesLibs'])
 
 def exists(env):
     return 1;
