3f3dbea9326b968ce3238021621dc683a94710f9
jrb <>
2009-08-07 01:30:27 +0000

    Use registerTargets; add libraries appropriately .cvsignore:  add SCons build products

--- CalibData/CalibDataLib.py
+++ CalibData/CalibDataLib.py
@@ -3,6 +3,7 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalibData'])
     env.Tool('identsLib')
+    env.Tool('facilitiesLib')
 
 def exists(env):
     return 1;
