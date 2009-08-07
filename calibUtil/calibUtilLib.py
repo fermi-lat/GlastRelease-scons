53967d02c5e6987f276c26dee53fc95b7dc828cf
jrb <>
2009-08-07 22:04:44 +0000

    SConscript: use registerTargets calibUtilLib.py:  add all libraries directly referenced .cvsignore: add SCons build products

--- calibUtil/calibUtilLib.py
+++ calibUtil/calibUtilLib.py
@@ -3,7 +3,8 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibUtil'])
     env.Tool('facilitiesLib')
+    env.Tool('xmlBaseLib')
     env.Tool('rdbModelLib')
-
+    env.Tool('addLibrary', library = env['xercesLibs'])
 def exists(env):
     return 1;
