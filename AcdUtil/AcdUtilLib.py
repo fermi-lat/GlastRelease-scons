16f7039bb1b29a6c2473c26a40f4ec7ac95ffd1c
jrb <>
2009-08-08 01:08:05 +0000

    SConscript:  use registerTargets AcdUtilLib.py:  add all the libraries showing up in ldd of shareable .cvsignore:   add SCons build products

--- AcdUtil/AcdUtilLib.py
+++ AcdUtil/AcdUtilLib.py
@@ -6,7 +6,17 @@ def generate(env, **kw):
     env.Tool('CalibDataLib')
     env.Tool('geometryLib')
     env.Tool('xmlBaseLib')
+    env.Tool('rdbModelLib')
+    env.Tool('calibUtilLib')
     env.Tool('EventLib')
     env.Tool('GlastSvcLib')
+    env.Tool('mootCoreLib')
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['mysqlLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('addLibrary', library = env['cppunitLibs'])
+    env.Tool('addLibrary', library = env['xercesLibs'])
+
 def exists(env):
     return 1;
