1f4e7a436189927e41d9ab9452a396e9f9ef48de
jrb <>
2009-08-25 23:24:21 +0000

    lsfDataLib.py:  take out dependence on ldfLibs, needed only by dumpEvent SConscript:     separate env for dumpEvent adds ldfLibs;                 convert to registerTargets .cvsignore:     ignore build, *.pyc

--- lsfData/lsfDataLib.py
+++ lsfData/lsfDataLib.py
@@ -3,6 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['lsfData'])
     env.Tool('eventFileLib')
-    env.Tool('addLibrary', library = env['ldfLibs'])
+    # env.Tool('addLibrary', library = env['ldfLibs'])
 def exists(env):
     return 1;
