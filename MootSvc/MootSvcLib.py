103b14d5902e2a79861a7de0c9599651474289a3
jrb <>
2009-08-27 16:39:00 +0000

    SConscript:  convert to use registerTargets MootSvcLib.py:  include all dependencies .cvsignore:     ignore build, *.pyc

--- MootSvc/MootSvcLib.py
+++ MootSvc/MootSvcLib.py
@@ -2,6 +2,12 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['MootSvc'])
-	
+
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('facilitiesLib')
+    env.Tool('xmlBaseLib')
+    env.Tool('EventLib')
+    env.Tool('LdfEventLib')
+    env.Tool('mootCoreLib')
 def exists(env):
     return 1;
