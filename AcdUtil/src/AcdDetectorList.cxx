7d908b17856e003d5833dde19b44f042a0d2113d
heather <>
2009-09-10 16:56:47 +0000

    warnings fix for unused params

--- AcdUtil/src/AcdDetectorList.cxx
+++ AcdUtil/src/AcdDetectorList.cxx
@@ -8,10 +8,10 @@
 namespace AcdUtil {
 
 IGeometry::VisitorRet 
-AcdDetectorList::pushShape(ShapeType s, const UintVector& idvec, 
-                       std::string name, std::string material, 
-                       const DoubleVector& params, 
-                       VolumeType type, SenseType sense)
+AcdDetectorList::pushShape(ShapeType /*s*/, const UintVector& idvec, 
+                       std::string name, std::string /*material*/, 
+                       const DoubleVector& /*params*/, 
+                       VolumeType /*type*/, SenseType /*sense*/)
 {
     // concatenate the id for this node to current id.
     m_idcount.push_back(idvec.size());
