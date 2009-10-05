332cb2d61198d81ced477e73a9106f76fd2d087e
usher <>
2009-10-05 22:58:35 +0000

    Complete overhaul of the Vector Links pat rec, initial import

--- TkrRecon/src/PatRec/VectorLinks/VectorLinkMaps.h
+++ TkrRecon/src/PatRec/VectorLinks/VectorLinkMaps.h
@@ -12,20 +12,20 @@
 #ifndef VectorLinkMaps_h
 #define VectorLinkMaps_h
 
-#include "TrackElements.h"
-#include "VecPointsLink.h"
+#include "Event/Recon/TkrRecon/TkrTrackElements.h"
+#include "Event/Recon/TkrRecon/TkrVecPointsLink.h"
 #include "Event/Recon/TkrRecon/TkrCluster.h"
 
 /// Define a map between a TrackElement and all associated TkrPointsLinks 
 /// In this case, TrackElement is the key in this map
-typedef std::map< const TrackElements*, VecPointsLinkPtrVec> TrackElementToLinksMap;
-typedef std::pair<const TrackElements*, VecPointsLinkPtrVec> TrackElementToLinkPair;
+typedef std::map< const Event::TkrTrackElements*, Event::TkrVecPointsLinkPtrVec> TrackElementToLinksMap;
+typedef std::pair<const Event::TkrTrackElements*, Event::TkrVecPointsLinkPtrVec> TrackElementToLinkPair;
 
 /// Define a reverse map taking us from a TrkPointsLink to all TrackElements it 
 /// is associated with. 
 /// In this case, TkrPointsLink is the key in this map
-typedef std::map< const VecPointsLink*, TrackElementsPtrVec> VecLinksToElementsMap;
-typedef std::pair<const VecPointsLink*, TrackElementsPtrVec> VecLinksToelementsPair;
+typedef std::map< const Event::TkrVecPointsLink*, Event::TkrTrackElementsPtrVec> VecLinksToElementsMap;
+typedef std::pair<const Event::TkrVecPointsLink*, Event::TkrTrackElementsPtrVec> VecLinksToelementsPair;
 
 #endif
 
