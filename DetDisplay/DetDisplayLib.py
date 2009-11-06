ba73ff95449c603a877a0faf030e06e01cc74f67
jrb <>
2009-11-06 23:31:01 +0000

    changes for SCons

--- DetDisplay/DetDisplayLib.py
+++ DetDisplay/DetDisplayLib.py
@@ -3,5 +3,7 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['DetDisplay'])
     env.Tool('TkrUtilLib')
+    env.Tool('GlastSvcLib')
+    env.Tool('geomrepLib')
 def exists(env):
     return 1;
