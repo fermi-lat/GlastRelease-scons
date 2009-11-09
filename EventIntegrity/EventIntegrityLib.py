c26d4a8a025e66e801922c350687f72bc5a82367
jrb <>
2009-11-09 23:59:17 +0000

    Updates for SCons

--- EventIntegrity/EventIntegrityLib.py
+++ EventIntegrity/EventIntegrityLib.py
@@ -3,5 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['EventIntegrity'])
     env.Tool('GlastSvcLib')
+    env.Tool('LdfEventLib')
 def exists(env):
     return 1;
