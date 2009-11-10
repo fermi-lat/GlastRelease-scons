f0603270ed9a16ddc4c3698d7df58b91f58f2427
jrb <>
2009-11-10 01:56:22 +0000

    Updates for SCons

--- ConfigSvc/ConfigSvcLib.py
+++ ConfigSvc/ConfigSvcLib.py
@@ -3,5 +3,11 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['ConfigSvc'])
 
+    env.Tool('configDataLib')
+    env.Tool('MootSvcLib')
+    env.Tool('EventLib')
+    env.Tool('LdfEventLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+
 def exists(env):
     return 1;
