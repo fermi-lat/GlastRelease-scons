7df335f44ab3f654ae77722bc9ec0ca9c3b0cd14
jrb <>
2009-11-10 23:11:24 +0000

    Add gaudiLibs, clhepLibs

--- CalibData/CalibDataLib.py
+++ CalibData/CalibDataLib.py
@@ -2,8 +2,9 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalibData'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('identsLib')
     env.Tool('facilitiesLib')
-
 def exists(env):
     return 1;
