53e604553e8962da728391ebbd2fbb4965253f30
jrb <>
2009-11-10 01:20:50 +0000

    Updates for SCons

--- classifier/classifierLib.py
+++ classifier/classifierLib.py
@@ -3,5 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['classifier'])
     env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
