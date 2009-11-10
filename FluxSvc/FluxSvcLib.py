9d5296e7d24456b95844d5e77b0cb788c7eba709
jrb <>
2009-11-10 23:35:11 +0000

    Updates for SCons

--- FluxSvc/FluxSvcLib.py
+++ FluxSvc/FluxSvcLib.py
@@ -2,10 +2,16 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['FluxSvc'])
-    env.Tool('celestialSourcesLib')
-    env.Tool('tipLib')
-    env.Tool('fluxLib')
     env.Tool('TriggerLib')
+    env.Tool('ntupleWriterSvcLib')
+    env.Tool('EventLib')
+    env.Tool('GlastSvcLib')
+    env.Tool('celestialSourcesLib')
+    env.Tool('fluxLib')
+    env.Tool('astroLib')
+    env.Tool('tipLib')
+    env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
