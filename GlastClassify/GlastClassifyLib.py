db77c59a4dc236c1dc3c146a11e0b4fa1c963f51
jrb <>
2009-11-21 00:35:20 +0000

    Updates for SCons

--- GlastClassify/GlastClassifyLib.py
+++ GlastClassify/GlastClassifyLib.py
@@ -1,10 +1,13 @@
 # $Header$
 def generate(env, **kw):
-    if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = ['GlastClassify'])
-    env.Tool('facilitiesLib')
+    if not kw.get('noGaudi', 0):
+        env.Tool('addLibrary', library = env['gaudiLibs'])
+        if not kw.get('depsOnly', 0):
+            env.Tool('addLibrary', library = ['GlastClassify'])
     env.Tool('classifierLib')
     env.Tool('ntupleWriterSvcLib')
     env.Tool('xmlBaseLib')
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['xercesLibs'])
 def exists(env):
     return 1;
