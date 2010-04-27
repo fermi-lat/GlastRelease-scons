65fbf117a12ed842c2de631b084100e97dc139f9
usher <>
2010-04-27 16:49:08 +0000

    Updates to include DiagnosticData into the Overlay

--- OverlayEvent/src/OverlayEventModel.cpp
+++ OverlayEvent/src/OverlayEventModel.cpp
@@ -17,11 +17,12 @@ std::string OverlayEventModel::OverlayEventHeader = "/Event";
 */
 
 // Digi Overlay 
-std::string OverlayEventModel::Overlay::EventOverlay  = OverlayEventModel::OverlayEventHeader    + "/Overlay";
-std::string OverlayEventModel::Overlay::SrcOverlay    = OverlayEventModel::Overlay::EventOverlay + "/Src";
-std::string OverlayEventModel::Overlay::AcdOverlayCol = OverlayEventModel::Overlay::EventOverlay + "/AcdOverlayCol";
-std::string OverlayEventModel::Overlay::TkrOverlayCol = OverlayEventModel::Overlay::EventOverlay + "/TkrOverlayCol";
-std::string OverlayEventModel::Overlay::CalOverlayCol = OverlayEventModel::Overlay::EventOverlay + "/CalOverlayCol";
-std::string OverlayEventModel::Overlay::GemOverlay    = OverlayEventModel::Overlay::EventOverlay + "/GemOverlay";
-std::string OverlayEventModel::Overlay::PtOverlay     = OverlayEventModel::Overlay::EventOverlay + "/PtOverlay";
+std::string OverlayEventModel::Overlay::EventOverlay    = OverlayEventModel::OverlayEventHeader    + "/Overlay";
+std::string OverlayEventModel::Overlay::SrcOverlay      = OverlayEventModel::Overlay::EventOverlay + "/Src";
+std::string OverlayEventModel::Overlay::AcdOverlayCol   = OverlayEventModel::Overlay::EventOverlay + "/AcdOverlayCol";
+std::string OverlayEventModel::Overlay::TkrOverlayCol   = OverlayEventModel::Overlay::EventOverlay + "/TkrOverlayCol";
+std::string OverlayEventModel::Overlay::CalOverlayCol   = OverlayEventModel::Overlay::EventOverlay + "/CalOverlayCol";
+std::string OverlayEventModel::Overlay::GemOverlay      = OverlayEventModel::Overlay::EventOverlay + "/GemOverlay";
+std::string OverlayEventModel::Overlay::DiagDataOverlay = OverlayEventModel::Overlay::EventOverlay + "/DiagDataOverlay";
+std::string OverlayEventModel::Overlay::PtOverlay       = OverlayEventModel::Overlay::EventOverlay + "/PtOverlay";
 
