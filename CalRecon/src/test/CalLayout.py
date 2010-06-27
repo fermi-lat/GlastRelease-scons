07d724b34439719cf30bb1192152437d2cd0e9ba
lbaldini <>
2010-06-27 12:44:02 +0000

    Top view added.

--- CalRecon/src/test/CalLayout.py
+++ CalRecon/src/test/CalLayout.py
@@ -8,7 +8,21 @@
 import ROOT
 ROOT.gStyle.SetCanvasColor(ROOT.kWhite)
 ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptTitle(0)
 ROOT.gStyle.SetFrameBorderMode(0)
+
+TEXT_STYLE   = 43
+TEXT_SIZE    = 18
+TITLE_SIZE   = 18
+LABEL_SIZE   = 16
+ROOT.gStyle.SetTextFont(TEXT_STYLE)
+ROOT.gStyle.SetTextSize(TEXT_SIZE)
+ROOT.gStyle.SetLabelFont(TEXT_STYLE, 'XY')
+ROOT.gStyle.SetLabelSize(LABEL_SIZE, 'XY')
+ROOT.gStyle.SetTitleFont(TEXT_STYLE, 'XY')
+ROOT.gStyle.SetTitleSize(TITLE_SIZE, 'XY')
+
+
 
 from math import sqrt
 
@@ -46,28 +60,34 @@ def getCalLayerZ(layer):
     return CAL_LAYER_Z_DICT[layer]
 
 
-X_MAX = 2*TOWER_PITCH
+X_MAX = 2.1*TOWER_PITCH
 X_MIN = -X_MAX
-Y_MAX = 0
-Y_MIN = -250
-
-#print X_MAX - X_MIN, Y_MAX - Y_MIN
-
+Z_MAX = 0
+Z_MIN = -250
 MODULE_COLOR = ROOT.kGray
 LOG_COLOR = ROOT.kGray
 
 
-def getCanvas(name, title = None):
+def getSideCanvas(name, title = None, width = 1200, height = 480):
     title = title or name
     rightMargin = 0.01
-    leftMargin = 0.04
-    c = ROOT.TCanvas(name, title, 1200, 480)
+    leftMargin = 0.05
+    c = ROOT.TCanvas(name, title, width, height)
     c.Divide(1, 2)
     c.GetPad(1).SetRightMargin(rightMargin)
     c.GetPad(1).SetLeftMargin(leftMargin)
     c.GetPad(2).SetRightMargin(rightMargin)
     c.GetPad(2).SetLeftMargin(leftMargin)
     return c
+
+def getTopCanvas(name, title = None, width = 600, height = 600):
+    title = title or name
+    c = ROOT.TCanvas(name, title, width, height)
+    c.SetRightMargin(0.01)
+    c.SetBottomMargin(0.08)
+    c.SetTopMargin(0.075)
+    return c
+
 
 
 class Box(ROOT.TBox):
@@ -91,7 +111,7 @@ class Box(ROOT.TBox):
 
 
 
-class CalModule:
+class CalSideModule:
 
     def __init__(self, xcenter, view):
         self.ModuleBox = Box(xcenter, CAL_MODULE_VCENTER, CAL_MODULE_WIDTH,
@@ -126,54 +146,73 @@ class CalModule:
         self.ModuleBox.draw()
         for log in self.Logs:
             log.draw()
+
+
+
+class CalTopModule:
+
+    def __init__(self, xcenter, ycenter):
+        self.ModuleBox = Box(xcenter, ycenter, CAL_MODULE_WIDTH,
+                             CAL_MODULE_WIDTH, MODULE_COLOR)
+
+    def draw(self):
+        self.ModuleBox.draw()
         
         
 
 class CalLayout:
 
     def __init__(self):
-        labelSize = 0.07
-        titleSize = 0.07
-        xTitleOffset = 0.50
-        yTitleOffset = 0.30
         self.BaseHistogramXZ = ROOT.TH1F('hxz', 'XZ view', 100, X_MIN, X_MAX)
-        self.BaseHistogramXZ.SetMinimum(Y_MIN)
-        self.BaseHistogramXZ.SetMaximum(Y_MAX)
-        self.BaseHistogramXZ.GetXaxis().SetLabelSize(labelSize)
-        self.BaseHistogramXZ.GetXaxis().SetTitleSize(titleSize)
-        self.BaseHistogramXZ.GetXaxis().SetTitleOffset(xTitleOffset)
+        self.BaseHistogramXZ.SetMinimum(Z_MIN)
+        self.BaseHistogramXZ.SetMaximum(Z_MAX)
         self.BaseHistogramXZ.GetXaxis().SetTitle('x (mm)')
-        self.BaseHistogramXZ.GetYaxis().SetLabelSize(labelSize)
-        self.BaseHistogramXZ.GetYaxis().SetTitleSize(titleSize)
-        self.BaseHistogramXZ.GetYaxis().SetTitleOffset(yTitleOffset)
         self.BaseHistogramXZ.GetYaxis().SetTitle('z (mm)')
+        self.BaseHistogramXZ.GetYaxis().SetTitleOffset(0.60)
         self.BaseHistogramYZ = self.BaseHistogramXZ.Clone('hyz')
         self.BaseHistogramYZ.SetTitle('YZ view')
         self.BaseHistogramYZ.GetXaxis().SetTitle('y (mm)')
+        self.BaseHistogramXY = self.BaseHistogramXZ.Clone('hxy')
+        self.BaseHistogramYZ.SetTitle('XY view')
+        self.BaseHistogramXY.SetMinimum(X_MIN)
+        self.BaseHistogramXY.SetMaximum(X_MAX)
+        self.BaseHistogramXY.GetXaxis().SetTitle('x (mm)')
+        self.BaseHistogramXY.GetYaxis().SetTitle('y (mm)')
+        self.BaseHistogramXY.GetYaxis().SetTitleOffset(1.5)
         self.Modules = {'xz': [],
-                        'yz': []}
+                        'yz': [],
+                        'xy': []
+                        }
         for i in range(4):
             xcenter = (i - 1.5)*TOWER_PITCH
             for view in ['xz', 'yz']:
-                self.Modules[view].append(CalModule(xcenter, view))
+                self.Modules[view].append(CalSideModule(xcenter, view))
+            for j in range(4):
+                ycenter = (j - 1.5)*TOWER_PITCH
+                self.Modules['xy'].append(CalTopModule(xcenter, ycenter))
 
     def draw(self, view = 'xz'):
         if view == 'xz':
             self.BaseHistogramXZ.Draw()
             for module in self.Modules['xz']:
                 module.draw()
-        else:
+        elif view == 'yz':
             self.BaseHistogramYZ.Draw()
             for module in self.Modules['yz']:
                 module.draw()
-
+        elif view == 'xy':
+            self.BaseHistogramXY.Draw()
+            for module in self.Modules['xy']:
+                module.draw()
 
 
 
 if __name__ == '__main__':
     cal = CalLayout()
-    c = getCanvas('test')
-    c.cd(1); cal.draw('xz')
-    c.cd(2); cal.draw('yz')
-    c.cd(); c.Update()
-    
+    c1 = getSideCanvas('Side')
+    c1.cd(1); cal.draw('xz')
+    c1.cd(2); cal.draw('yz')
+    c1.cd(); c1.Update()
+    c2 = getTopCanvas('Top')
+    cal.draw('xy')
+    c2.Update()
