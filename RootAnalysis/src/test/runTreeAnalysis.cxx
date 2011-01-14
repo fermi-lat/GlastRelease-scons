f9043deae5143dbde4a395293a45cac0836aa112
lsrea <>
2011-01-14 00:18:24 +0000

    Get the test job running again

--- RootAnalysis/src/test/runTreeAnalysis.cxx
+++ RootAnalysis/src/test/runTreeAnalysis.cxx
@@ -15,16 +15,23 @@
 #include <iostream>
 #include <string>
 #include "facilities/commonUtilities.h"
+#include "facilities/Util.h"
+
 int main(int argn, char** argc) {
 
 #ifdef WIN32
     gSystem->Load("libTree.dll");
     gSystem->Load("libTreePlayer.dll");
     gSystem->Load("reconRootData.dll");
+    gSystem->Load("mcRootData.dll");
 #endif
-    Long64_t numEvents = 25;
-    std::string dataPath = 
-      facilities::commonUtilities::getDataPath("rootTestData");
+
+    Long64_t numEvents = 15;
+    
+    facilities::commonUtilities::setupEnvironment();
+    
+    std::string dataPath("$(ROOTTESTDATADATAPATH)");
+    facilities::Util::expandEnvVar(&dataPath);
     if ( argn > 5) numEvents = atoi(argc[5]);
     std::string digiFileName(dataPath);
     digiFileName += "/default/digi.root";
@@ -38,13 +45,14 @@ int main(int argn, char** argc) {
     std::string histFileName = "Histograms.root";
     if (argn > 4 && strlen(argc[4]) > 0)
         histFileName = argc[4];
+    std::cout << "Will process a maximum of " << numEvents << " events" << std::endl;
     std::cout << "Setup for Processing" << std::endl;
     RootTreeAnalysis r(digiFileName.c_str(), reconFileName.c_str(),
                        mcFileName.c_str(),
                        const_cast<char*>(histFileName.c_str()));
     std::cout << "        histogram: " << r.m_histFileName << std::endl;
     r.Go(numEvents);
-    std::cout << "Done Processing " << numEvents << " Events" << std::endl;
+    std::cout << "Done" << std::endl;
     r.WriteHist();
 
     return 0;
