7848dced8b6f65904df2f01408d535a309c3c375
lsrea <>
2011-07-15 04:24:15 +0000

    Fix the mix-up of slopes that leads to bad ToT values

--- AnalysisNtuple/src/TkrValsTool.cxx
+++ AnalysisNtuple/src/TkrValsTool.cxx
@@ -1222,8 +1222,8 @@ StatusCode TkrValsTool::calculate()
             const Event::TkrCluster* cluster = hit->getClusterPtr();
             int size =  (int) (const_cast<Event::TkrCluster*>(cluster))->size();
             // get the local slopes
-            double slope  = fabs(hit->getMeasuredSlope(Event::TkrTrackHit::SMOOTHED));
-            double slope1 = fabs(hit->getNonMeasuredSlope(Event::TkrTrackHit::SMOOTHED));
+            double slope1  = fabs(hit->getMeasuredSlope(Event::TkrTrackHit::SMOOTHED));
+            double slope = fabs(hit->getNonMeasuredSlope(Event::TkrTrackHit::SMOOTHED));
 
             // theta1 is the projected angle across the strip
             double theta1       = atan(slope1);
