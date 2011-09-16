8e43c48621b4427097a657f26fe9d22d2efe73b1
heather <>
2011-09-16 04:15:02 +0000

    patches for windows

--- gui/guiLib.py
+++ gui/guiLib.py
@@ -3,7 +3,10 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['gui'])
     env.Tool('addLibrary', library = ['guisystem'])
-    env.Tool('addLibrary', library = env['x11Libs'])
+    if env['PLATFORM'] != 'win32':
+        env.Tool('addLibrary', library = env['x11Libs'])
+    else:
+        env.Tool('addLibrary', library = ['gdi32', 'winspool', 'comdlg32', 'shell32'])
     env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
