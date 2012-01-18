4abc2f20ea21ff1939a417f9241b1738e843cdd5
jrb <>
2012-01-18 01:25:36 +0000

    Add compile- and link-time stuff for dependency on OmniOrb

--- HepRepCorba/HepRepCorbaLib.py
+++ HepRepCorba/HepRepCorbaLib.py
@@ -1,8 +1,9 @@
 # $Header$
 def generate(env, **kw):
-    if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = ['HepRepCorba'])
+    #if not kw.get('depsOnly', 0):
+    #    env.Tool('addLibrary', library = ['HepRepCorba'])
     env.Tool('HepRepSvcLib')	
+    env.Tool('addLibrary', library = env['OmniOrbLibs'])
     if env['PLATFORM'] == 'win32':
 	env.Tool('addLibrary', library = ['oldnames'])
     
