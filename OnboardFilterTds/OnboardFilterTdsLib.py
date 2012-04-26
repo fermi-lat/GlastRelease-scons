00b88caaa379a4021dff2d9d7b61274cec1e169a
jrb <>
2012-04-26 21:48:47 +0000

    for slightly upgraded obf B3-0-0 (-gl1)

--- OnboardFilterTds/OnboardFilterTdsLib.py
+++ OnboardFilterTds/OnboardFilterTdsLib.py
@@ -3,5 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OnboardFilterTds'])
     env.Tool('EventLib')
+    env.Tool('addLibrary', library=env['obfLibs'])
 def exists(env):
     return 1;
