df7e1a55f9e3d56bb2cfa2126c1c70a21d3baa2d
jrb <>
2012-05-02 22:57:23 +0000

    geometry pkg has dependence on clhep

--- geometry/geometryLib.py
+++ geometry/geometryLib.py
@@ -2,6 +2,6 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['geometry'])
-
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
