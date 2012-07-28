6fc51019e2b3b203970a614e873d6654d3bec1cb
bregeon <>
2012-07-28 20:14:45 +0000

    reorder for fun

--- calibGenTRG/calibGenTRGLib.py
+++ calibGenTRG/calibGenTRGLib.py
@@ -3,11 +3,12 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibGenTRG'])
     env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('commonRootDataLib')
     env.Tool('digiRootDataLib')
     env.Tool('reconRootDataLib')
     env.Tool('mcRootDataLib')
-    env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('CalUtilLib')
 
 def exists(env):
