ef8dde1328be84df4f1c663365a9bc69a4584a91
bregeon <>
2012-07-28 13:53:21 +0000

    first versions that compiles, still probably very wrong

---
+++ calibGenTRG/calibGenTRGLib.py
@@ -0,0 +1,16 @@
+# $Header$
+def generate(env, **kw):
+    if not kw.get('depsOnly', 0):
+        env.Tool('addLibrary', library = ['calibGenTRG'])
+    env.Tool('addLibrary', library = env['rootLibs'])
+#    env.Tool('addLibrary', library = env['rootGuiLibs'])
+#    env.Tool('addLibrary', library = env['pythonLibs'])
+    env.Tool('commonRootDataLib')
+    env.Tool('digiRootDataLib')
+    env.Tool('reconRootDataLib')
+    env.Tool('mcRootDataLib')
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('CalUtilLib')
+
+def exists(env):
+    return 1;
