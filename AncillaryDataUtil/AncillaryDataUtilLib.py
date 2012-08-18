97b3a821a9d237ac6983b19dc33a5904de4c130f
jrb <>
2012-08-18 01:25:21 +0000

    for Windows no-install-headers

--- AncillaryDataUtil/AncillaryDataUtilLib.py
+++ AncillaryDataUtil/AncillaryDataUtilLib.py
@@ -2,6 +2,15 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AncillaryDataUtil'])
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'AncillaryDataUtil') 
+            env.Tool('findPkgPath', package = 'AncillaryDataEvent') 
+            env.Tool('findPkgPath', package = 'AdfEvent') 
+            env.Tool('findPkgPath', package = 'facilities') 
     env.Tool('facilitiesLib')
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'AncillaryDataEvent') 
+        env.Tool('findPkgPath', package = 'AdfEvent') 
+        env.Tool('findPkgPath', package = 'facilities') 
 def exists(env):
     return 1;
