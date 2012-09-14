ec118fca4e3f7d296f6a151cf23146c886113909
usher <>
2012-09-14 14:46:11 +0000

    Prepare to tag Overlay-02-04-00

--- Overlay/src/MergeAlgs/CalOverlayMergeAlg.cxx
+++ Overlay/src/MergeAlgs/CalOverlayMergeAlg.cxx
@@ -261,6 +261,12 @@ StatusCode CalOverlayMergeAlg::execute()
         // Does the identifier for this CalOverlay match an McIntegratingHit in the sim map?
         std::map<idents::VolumeIdentifier,Event::McIntegratingHit*>::iterator calIter = idToMcHitMap.find(overId);
 
+		// If not a pure overlay McIntegratingHit then set iterator to end
+		// This will insure that the overlay information in the McIntegratingHit collection in the TDS is 
+		// included in a unique (to the simulated hits) McIntegratingHit object
+        if (calIter != idToMcHitMap.end() && (calIter->second)->getPackedFlags() != Event::McIntegratingHit::overlayHit) 
+			calIter = idToMcHitMap.end();
+
         // Pointer to the mcHit
         Event::McIntegratingHit* mcHit = 0;
 
