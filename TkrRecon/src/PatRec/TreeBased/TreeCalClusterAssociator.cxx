d68a6316a8f3275e84a829d1f2bce36d9e54b904
usher <>
2013-02-21 19:58:11 +0000

    Change from expecting a reference to a copy

--- TkrRecon/src/PatRec/TreeBased/TreeCalClusterAssociator.cxx
+++ TkrRecon/src/PatRec/TreeBased/TreeCalClusterAssociator.cxx
@@ -116,7 +116,7 @@ int TreeCalClusterAssociator::associateTreeToClusters(Event::TkrTree* tree)
             if (fabs(posAtCalTop.x()) < calXMax + 500. && fabs(posAtCalTop.y()) < calYMax + 500.) 
             {
                 // Get a copy of the raw cluster vec
-                Event::CalClusterVec& clusterVec = m_calClusterMap->getRawClusterVec();
+                Event::CalClusterVec clusterVec = m_calClusterMap->getRawClusterVec();
 
                 // If more than one cal cluster then we only consider those that have "significant" energy
                 // which we loosely define as within 10% of the main cluster
